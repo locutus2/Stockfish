fb88787186e37c498c7dab2298057bbddf7ed27d
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-05-22 23:20:41 +0200

    Fixed: Ignore stat score condition for captures. bench 4464246

--- src/search.cpp
+++ src/search.cpp
@@ -749,7 +749,6 @@ namespace {
         &&  eval >= beta
         &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
         && !excludedMove
-        && (*(ss-2)->contHistory)[pos.piece_on(prevSq)][prevSq] < 15000
         &&  pos.non_pawn_material(pos.side_to_move())
         && (ss->ply >= thisThread->nmp_ply || ss->ply % 2 != thisThread->nmp_odd))
     {
