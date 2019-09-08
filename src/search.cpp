af885e3e7e57c280ae9c706401a14dc72d9567af
Stefan Geschwentner <locutus2@users.noreply.github.com>
2019-09-08 16:17:01 +0200

    No LMR at root if no best move was found. Bench: 3759356

--- src/search.cpp
+++ src/search.cpp
@@ -1076,7 +1076,7 @@ moves_loop: // When in check, search starts from here
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
           &&  moveCount > 1 + 2 * rootNode
-          && (!rootNode || thisThread->best_move_count(move) == 0)
+          && (!rootNode || (bestMove && thisThread->best_move_count(move) == 0))
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
