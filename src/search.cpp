cab23d4e37a595095d4f8b2d7c54d5ff2c555aa0
Stefan Geschwentner <locutus2@users.noreply.github.com>
2019-09-08 21:47:03 +0200

    Less reduction at root if at least second research. Bench: 4456743

--- src/search.cpp
+++ src/search.cpp
@@ -1091,8 +1091,8 @@ moves_loop: // When in check, search starts from here
           if (th.marked())
               r += ONE_PLY;
 
-          // Decrease reduction at ply < 2 if in research
-          if (ss->ply < 2 && thisThread->research)
+          // Decrease reduction at root if at least second research
+          if (rootNode && thisThread->research > 1)
               r -= ONE_PLY;
 
           // Decrease reduction if position is or has been on the PV
