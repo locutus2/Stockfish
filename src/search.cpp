82e086d45f64c0474c9003c5d597c6e91e5b1ff1
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-12-24 22:20:05 +0100

    Only at PV nodes: Less reduction if tt move is not best move. Bench: 5432009

--- src/search.cpp
+++ src/search.cpp
@@ -1109,7 +1109,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Decrease reduction if tt move is not best move
-          if (ttMove && ttMove != bestMove)
+          if (PvNode && ttMove && ttMove != bestMove)
               r--;
 
           // Decrease reduction if the ttHit running average is large
