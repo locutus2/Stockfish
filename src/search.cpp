b40a5e3087c8abd92779efce13bd2915a473b019
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-01 21:25:22 +0100

    Additional condition for quiet SEE pruning: hist+cmh0+cmh1+cmh3 < 30000. Bench: 5310243

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,7 @@ moves_loop: // When in check, search starts from here
                   &&  thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)] < 40000)
+                    + (*contHist[3])[movedPiece][to_sq(move)] < 30000)
                   continue;
           }
           else if (!pos.see_ge(move, Value(-194) * depth)) // (~20 Elo)
