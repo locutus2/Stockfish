e78beb609d5f81774c4d6d79f5fdcaf8a603efd5
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-04 15:20:34 +0100

    Tweak futility pruning history threshold: 40000-2000*lmrDepth. Test against my passed prune_tweak2 patch. Bench: 4986112

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts from here
                   &&  thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)] < 32000 - 2000 * lmrDepth)
+                    + (*contHist[3])[movedPiece][to_sq(move)] < 40000 - 2000 * lmrDepth)
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
