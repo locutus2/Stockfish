edbefb3c3148a284fcb21e4575a107c828744198
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-19 14:09:42 +0100

    Do a series of tests on the different reductions at PV nodes. First try: no tt capture reduction at PV nodes. Bench: 4817887

--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts from here
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
-              if (ttCapture)
+              if (!PvNode && ttCapture)
                   r++;
 
               // Increase reduction for cut nodes (~10 Elo)
