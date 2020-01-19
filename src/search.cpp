6eb06cc2b4aacb40d0f19819239ba457042eef76
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-19 14:38:25 +0100

    Less reduction for escape moves at PV nodes. Bench: 5137837

--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts from here
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
-              if (!PvNode && ttCapture)
+              if (ttCapture)
                   r++;
 
               // Increase reduction for cut nodes (~10 Elo)
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts from here
               // hence break make_move(). (~2 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(reverse_move(move)))
-                  r -= 2;
+                  r -= 2 + PvNode;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
