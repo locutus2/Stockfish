20df38eea9f6d684c07a1412def7cc9797a1def4
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-19 14:41:17 +0100

    Less reduction for better stat score at PV nodes. Bench: 4455970

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts from here
               // hence break make_move(). (~2 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(reverse_move(move)))
-                  r -= 2 + PvNode;
+                  r -= 2;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
@@ -1177,7 +1177,7 @@ moves_loop: // When in check, search starts from here
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -102 && (ss-1)->statScore < -114)
-                  r--;
+                  r -= 1 + PvNode;
 
               else if ((ss-1)->statScore >= -116 && ss->statScore < -154)
                   r++;
