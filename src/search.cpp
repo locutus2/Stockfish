6f3f2fa5d11643997a8139baa9c2c699ecc25e63
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-30 10:31:00 +0100

    Abort continuation history update if null move found for ss-1. Bench: 4516102

--- src/search.cpp
+++ src/search.cpp
@@ -1652,7 +1652,7 @@ moves_loop: // When in check, search starts from here
     for (int i : {1, 2, 4, 6})
         if (is_ok((ss-i)->currentMove))
             (*(ss-i)->continuationHistory)[pc][to] << bonus;
-        else
+        else if (i == 1)
             break;
   }
 
