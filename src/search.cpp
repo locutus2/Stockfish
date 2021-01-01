1e1aab68615abbfc4904801fb6b5116996a29dad
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-01 21:52:17 +0100

    No LMR for recaptures. Bench: 4007238

--- src/search.cpp
+++ src/search.cpp
@@ -1156,11 +1156,12 @@ moves_loop: // When in check, search starts from here
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
-              || moveCountPruning
-              || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode
-              || (!PvNode && !formerPv && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
-              || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
+              || (   (!priorCapture || prevSq != to_sq(move))
+                  && (   moveCountPruning
+                      || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
+                      || cutNode
+                      || (!PvNode && !formerPv && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
+                      || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))))
       {
           Depth r = reduction(improving, depth, moveCount);
 
