ed517eb68657e6768df44212a31c9bebd312d329
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-17 20:22:51 +0100

    NMP: Ignore statScore condition if last move was a capture. Bench: 4237557

--- src/search.cpp
+++ src/search.cpp
@@ -839,7 +839,7 @@ namespace {
     // Step 8. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 22977
+        && (priorCapture || (ss-1)->statScore < 22977)
         &&  eval >= beta
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 30 * depth - 28 * improving + 84 * ss->ttPv + 168
