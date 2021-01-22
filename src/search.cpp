a51d3f5e481f2c41b7dcd3adba7b573a4a671606
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-22 21:24:02 +0100

    More reduction at expected ALL nodes if moveCountPruning && ttCapture && priorCapture && !givesCheck. Bench: 4344973

--- src/search.cpp
+++ src/search.cpp
@@ -1192,6 +1192,9 @@ moves_loop: // When in check, search starts from here
           if (singularQuietLMR)
               r--;
 
+          if (!PvNode && !cutNode && moveCountPruning && ttCapture && priorCapture && !givesCheck)
+              r++;
+
           if (captureOrPromotion)
           {
               // Unless giving check, this capture is likely bad
