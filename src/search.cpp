4b7c8abbbb415e21b2a340747aba3ca8afd72b13
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-23 09:42:30 +0100

    More reduction for quiet move if ttCapture and in check. Bench: 4216166

--- src/search.cpp
+++ src/search.cpp
@@ -1203,7 +1203,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r++;
+                  r += 1 + ss->inCheck;
 
               // Increase reduction at root if failing high
               r += rootNode ? thisThread->failedHighCnt * thisThread->failedHighCnt * moveCount / 512 : 0;
