3089733fa63190959368d507f06e917f23fe2f9a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-28 07:29:31 +0100

    If not in check and at PV node a new best quiet move is found update second killer. Bench: 3591955

--- src/search.cpp
+++ src/search.cpp
@@ -1340,8 +1340,8 @@ moves_loop: // When in check, search starts from here
               {
                   alpha = value;
 
-                  if (!ss->inCheck && !captureOrPromotion && from_sq((ss+2)->killers[0]) == from_sq(move) && (ss+2)->killers[1] != move)
-                      (ss+2)->killers[0] = move;
+                  if (!ss->inCheck && !captureOrPromotion && from_sq((ss+2)->killers[1]) == from_sq(move) && (ss+2)->killers[0] && (ss+2)->killers[0] != move)
+                      (ss+2)->killers[1] = move;
               }
               else
               {
