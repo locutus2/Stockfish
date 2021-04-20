96970c143ec8e531018efa9ce9debfb30713ca9d
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-20 19:14:46 +0200

    Randomly do less reduction using p=1/rootDepth. Bench: 4195329

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           if (!(thisThread->nodes % thisThread->rootDepth))
-              r++;
+              r--;
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
