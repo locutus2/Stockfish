ef58c80f5a16b2ef78c5f86e5e3f53ef92e5cf87
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-20 20:49:02 +0200

    Randomly do more reduction using p=1/(2*rootDepth). Bench: 4283703

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (thisThread->nodes % thisThread->rootDepth <= 1)
+          if (!(thisThread->nodes % (2 * thisThread->rootDepth)))
               r++;
 
           // Decrease reduction if the ttHit running average is large
