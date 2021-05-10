c4e15f3c0316b37656254e33b67c63b563a6d504
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-10 19:22:54 +0200

    Also extend randomly (p=1/2) in else case of singular search. Bench: 4451083

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
 
-          else
+          else if (thisThread->nodes & 1)
               extension = 1;
       }
 
