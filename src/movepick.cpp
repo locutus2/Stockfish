c26965ad095c58a7c2dc6b313f9e2f2d0875861f
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-07 11:34:07 +0200

    Half divisor of capture history. Compensate by double up weight of usages (+refactor to simplify). Fixed sorting limit. Bench: 5903412

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -201,7 +201,7 @@ top:
       endMoves = generate<CAPTURES>(pos, cur);
 
       score<CAPTURES>();
-      partial_insertion_sort(cur, endMoves, -3000 * depth);
+      partial_insertion_sort(cur, endMoves, -1500 * depth);
       ++stage;
       goto top;
 
