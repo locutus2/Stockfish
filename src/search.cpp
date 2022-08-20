31c0254a638673f63008372932dff1c8a8045d6d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-20 20:33:32 +0200

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (!improving && PvNode)
+          if (!priorCapture && ss->inCheck)
           {
               r--;
               dbg_mean_of(100);
