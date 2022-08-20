9b5e3a9520df657cc0b2743d9d0a712147257190
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-20 21:12:28 +0200

    Less reduction at cut nodes if prior capture. Bench: 5225384

--- src/search.cpp
+++ src/search.cpp
@@ -1151,7 +1151,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 2 - ss->inCheck;
+              r += 2 - priorCapture;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
