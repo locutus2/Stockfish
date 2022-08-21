e484a36516d2d0807b5b0fcb200548fdd3f1c1e2
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-21 14:59:15 +0200

    Less reduction if move count pruning. Bench: 5881033

--- src/search.cpp
+++ src/search.cpp
@@ -1151,7 +1151,10 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 2 - (capture && depth < 8);
+              r += 2;
+
+          if (moveCountPruning)
+              r--;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
