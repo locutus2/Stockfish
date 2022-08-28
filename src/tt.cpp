8f55189de911ac038fadf93c2eacc7958028dd5d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-28 10:26:25 +0200

    Use only 15 bit in tt for static eval (assuming always 0 for least significant bit so resoultion is halfed) and use the last significant bit instead for PV flag. So generation can use 6 instead of 5 bits. Bench: 5600264

--- src/tt.cpp
+++ src/tt.cpp
@@ -49,9 +49,9 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
 
       key16     = (uint16_t)k;
       depth8    = (uint8_t)(d - DEPTH_OFFSET);
-      genBound8 = (uint8_t)(TT.generation8 | uint8_t(pv) << 2 | b);
+      genBound8 = (uint8_t)(TT.generation8 | b);
       value16   = (int16_t)v;
-      eval16    = (int16_t)ev;
+      eval16    = (int16_t)(ev & 0xFE) | int16_t(pv);
   }
 }
 
--- src/tt.h
+++ src/tt.h
@@ -28,20 +28,20 @@ namespace Stockfish {
 ///
 /// key        16 bit
 /// depth       8 bit
-/// generation  5 bit
-/// pv node     1 bit
+/// generation  6 bit
 /// bound type  2 bit
 /// move       16 bit
 /// value      16 bit
-/// eval value 16 bit
+/// eval value 15 bit
+/// pv node     1 bit
 
 struct TTEntry {
 
   Move  move()  const { return (Move )move16; }
   Value value() const { return (Value)value16; }
-  Value eval()  const { return (Value)eval16; }
+  Value eval()  const { return (Value)(eval16 & 0xFE); }
   Depth depth() const { return (Depth)depth8 + DEPTH_OFFSET; }
-  bool is_pv()  const { return (bool)(genBound8 & 0x4); }
+  bool is_pv()  const { return (bool)(eval16 & 0x1); }
   Bound bound() const { return (Bound)(genBound8 & 0x3); }
   void save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev);
 
@@ -75,7 +75,7 @@ class TranspositionTable {
   static_assert(sizeof(Cluster) == 32, "Unexpected Cluster size");
 
   // Constants used to refresh the hash table periodically
-  static constexpr unsigned GENERATION_BITS  = 3;                                // nb of bits reserved for other things
+  static constexpr unsigned GENERATION_BITS  = 2;                                // nb of bits reserved for other things
   static constexpr int      GENERATION_DELTA = (1 << GENERATION_BITS);           // increment for generation field
   static constexpr int      GENERATION_CYCLE = 255 + (1 << GENERATION_BITS);     // cycle length
   static constexpr int      GENERATION_MASK  = (0xFF << GENERATION_BITS) & 0xFF; // mask to pull out generation number
