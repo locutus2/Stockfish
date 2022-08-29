f66c91b240bb5466a8f246a9eff94f42512176ad
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-30 01:41:35 +0200

    Prefer always bound in order NONE, UPPER/LOWER, EXACT for replacement. Bench: 5927345

--- src/tt.cpp
+++ src/tt.cpp
@@ -138,8 +138,8 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const {
       // is needed to keep the unrelated lowest n bits from affecting
       // the result) to calculate the entry age correctly even after
       // generation8 overflows into the next cycle.
-      if (   replace->bound() > tte[i].bound()
-          || (    replace->bound() == tte[i].bound()
+      if (   popcount(replace->bound()) > popcount(tte[i].bound())
+          || (    popcount(replace->bound()) == popcount(tte[i].bound())
               &&  replace->depth8 - ((GENERATION_CYCLE + generation8 - replace->genBound8) & GENERATION_MASK)
                 >   tte[i].depth8 - ((GENERATION_CYCLE + generation8 -   tte[i].genBound8) & GENERATION_MASK)))
           replace = &tte[i];
