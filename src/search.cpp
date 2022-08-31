56ffc19a6df18d7a9846462fd74da261c7308059
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-01 00:05:33 +0200

    Don't do cutoffCnt reduction for each second thread. Bench: 6079565

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if ((ss+1)->cutoffCnt > 3 && !PvNode)
+          if ((ss+1)->cutoffCnt > 3 && !PvNode && thisThread->id() % 2 == 0)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
