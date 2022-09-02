da5ab5a32cb82be9714543ac49274038194113b6
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-02 19:58:28 +0200

    Do even less reduction if late previous move. Bench: 5756188

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 7)
-              r--;
+              r -= 2;
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
