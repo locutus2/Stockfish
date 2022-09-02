975341e733622014eb604ae11630239f3064756a
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-02 17:25:27 +0200

    Do less reduction if late previous move for each second thread. Bench: 6079565

--- src/search.cpp
+++ src/search.cpp
@@ -1143,11 +1143,11 @@ moves_loop: // When in check, search starts here
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
               && !likelyFailLow)
-              r -= 2 - thisThread->id() % 2;
+              r -= 2;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 7)
-              r--;
+              r -= 1 + thisThread->id() % 2;
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
