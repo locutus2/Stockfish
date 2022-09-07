49540d4365dec0d507cbf9183f9d6a512c60ddf9
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-07 14:01:19 +0200

    Less reduction for captures if beta >= draw. Bench: 5943764

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / (3 + depth);
 
-          if (capture && bestValue >= alpha)
+          if (capture && beta >= VALUE_DRAW)
               r--;
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
