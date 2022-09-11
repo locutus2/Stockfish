301b249572cd49384fe7d8aac13840eae4dce3bf
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-11 16:12:40 +0200

    The passed bigpenorPR reverts partially my stats tweak patch (only the bonus formula himself). So try now new formula * 2 / 3 (which has the same quadratic factor as my old formula). Bench: 4534695

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((11 * d + 259) * d - 319 , 1461);
+    return std::min((8 * d + 188) * d - 232 , 1063);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
