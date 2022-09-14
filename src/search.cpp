a5dca9613709420d38b224451c0bd54fb9625212
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-15 00:01:08 +0200

    More reduction at root if root average score of best move is at least roughly two pawns better than the next move average score. Bench: 4224684

--- src/search.cpp
+++ src/search.cpp
@@ -1168,10 +1168,10 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (   PvNode
+          if (   rootNode
               && thisThread->rootMoves.size() >= 2 + thisThread->pvIdx
               && thisThread->rootMoves[thisThread->pvIdx + 1].averageScore != VALUE_INFINITE
-              && thisThread->rootMoves[thisThread->pvIdx].averageScore > 200 + thisThread->rootMoves[thisThread->pvIdx + 1].averageScore)
+              && thisThread->rootMoves[thisThread->pvIdx].averageScore > 400 + thisThread->rootMoves[thisThread->pvIdx + 1].averageScore)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
