82ae4523cc26efa611c910934100272863a5ed83
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-15 22:56:39 +0200

    More reduction at PV nodes if root average score of best move is at least roughly a half pawn better than the next move average score. Bench: 3653807

--- src/search.cpp
+++ src/search.cpp
@@ -1171,8 +1171,8 @@ moves_loop: // When in check, search starts here
           if (   PvNode
               && thisThread->rootMoves.size() >= 2 + thisThread->pvIdx
               && thisThread->rootMoves[thisThread->pvIdx + 1].averageScore != VALUE_INFINITE
-              && thisThread->rootMoves[thisThread->pvIdx].averageScore > 200 + thisThread->rootMoves[thisThread->pvIdx + 1].averageScore)
-              r += (thisThread->rootMoves[thisThread->pvIdx].averageScore - thisThread->rootMoves[thisThread->pvIdx + 1].averageScore) / 200;
+              && thisThread->rootMoves[thisThread->pvIdx].averageScore > 100 + thisThread->rootMoves[thisThread->pvIdx + 1].averageScore)
+              r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
