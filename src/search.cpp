90d1f6b335d1b8aac57e508cffc2ad5c5c8bb787
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-15 18:58:45 +0200

    More reduction at PV nodes which nearer to root as to the leafs if root average score of best move is at least roughly a pawn better than the next move average score. Bench: 3724652

--- src/search.cpp
+++ src/search.cpp
@@ -1168,7 +1168,8 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (   ss->ttPv
+          if (   PvNode
+              && ss->ply < depth
               && thisThread->rootMoves.size() >= 2 + thisThread->pvIdx
               && thisThread->rootMoves[thisThread->pvIdx + 1].averageScore != VALUE_INFINITE
               && thisThread->rootMoves[thisThread->pvIdx].averageScore > 200 + thisThread->rootMoves[thisThread->pvIdx + 1].averageScore)
