979cd6f4dbac313ac7a8fc2b761758990a51968f
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-16 22:50:00 +0200

    Do no reductions for checking moves (under same condition as capture). Bench: 4263292

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop: // When in check, search starts here
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
-              || !capture
+              || !(capture || givesCheck)
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
