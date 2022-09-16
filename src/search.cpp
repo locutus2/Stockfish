4541d36e53a8ec353c7a0b3d396962469ed3a6f6
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-16 13:44:17 +0200

    More reduction at PV nodes if root previous score of best move is at least roughly a pawn better than the next move previous score. Bench: 3397962

--- src/search.cpp
+++ src/search.cpp
@@ -1170,8 +1170,8 @@ moves_loop: // When in check, search starts here
 
           if (   PvNode
               && thisThread->rootMoves.size() >= 2 + thisThread->pvIdx
-              && thisThread->rootMoves[thisThread->pvIdx + 1].score != VALUE_INFINITE
-              && thisThread->rootMoves[thisThread->pvIdx].score > 200 + thisThread->rootMoves[thisThread->pvIdx + 1].score)
+              && thisThread->rootMoves[thisThread->pvIdx + 1].previousScore != VALUE_INFINITE
+              && thisThread->rootMoves[thisThread->pvIdx].previousScore > 200 + thisThread->rootMoves[thisThread->pvIdx + 1].previousScore)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
