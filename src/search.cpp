2bdedd7b554402364d6de08d1f4f179944dc1dbc
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-16 18:38:19 +0200

    Two plies more reduction at PV nodes at even plies if root average score of best move is at least roughly a pawn better than the next move average score. Bench: 4251844

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
               && thisThread->rootMoves.size() >= 2 + thisThread->pvIdx
               && thisThread->rootMoves[thisThread->pvIdx + 1].averageScore != VALUE_INFINITE
               && thisThread->rootMoves[thisThread->pvIdx].averageScore > 200 + thisThread->rootMoves[thisThread->pvIdx + 1].averageScore)
-              r++;
+              r += 2;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
