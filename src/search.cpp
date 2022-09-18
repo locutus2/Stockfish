a41bfb13649c34f095024217bdba2088ee62acc2
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-18 09:45:06 +0200

    Do no reductions for captures if in check. Bench: 4426326

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,8 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || (cutNode && (ss-1)->moveCount > 1)))
+              || (cutNode && (ss-1)->moveCount > 1))
+          && !(capture && ss->inCheck))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
