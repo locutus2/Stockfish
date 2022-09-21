5c798446606b2abf16a7f2204ad49b99e337ff92
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-21 23:12:54 +0200

    Extend cut node case also to ALL nodes but use there 1 + depth + ply as limit. Bench: 4100755

--- src/search.cpp
+++ src/search.cpp
@@ -1131,7 +1131,7 @@ moves_loop: // When in check, search starts here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
-          && (   !(PvNode || (ss->ttPv && (ss-1)->moveCount <= 1 + (depth + 20) * !cutNode))
+          && (   !(PvNode || (ss->ttPv && (ss-1)->moveCount <= 1 + (depth + ss->ply) * !cutNode))
               || !capture))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
