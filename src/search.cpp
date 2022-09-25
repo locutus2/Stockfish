7529b514ea01d234a69ed0a56aa847d34666aee5
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-09-25 20:13:50 +0200

    Less reduction for depth >= 11. Bench: 4353314

--- src/search.cpp
+++ src/search.cpp
@@ -1167,8 +1167,8 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (depth <= 3)
-              r++;
+          if (depth >= 11)
+              r--;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
