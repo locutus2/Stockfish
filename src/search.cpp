c907a794f693ccb61d16b083ceba88861c7238c7
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-07 18:34:22 +0200

    More reduction at non-PV nodes if current best value is a TB win or better. Bench: 4086125

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (!givesCheck && bestValue >= VALUE_TB_WIN_IN_MAX_PLY)
+          if (!PvNode && bestValue >= VALUE_TB_WIN_IN_MAX_PLY)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
