1c6c6c48cf8b737f85b9843115b4ce62bbbe6d29
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-07 16:57:49 +0200

    More reduction for captures if current best value is a TB win or better. Bench: 4114228

--- src/search.cpp
+++ src/search.cpp
@@ -1133,8 +1133,7 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || (cutNode && (ss-1)->moveCount > 1)
-              || bestValue >= VALUE_TB_WIN_IN_MAX_PLY))
+              || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
@@ -1173,7 +1172,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (bestValue >= VALUE_TB_WIN_IN_MAX_PLY)
+          if (capture && bestValue >= VALUE_TB_WIN_IN_MAX_PLY)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
