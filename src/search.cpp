79adabd815db702d2a1ad460200f1ff148221750
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-07 11:19:41 +0200

    Combine TB win ideas 'Do also reductions if current best value is a TB win or better.' and 'More reduction if current best value is a TB win or better. ' because each seems to have a small elo gain. Bench: 4094210

--- src/search.cpp
+++ src/search.cpp
@@ -1173,6 +1173,9 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if (bestValue >= VALUE_TB_WIN_IN_MAX_PLY)
+              r++;
+
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
