77ec761adbc84147ad3b485b1b1f1ba39d79b7f5
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-08 03:40:17 +0200

    Less reduction at PV nodes for first killer from grand childs. Bench: 3919477

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (!capture && move == (ss+2)->killers[0])
+          if (PvNode && !capture && move == (ss+2)->killers[0])
               r--;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
