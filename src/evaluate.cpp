02d0046dc0ab326c3da62cf5940c822fd471febf
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-13 20:06:07 +0200

    Scale down endgame NNUE eval by 10%. Bench: 5067766

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   else
   {
       int nnueComplexity;
-      int scale = 1064 + 106 * pos.non_pawn_material() / 5120;
+      int scale = 958 + pos.non_pawn_material() / 48;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
