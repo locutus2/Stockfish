ee03e0e8b9654703c3e7cac3226ee0ead436ed87
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-16 23:03:08 +0200

    Even less high previous move count reduction if move count >= 32. Bench: 4071366

--- src/search.cpp
+++ src/search.cpp
@@ -1143,11 +1143,11 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 7)
-              r--;
+              r -= 1 + (moveCount >= 32);
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 + (moveCount < 32);
+              r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
