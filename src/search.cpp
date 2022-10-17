9b75424a8ae717ce4fc1f2ea945e05db7f5bd12f
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-17 21:43:13 +0200

    Half improvement if using ss-4. Bench: 4346817

--- src/search.cpp
+++ src/search.cpp
@@ -768,9 +768,9 @@ namespace {
     // in check at our previous move we look at the move prior to it). The improvement
     // margin and the improving flag are used in various pruning heuristics.
     improvement =   (ss-2)->staticEval != VALUE_NONE ? ss->staticEval - (ss-2)->staticEval
-                  : (ss-4)->staticEval != VALUE_NONE ? ss->staticEval - (ss-4)->staticEval
+                  : (ss-4)->staticEval != VALUE_NONE ? (ss->staticEval - (ss-4)->staticEval) / 2
                   :                                    168;
-    improving = improvement > -20;
+    improving = improvement > 0;
 
     // Step 7. Razoring.
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
