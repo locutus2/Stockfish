79e3f45915e9eece765f4bd66c9f9f47d22d8a64
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-17 09:42:29 +0200

    Less reduction for extended moves if move count >= 16. Bench: 4464142

--- src/search.cpp
+++ src/search.cpp
@@ -1145,7 +1145,7 @@ moves_loop: // When in check, search starts here
           if ((ss-1)->moveCount > 7)
               r--;
 
-          if (extension > 0 && moveCount >= 32)
+          if (extension > 0 && moveCount >= 16)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
