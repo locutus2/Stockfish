703ae03b64679a68e0348072c3f68060758cae8c
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-18 05:49:19 +0200

    Count even a drop of at most 30 units in static eval as 'improved'. Bench: 3952076

--- src/search.cpp
+++ src/search.cpp
@@ -770,7 +770,7 @@ namespace {
     improvement =   (ss-2)->staticEval != VALUE_NONE ? ss->staticEval - (ss-2)->staticEval
                   : (ss-4)->staticEval != VALUE_NONE ? ss->staticEval - (ss-4)->staticEval
                   :                                    168;
-    improving = improvement > 0 && (ss-1)->statScore >= 0;
+    improving = improvement > -30;
 
     // Step 7. Razoring.
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
