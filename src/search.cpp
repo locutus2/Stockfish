cdae8919d83351f3f8fa12feb7c4a968a0143066
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-10-20 23:10:26 +0200

    More reduction if moveCount + (ss-2)->moveCount + (ss-4)->moveCount > 102. Bench: 4132788

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (moveCount + (ss-2)->moveCount > 74)
+          if (moveCount + (ss-2)->moveCount + (ss-4)->moveCount > 102)
               r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
