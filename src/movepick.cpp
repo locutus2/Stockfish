6c663d0f974abb77e1eb8d0317e6ec9c0a4960a5
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-02 20:38:28 +0100

    Decrease quiet order score resolution by factor 16. Bench: 4381215

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -127,18 +127,18 @@ void MovePicker::score() {
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
-          m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   +     (threatenedPieces & from_sq(m) ?
-                           (type_of(pos.moved_piece(m)) == QUEEN && !(to_sq(m) & threatenedByRook)  ? 50000
-                          : type_of(pos.moved_piece(m)) == ROOK  && !(to_sq(m) & threatenedByMinor) ? 25000
-                          :                                         !(to_sq(m) & threatenedByPawn)  ? 15000
-                          :                                                                           0)
-                          :                                                                           0)
-                   +     bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m)) * 16384;
+          m.value =(  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
+                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
+                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
+                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
+                    +     (threatenedPieces & from_sq(m) ?
+                            (type_of(pos.moved_piece(m)) == QUEEN && !(to_sq(m) & threatenedByRook)  ? 50000
+                           : type_of(pos.moved_piece(m)) == ROOK  && !(to_sq(m) & threatenedByMinor) ? 25000
+                           :                                         !(to_sq(m) & threatenedByPawn)  ? 15000
+                           :                                                                           0)
+                           :                                                                           0)
+                    +     bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m)) * 16384) / 16;
       else // Type == EVASIONS
       {
           if (pos.capture(m))
@@ -229,7 +229,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -188 * depth);
       }
 
       ++stage;
