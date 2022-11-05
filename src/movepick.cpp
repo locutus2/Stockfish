958d784f226a1d7bb1c2e47b0b1b959e84feb78d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-05 16:28:23 +0100

    Order bonus 4096 for a capture of the next ply first killer piece (only pseudo legal killer, but this version is around 0.5% faster). Bench: 4561447

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -126,7 +126,7 @@ void MovePicker::score() {
       if constexpr (Type == CAPTURES)
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   +     (nextKiller != MOVE_NONE && to_sq(m) == from_sq(nextKiller)) * 1024;
+                   +     (nextKiller != MOVE_NONE && to_sq(m) == from_sq(nextKiller)) * 4096;
 
       else if constexpr (Type == QUIETS)
           m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
