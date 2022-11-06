8d9cd1d93e8b2c8eb9399f405b5fc082201a0139
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-06 12:25:04 +0100

    Use first killer from two plies earlier as 4th refutation in quiet move ordering. Bench: 4405356

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -61,9 +61,10 @@ MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHist
                                                              const CapturePieceToHistory* cph,
                                                              const PieceToHistory** ch,
                                                              Move cm,
-                                                             const Move* killers)
+                                                             const Move* killers,
+                                                             Move prevKiller)
            : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch),
-             ttMove(ttm), refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}}, depth(d)
+             ttMove(ttm), refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}, {prevKiller, 0}}, depth(d)
 {
   assert(d > 0);
 
@@ -207,9 +208,17 @@ top:
       endMoves = std::end(refutations);
 
       // If the countermove is the same as a killer, skip it
+      if (   refutations[0].move == refutations[3].move
+          || refutations[1].move == refutations[3].move
+          || refutations[2].move == refutations[3].move)
+          --endMoves;
+
       if (   refutations[0].move == refutations[2].move
           || refutations[1].move == refutations[2].move)
+      {
+          refutations[2].move = refutations[3].move;
           --endMoves;
+      }
 
       ++stage;
       [[fallthrough]];
@@ -239,7 +248,8 @@ top:
       if (   !skipQuiets
           && select<Next>([&](){return   *cur != refutations[0].move
                                       && *cur != refutations[1].move
-                                      && *cur != refutations[2].move;}))
+                                      && *cur != refutations[2].move
+                                      && *cur != refutations[3].move;}))
           return *(cur - 1);
 
       // Prepare the pointers to loop over the bad captures
