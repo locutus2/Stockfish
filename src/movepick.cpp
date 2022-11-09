89f68786d01d28c172df13ae248d420a616a638f
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-09 08:26:31 +0100

    Shift sort limit for quiet move ordering depending on stat score of previous own move (if it was good do less sorting). But constrain limit to non-negative numbers. Bench: 4519860

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -230,7 +230,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth + sortOffset);
+          partial_insertion_sort(cur, endMoves, std::min(-3000 * depth + sortOffset, 0));
       }
 
       ++stage;
