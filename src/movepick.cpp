498df311f548ba0eeab297ea3c757c13d5fea55b
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-10 18:59:27 +0100

    Use higher sort limit -1500*depth if tt move found. Bench: 4836305

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -229,7 +229,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, (1500 * bool(ttMove) - 3000) * depth);
       }
 
       ++stage;
