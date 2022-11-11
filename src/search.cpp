ab39721eae22d7513a393fb4f18201e3d547ab16
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-11 21:18:49 +0100

    Reduce other moves if we have found at least one score improvement for 2 * depth < ply. Bench: 4608597

--- src/search.cpp
+++ src/search.cpp
@@ -1277,7 +1277,7 @@ moves_loop: // When in check, search starts here
 
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 1
-                      && depth < 6 + ss->ply
+                      && 2 * depth < ss->ply
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                      depth -= 1;
