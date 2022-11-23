bcd8db1108b00432eb0de68dd9933992051ab961
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-11-23 23:45:05 +0100

    Update continuation history immediately for quiet best moves in the window. Bench: 4354302

--- src/search.cpp
+++ src/search.cpp
@@ -1284,6 +1284,9 @@ moves_loop: // When in check, search starts here
               {
                   alpha = value;
 
+                  if (!capture)
+                      update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus(depth));
+
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 1
                       && depth < 6
