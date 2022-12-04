6c02b7cb536a0818035c69d1c6cc6533e544b422
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-04 23:46:32 +0100

    double shift

--- src/uci.cpp
+++ src/uci.cpp
@@ -234,13 +234,12 @@ namespace {
 
     auto rng = []()->double { return std::rand()/(double)RAND_MAX; };
 
-    constexpr double A = 1;
     //constexpr double L = 10000;
     constexpr double L = 0;
     constexpr double ALPHA = 0.001;
     constexpr double T0 = 100000000;
     constexpr double BETA = 0.98;
-    constexpr int SHIFT = 128;
+    constexpr int SHIFT = 2*128;
     constexpr int KMAX = 1000;
     double score0 = nodes;
     double score = score0;
