72c9d93b9701f806deeed5576a42b2a6562225df
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-06 12:46:57 +0100

    Scale up weight of main history in evasion move ordering with depth: (8+detph)/(4+depth). Bench: 3574587

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -146,8 +146,8 @@ void MovePicker::score() {
                        - Value(type_of(pos.moved_piece(m)))
                        + (1 << 28);
           else
-              m.value =  3 * (*mainHistory)[pos.side_to_move()][from_to(m)]
-                       +     (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
+              m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)] * (8 + std::max(depth, 1)) / (4 + std::max(depth, 1))
+                       + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
       }
 }
 
