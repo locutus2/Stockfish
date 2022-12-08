6f0dc8382945b68a3bb5a6c02aa31b57f924e587
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-08 18:21:04 +0100

    More reduction at PV nodes if in check. Bench: 3527253

--- src/search.cpp
+++ src/search.cpp
@@ -1137,7 +1137,7 @@ moves_loop: // When in check, search starts here
               r -= 2;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
-          if ((ss-1)->moveCount > 7 && !ss->inCheck)
+          if ((ss-1)->moveCount > 7)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
@@ -1150,7 +1150,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 11 / (3 + depth);
+              r -= 1 - ss->inCheck + 11 / (3 + depth);
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
