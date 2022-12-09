35806a4a21328ef388300448d89d8ab6b4bc6b68
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-09 12:05:00 +0100

    Less reduction if ttCapture and promotion. Bench: 3427422

--- src/search.cpp
+++ src/search.cpp
@@ -1142,10 +1142,10 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 2 - (type_of(move) == PROMOTION);
+              r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture)
+          if (ttCapture && type_of(move) != PROMOTION)
               r++;
 
           // Decrease reduction for PvNodes based on depth
