7492be8ae015036ea3274c6c1a3d2b3ce71cf9bf
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-10 19:50:09 +0100

    Consider for the cutoff counter only non-checking moves. Bench: 3978554

--- src/search.cpp
+++ src/search.cpp
@@ -1307,7 +1307,7 @@ moves_loop: // When in check, search starts here
               }
               else
               {
-                  if (!capture)
+                  if (!givesCheck)
                       ss->cutoffCnt++;
                   assert(value >= beta); // Fail high
                   break;
