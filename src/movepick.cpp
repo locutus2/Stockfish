60d9f737a2bbf5cf13b4a5860aa53875d0847674
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-13 20:57:04 +0100

    Evasion order: weight ratio main/cont history = 1 : 2. Bench: 3642786

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -146,8 +146,8 @@ void MovePicker::score() {
                        - Value(type_of(pos.moved_piece(m)))
                        + (1 << 28);
           else
-              m.value =  3 * (*mainHistory)[pos.side_to_move()][from_to(m)]
-                       + 4 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
+              m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
+                       + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
       }
 }
 
