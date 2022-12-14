0c416b749af06c1be032941a1c8e686d99d513f7
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-14 19:43:17 +0100

    Evasion order: use capture history with ratio capture_piece/capture_hist = 8:1. Bench: 3663966

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -142,12 +142,12 @@ void MovePicker::score() {
       else // Type == EVASIONS
       {
           if (pos.capture(m))
-              m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
-                       - Value(type_of(pos.moved_piece(m)))
+              m.value =  8 * PieceValue[MG][pos.piece_on(to_sq(m))]
+                       +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
                        + (1 << 28);
           else
-              m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
-                       + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
+              m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
+                       + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
       }
 }
 
