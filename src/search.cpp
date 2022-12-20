cddfa1f24963cf573a07833ca1201d6e4689dc4e
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-12-20 06:28:42 +0100

    Tweak stat bonus formula. Bench: 3378785

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((12 * d + 282) * d - 349 , 1480);
+    return std::min((13 * d + 435) * d - 171 , 2313);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
