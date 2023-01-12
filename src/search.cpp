5dc134406ce36acf543350c7fcd9f0136f293d60
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-12 23:10:03 +0100

    Try more often less depth at expected ALL nodes for big reduction if no tt move exists and countermove (+fix indention). Bench: 4000267

--- src/search.cpp
+++ src/search.cpp
@@ -1219,7 +1219,7 @@ moves_loop: // When in check, search starts here
           if (!ttMove && cutNode)
               r += 2;
 
-          if (!PvNode && !cutNode)
+          if (!PvNode && !cutNode && !ttMove && move == countermove)
               r++;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
