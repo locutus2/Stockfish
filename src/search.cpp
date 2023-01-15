410eca54a928ab3c0b1c242456b3d1654bedd96c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-15 15:09:02 +0100

    Do full depth research if value > alpha. Bench: 4418850

--- src/search.cpp
+++ src/search.cpp
@@ -1220,6 +1220,9 @@ moves_loop: // When in check, search starts here
                          r += 2;
 
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
+
+               if (!PvNode && value > alpha && r > 4 && move != ttMove)
+                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
