4ebebc951ec3f66b6a432a3ea3f5ab7125fd436d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-15 16:45:17 +0100

    Use also reduced depth for tt store if best move was reduced. Take 3. Bench: 4108244

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop: // When in check, search starts here
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
 
           if (value > alpha)
-              bestMoveReduced = (r > 4 && move != ttMove);
+              bestMoveReduced = (r > 4 && moveCount > 1);
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
