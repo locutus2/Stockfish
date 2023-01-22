d265ace2dda14662fb8dd11763ed2e9f883185c9
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-22 01:55:44 +0100

    Quadruple effect: Add complexity to stat score. Bench: 4243280

--- src/search.cpp
+++ src/search.cpp
@@ -1164,8 +1164,8 @@ moves_loop: // When in check, search starts here
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
                      + (*contHist[3])[movedPiece][to_sq(move)]
-                     + 2 * complexity
-                     - 5000;
+                     + 4 * complexity
+                     - 5531;
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
       r -= ss->statScore / (12800 + 4410 * (depth > 7 && depth < 19));
