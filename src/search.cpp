1d54720a46eaa4409fcf32a9672bb289a2346020
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-22 09:09:18 +0100

    More reduction at PV nodes (depth < 13) if a best move found. Bench: 3781301

--- src/search.cpp
+++ src/search.cpp
@@ -1145,7 +1145,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
-          r -= (ss->ply < depth || !bestMove) + 11 / (3 + depth);
+          r -= (depth < 13 || !bestMove) + 11 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
