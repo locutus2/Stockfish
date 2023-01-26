e2a152a2fcbaa9a842972bc38f44c07989e608eb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-26 20:25:53 +0100

    Less reduction if capturing the moveing piece of previous ply best move. Bench: 3922471

--- src/search.cpp
+++ src/search.cpp
@@ -1169,6 +1169,9 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
+      if (capture && (ss-1)->bestMove && to_sq(move) == from_sq((ss-1)->bestMove))
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
