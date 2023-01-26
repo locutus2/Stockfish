68957df2656eced788aa66b6d4454bcafa186ca1
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-26 19:42:31 +0100

    Fixed version of more reduction at PV nodes if a best move found and is a capture. Using capture function here can be wrong because we have already done the move on the board (if current done move has the same target square then the best move, triggers around in 2% of all cases). Bench: 3692478

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
-          r -= 1 - (1 + (bestMove == ttMove)) * bool(bestMove) + 11 / (3 + depth);
+          r -= 11 / (3 + depth) + (!bestMove || pos.empty(to_sq(bestMove)) || color_of(pos.piece_on(to_sq(bestMove))) == us);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
