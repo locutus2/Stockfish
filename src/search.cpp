3bccc290ef3bd399b952f2843f2792db5b43f61d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-26 21:48:23 +0100

    More reduction at PV nodes if a best move found. Exclude non-quiet moves, counter moves and killers. Bench: 3576928

--- src/search.cpp
+++ src/search.cpp
@@ -1155,6 +1155,16 @@ moves_loop: // When in check, search starts here
       if (PvNode)
           r -= 1 + 11 / (3 + depth);
 
+      if (   PvNode
+          && bestMove
+          && !capture
+          && !givesCheck
+          && type_of(move) != PROMOTION
+          && move != countermove
+          && move != ss->killers[0]
+          && move != ss->killers[1])
+          r++;
+
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
           r--;
