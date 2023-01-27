8898cdbda8e85fc17455620106ee171f2554444d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-01-27 18:54:10 +0100

    More reduction at PV nodes if a best move found. Do even more reduction if best move is a capture. Bench: 3487291

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
-          r -= (!bestMove || capture) + 11 / (3 + depth);
+          r -= 1 - (bestMove && (1 + pos.capture(bestMove))) + 11 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
