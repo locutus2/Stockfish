e9a50938bfbe387176ac11d2605d05a7c9ca151c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-02-07 19:24:35 +0100

    Tweak reduction formula so that more reduction are given in the endgame. Bench: 3399062

--- src/search.cpp
+++ src/search.cpp
@@ -69,8 +69,8 @@ namespace {
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
-  Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
-    int r = Reductions[d] * Reductions[mn];
+  Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta, Value material) {
+    int r = Reductions[d] * Reductions[mn] + 1038 - material / 16;
     return (r + 1460 - int(delta) * 1024 / int(rootDelta)) / 1024 + (!i && r > 937);
   }
 
@@ -998,7 +998,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta, pos.non_pawn_material()), 0);
 
           if (   capture
               || givesCheck)
@@ -1135,7 +1135,7 @@ moves_loop: // When in check, search starts here
       // Step 16. Make the move
       pos.do_move(move, st, givesCheck);
 
-      Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
+      Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta, pos.non_pawn_material());
 
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
