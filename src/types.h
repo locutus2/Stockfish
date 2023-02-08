d9582ef47cef00354f8cf53d92212345b57fa172
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-02-08 20:21:48 +0100

    Fixed: Simpler and extended tuning of piece and material dependent LMR which allows both less and more reduction for one piece dependent on material at the same time. Bench: 4758694

--- src/types.h
+++ src/types.h
@@ -195,7 +195,9 @@ enum Value : int {
   RookValueMg   = 1276,  RookValueEg   = 1380,
   QueenValueMg  = 2538,  QueenValueEg  = 2682,
 
-  MidgameLimit  = 15258, EndgameLimit  = 3915
+  MidgameLimit  = 15258, EndgameLimit  = 3915,
+
+  MaxMaterial   = 4 * KnightValueMg + 4 * BishopValueMg + 4 * RookValueMg + 2 * QueenValueMg
 };
 
 enum PieceType {
