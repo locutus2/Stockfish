094a5faea9d0d0c9a98a86439308bd25a9ffe654
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-01 20:20:27 +0100

    Decrease stats core reduction effect by 10% at ver low/high depth. Bench: 4717156

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop: // When in check, search starts here
                      - 4182;
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-      r -= ss->statScore / (10612 + 5171 * (depth > 6 && depth < 19));
+      r -= ss->statScore / (12970 + 2813 * (depth > 6 && depth < 19));
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
