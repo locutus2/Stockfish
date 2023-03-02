26426c7d7e3868ff5b1e1d177f48a54ce4226115
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-03 00:52:52 +0100

    No nnue hint at tt hit if tt move is king move. Bench: 4814343

--- src/search.cpp
+++ src/search.cpp
@@ -747,7 +747,7 @@ namespace {
         {
             complexity = abs(ss->staticEval - pos.psq_eg_stm());
 
-            if (PvNode || (ttValue <= alpha && tte->bound() & BOUND_UPPER))
+            if (PvNode && (!ttMove || type_of(pos.moved_piece(ttMove)) != KING))
                Eval::NNUE::hint_common_parent_position(pos);
         }
 
