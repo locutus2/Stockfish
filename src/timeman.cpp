6077f308d267298f46310ae27c3734f7490c5005
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-03 18:09:36 +0100

    Use 10% more time in the beginning. Bench: 4814343

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -80,8 +80,8 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   // game time for the current move, so also cap to 20% of available game time.
   if (limits.movestogo == 0)
   {
-      optScale = std::min(0.06456 + std::pow(ply + 3.0, 0.45) * 0.021, 0.2)
-                 * limits.time[us] / double(timeLeft)
+      optScale = std::min(0.014 + std::pow(ply + 3.0, 0.45) * 0.00386,
+                          0.2 * limits.time[us] / double(timeLeft))
                  * optExtra;
       maxScale = std::min(7.0, 4.0 + ply / 12.0);
   }
