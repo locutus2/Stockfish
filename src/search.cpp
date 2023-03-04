fa003ebeb23b784342fea8b19ad4361cb0f5fc88
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-04 12:01:00 +0100

    Check after 6 iterations of linrock tuner. I used tc 10+0.1 for tuning. This iteration also needed 94 evaluations like the last iteration. Bench: 4103823

--- src/search.cpp
+++ src/search.cpp
@@ -69,8 +69,8 @@ namespace {
   // Mid- and endgame weights for piece dependent reduction
   const int LmrWeight[2][6] = {
     //  Pawn, Knight, Bishop, Rook, Queen, King
-     {  -62,   -34,    -48,    69,    28,    58 }, // midgame
-     {  -34,   -25,     23,   -70,   -85,   -71 }  // endgame
+     {  -60,   -53,    -30,    68,    21,    40 }, // midgame
+     {  -57,   -25,     39,   -65,   -98,   -63 }  // endgame
   };
 
   // Reductions lookup table, initialized at startup
