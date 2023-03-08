9274c471a4f728be33193f8c519ddbd2dd7c70fc
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-08 07:09:32 +0100

    Tweak recently added ttvalue <= alpha condition. Instead of negative extension even reduce overall depth. Bench: 4752146

--- src/search.cpp
+++ src/search.cpp
@@ -1112,12 +1112,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
-              {
-                  if (PvNode)
-                      extension = -1;
-                  else
-                      depth--;
-              }
+                  depth--;
           }
 
           // Check extensions (~1 Elo)
