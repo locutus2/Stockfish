24f9d88ebea949cc43341ef56a29d54e4eef1f44
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-08 18:24:47 +0100

    Partial undo depth reduction (1 ply) at PV nodes if later a move with value > alpha found. Bench: 4752881

--- src/search.cpp
+++ src/search.cpp
@@ -908,7 +908,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3, depthOffset = -3;
+        depth -= 3, depthOffset = -1;
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
