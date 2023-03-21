66b9d19bf75b7fc24444b6946107aeb85eac02a3
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-21 22:13:48 +0100

    Allow double extension at PV nodes if captue and prior capture. Bench: 5287615

--- src/search.cpp
+++ src/search.cpp
@@ -1084,7 +1084,7 @@ moves_loop: // When in check, search starts here
                   singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
-                  if (  (!PvNode || ss->inCheck)
+                  if (  (!PvNode || (priorCapture && capture))
                       && value < singularBeta - 25
                       && ss->doubleExtensions <= 10)
                   {
