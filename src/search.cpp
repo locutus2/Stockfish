a031672766334501811ca263ce1bff1ae1273077
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-21 21:33:44 +0100

    Allow double extension at PV nodes if capture. Bench: 5395571

--- src/search.cpp
+++ src/search.cpp
@@ -1084,7 +1084,7 @@ moves_loop: // When in check, search starts here
                   singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
-                  if (  (!PvNode || givesCheck)
+                  if (  (!PvNode || capture)
                       && value < singularBeta - 25
                       && ss->doubleExtensions <= 10)
                   {
