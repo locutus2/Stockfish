badc06f689d5602273bb5f4e8d606b6a9fb38e18
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-22 07:27:57 +0100

    Bound double extensions by previous move count (so in side variations more double extensions allowed). Bench: 4550254

--- src/search.cpp
+++ src/search.cpp
@@ -1084,8 +1084,9 @@ moves_loop: // When in check, search starts here
                   singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
-                  if (   value < singularBeta - 25
-                      && ss->doubleExtensions <= (PvNode ? 0 : 10))
+                  if (  !PvNode
+                      && value < singularBeta - 25
+                      && ss->doubleExtensions <= (ss-1)->moveCount)
                   {
                       extension = 2;
                       depth += depth < 13;
