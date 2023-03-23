2f09038ae40d3ff56f9d8d347118f860ea026f0a
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-23 16:07:33 +0100

    Try a variant of this my other test after a discussion at repo: https://github.com/locutus2/Stockfish/commit/ad2ff287a2979ddb84a7d267ec9f992dbd9768e9#comments. Bench: 4628620

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,10 @@ moves_loop: // When in check, search starts here
               // that multiple moves fail high, and we can prune the whole subtree by returning
               // a soft bound.
               else if (singularBeta >= beta)
-                  return singularBeta;
+              {
+                  if (is_ok((ss-1)->currentMove))
+                      return singularBeta;
+              }
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
