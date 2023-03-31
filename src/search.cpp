b451f590b40bb5d25a51e7bcd0dbc7407585be08
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-03-31 22:35:22 +0200

    LMR capture condition tuning: only parameter A[6][1] but double effect. Bench: 4916537

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
-          r -= 1 - (!capture && thisThread->nodes & 1);
+          r--;
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
@@ -1184,7 +1184,7 @@ moves_loop: // When in check, search starts here
       // Decrease reduction if we move a threatened piece (~1 Elo)
       if (   depth > 9
           && (mp.threatenedPieces & from_sq(move)))
-          r--;
+          r -= 1 - 2 * capture;
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
