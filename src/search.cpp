01f764044aa76ca258b661c9f155af2b1a4c11fb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-01 10:20:37 +0200

    LMR improving condition tuning: only parameter A[1][1]. Bench: 4184949

--- src/search.cpp
+++ src/search.cpp
@@ -1162,7 +1162,7 @@ moves_loop: // When in check, search starts here
           r -= 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 7)
+      if ((ss-1)->moveCount > 7 && !improving)
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
