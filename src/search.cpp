1786c7c5cd75d696fb4ebf1aa397c6e3d8d83233
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-02 10:00:16 +0200

    LMR ply > depth condition tuning: only parameter A[6][0]. After 15K this parameter was already at 100%. Bench: 4241169

--- src/search.cpp
+++ src/search.cpp
@@ -1184,6 +1184,9 @@ moves_loop: // When in check, search starts here
       if (singularQuietLMR)
           r--;
 
+      if (ss->ply > depth)
+          r++;
+
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
