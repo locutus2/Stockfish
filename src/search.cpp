cd6a36bb302129cf3e02182402e53baf1277c6d7
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-05 09:45:32 +0200

    Try the nonsense update also in the non-LMR case but only if value <= alpha (signature changes on my big bench). Bench: 5044536

--- src/search.cpp
+++ src/search.cpp
@@ -1246,6 +1246,9 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
+
+          if (moveCount > 1 && newDepth >= depth && !capture && value <= alpha)
+              update_continuation_histories(ss, movedPiece, to_sq(move), -stat_bonus(newDepth));
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
