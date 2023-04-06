76713cac4116292f7534d5eb4d8cf2c41feb5ae8
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-06 10:36:47 +0200

    LMR rule tuning (after 50%): use for the more reduction rule only the strongest term A[3]. Bench: 4514670

--- src/search.cpp
+++ src/search.cpp
@@ -1189,6 +1189,9 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
           r--;
 
+      if (type_of(move) == PROMOTION)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
