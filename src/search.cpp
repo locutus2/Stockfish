76745a45d2d9c48168fe18d96ba394e41f335af8
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-07 09:14:26 +0200

    Shift optimism so that break point is at -30. But keep the asymptotic behavior the same. Bench: 5162499

--- src/search.cpp
+++ src/search.cpp
@@ -363,7 +363,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust optimism based on root move's previousScore
-              int contempt = us == BLACK ? 20 : 0;
+              int contempt = 30;
               int opt = 120 * (prev + contempt) / (std::abs(prev + contempt) + 161);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
