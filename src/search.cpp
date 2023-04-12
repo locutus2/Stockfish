bad2a5c37bcef8316a394d3315272ae368bfbd10
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-12 17:23:39 +0200

    LMR rule combination tuning: use A[8][10][...]. Bench: 3026057

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
       if (ss->ttPv)
-          r -= !PvNode + 2 * !likelyFailLow;
+          r -= 1 + ttCapture - 2 * likelyFailLow;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
