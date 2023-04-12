f5f41825a1114872da724422d8a7ec308ec1820b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-13 01:26:57 +0200

    LMR rule combination tuning: use A[11][7][...]. Bench: 3817851

--- src/search.cpp
+++ src/search.cpp
@@ -1181,7 +1181,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
-          r--;
+          r -= 2 - priorCapture;
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
