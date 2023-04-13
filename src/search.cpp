8bae2f438a7faeaeb716ea8d43762fae75c92a8c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-13 20:45:50 +0200

    New LMR rule tuning: use two strongest conditions A[25] and A[19]. Bench: 3741606

--- src/search.cpp
+++ src/search.cpp
@@ -1185,7 +1185,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
-          r += 1 + (type_of(movedPiece) == KING && !cutNode && ss->inCheck);
+          r += 1 + (type_of(movedPiece) == KING);
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
