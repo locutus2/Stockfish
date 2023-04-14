a4ba9d09e4b3129648dd2100e3bdd820df01443c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-14 07:41:23 +0200

    LMR rule combination tuning: use A[1][1][...]. Bench: 6359745

--- src/search.cpp
+++ src/search.cpp
@@ -1169,10 +1169,12 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2;
+          r += 3;
+      else
+          r--;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture && !priorCapture)
+      if (ttCapture)
           r++;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
