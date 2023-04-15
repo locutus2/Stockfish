0842ad16c95156a81172e7c1d8d9d19e90d8bd24
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-15 13:05:25 +0200

    New LMR rule combination tuning: use A[6][4][...]. Bench: 3643356

--- src/search.cpp
+++ src/search.cpp
@@ -1187,7 +1187,7 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      if (ss->inCheck && priorCapture)
+      if (givesCheck && improving)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
