f729dd1388ccfc39b71a0213a01a430ae0b2ff25
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-15 10:56:34 +0200

    New LMR rule combination tuning: use A[1][3][...]. Bench: 3920411

--- src/search.cpp
+++ src/search.cpp
@@ -1171,6 +1171,9 @@ moves_loop: // When in check, search starts here
       if (cutNode)
           r += 2;
 
+      else if (type_of(move) == PROMOTION)
+          r++;
+
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
           r++;
@@ -1186,9 +1189,6 @@ moves_loop: // When in check, search starts here
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
-
-      if (ss->inCheck)
-          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
