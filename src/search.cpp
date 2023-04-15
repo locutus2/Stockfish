fad5539f8bc97e64745bb002d0711e5883346480
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-15 16:12:13 +0200

    New LMR rule combination tuning: use A[10][9][...]. Bench: 4448166

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
           r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
+      if (ttCapture && likelyFailLow)
           r++;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
@@ -1186,9 +1186,6 @@ moves_loop: // When in check, search starts here
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
-
-      if (givesCheck && improving)
-          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
