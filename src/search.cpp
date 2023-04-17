1ee8e7b14e43c21ad9083fce74c7f562ef29f17b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-17 20:51:56 +0200

    Less reduction at root if best value <= previous best score. Bench: 4058157

--- src/search.cpp
+++ src/search.cpp
@@ -1157,6 +1157,9 @@ moves_loop: // When in check, search starts here
       // Step 16. Make the move
       pos.do_move(move, st, givesCheck);
 
+      if (rootNode && thisThread->pvIdx == 0 && bestValue <= Threads.main()->bestPreviousScore)
+          r--;
+
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
       if (   ss->ttPv
