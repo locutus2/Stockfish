6d6275e6322b797663b562229a0e22dddbf2f851
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-18 07:07:12 +0200

    New LMR rule combination tuning: use A[9][0][...]. Bench: 4062339

--- src/search.cpp
+++ src/search.cpp
@@ -1177,7 +1177,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
-          r -= 1 + 12 / (3 + depth);
+          r -= 1 + 12 / (3 + depth) + likelyFailLow;
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
@@ -1186,9 +1186,6 @@ moves_loop: // When in check, search starts here
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
-
-      if (ss->inCheck && !improving)
-          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
