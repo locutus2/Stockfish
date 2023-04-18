84b59337c45cc0a6c2fb8557db6f29afb7d6f6b4
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-18 17:55:59 +0200

    Double effect: New LMR rule combination tuning: use A[9][4][...]. Bench: 3875419

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2 - (ss->ttPv && ss->inCheck);
+          r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
@@ -1186,6 +1186,9 @@ moves_loop: // When in check, search starts here
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
           r++;
+
+      if (likelyFailLow && !improving)
+          r -= 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
