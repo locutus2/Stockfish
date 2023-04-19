e7a566158fce104f17068cd4c5a9439424d3d44f
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-19 10:19:59 +0200

    If found best move do less reduction for the alternative moves of the same target square. Bench: 4156563

--- src/search.cpp
+++ src/search.cpp
@@ -1187,7 +1187,7 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      if (PvNode && bestMove && from_sq(move) == from_sq(bestMove))
+      if (PvNode && bestMove && to_sq(move) == to_sq(bestMove))
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
