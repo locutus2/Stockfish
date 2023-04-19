e3649848290002b25f4da77e550286b74fe25eff
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-19 07:30:48 +0200

    New LMR rule combination tuning: combine A[6][4][...] and A[10][9][...]. Bench: 3969307

--- src/search.cpp
+++ src/search.cpp
@@ -1187,8 +1187,8 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      if (likelyFailLow && !improving)
-          r -= 2;
+      if (ttCapture && !likelyFailLow && improving && givesCheck)
+          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
