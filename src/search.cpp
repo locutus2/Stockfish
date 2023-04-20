29809066f3e1f91b661db22108b1e8d9c7726cc6
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-20 14:33:41 +0200

    Second LMR rule combination tuning: use A[2][11][...]. Bench: 4405318

--- src/search.cpp
+++ src/search.cpp
@@ -1187,8 +1187,8 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      if (move == ttMove && (ss-1)->ttPv)
-          r--;
+      if (move == ttMove && more_than_one(pos.checkers()))
+          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
