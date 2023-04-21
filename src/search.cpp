591515f0947d1e88f1cd73962ece8b6c2ac28471
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-21 23:41:44 +0200

    Less stat score reduction if priorCapture (factor 6050). Test against my passed tuned_lmr_rule6_PR. Bench: 3844762

--- src/search.cpp
+++ src/search.cpp
@@ -1197,7 +1197,7 @@ moves_loop: // When in check, search starts here
                      - 4082;
 
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
-      r -= (ss->statScore + 4588 * improving) / (11079 + 4626 * (depth > 6 && depth < 19));
+      r -= (ss->statScore + 6050 * priorCapture) / (11079 + 4626 * (depth > 6 && depth < 19));
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
