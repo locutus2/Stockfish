f9fe2bcbd6365f3484c2082877a066588177eff2
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-23 00:42:42 +0200

    No pruning for captures if in check. Bench: 4088498

--- src/search.cpp
+++ src/search.cpp
@@ -976,6 +976,7 @@ moves_loop: // When in check, search starts here
       // Step 14. Pruning at shallow depth (~120 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
           && pos.non_pawn_material(us)
+          && !(ss->inCheck && capture)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
