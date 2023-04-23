6eca5324096b662d3906234e90fcf9f7461461b3
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-23 09:16:46 +0200

    Extend pawn push which was unblocked by own previous move. Bench: 4059173

--- src/search.cpp
+++ src/search.cpp
@@ -1120,6 +1120,11 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
               extension = 1;
+
+          else if (   type_of(movedPiece) == PAWN
+                   && is_ok((ss-2)->currentMove)
+                   && to_sq(move) == from_sq((ss-2)->currentMove))
+              extension = 1;
       }
 
       // Add extension to new depth
