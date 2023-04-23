5a979b5a06812839c36051d9c8d2ca637a67060c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-23 15:08:50 +0200

    Delta update: delta += delta / 4 + 1. Bench: 3579495

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 3;
+              delta += delta / 4 + 1;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
