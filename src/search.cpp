a7d2b9f489d826c2b6fdc41230517023edda3dce
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-24 09:53:10 +0200

    New extended LMR condition tuning: use A[0][4][0]. Bench: 2324859

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
-      if (!(thisThread->nodes & 0xFF) && (!PvNode || !ss->inCheck))
+      if (!PvNode || !ss->inCheck)
           r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
