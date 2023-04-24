43f49cb4d3ae74752d7e328d615c833014e2e273
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-24 09:23:00 +0200

    New extended LMR condition tuning: use A[0][4][0] but only randomly with p=1/256 because the last tuning uses at each node only one condition which is a chance of 1/320.  Bench: 3735825

--- src/search.cpp
+++ src/search.cpp
@@ -1172,6 +1172,9 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (!(thisThread->nodes & 0xFF) && (!PvNode || !ss->inCheck))
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
