8226bc293a239701470cee75263bd77dcac9c0fb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-25 13:20:39 +0200

    Use difference of current and previous best value in delta update (ratio delta:diff = 3:1): delta += delta * 3 / 16 + std::abs(bestValue - prev) / 28 + 2. Bench: 3351881

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 6 + std::abs(bestValue - prev) / 21 + 2;
+              delta += delta * 3 / 16 + std::abs(bestValue - prev) / 28 + 2;
               prev = bestValue;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
