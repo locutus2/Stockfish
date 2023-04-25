831694f708e72a24b710933bf1a9850abd3bbeb9
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-25 18:49:24 +0200

    Further new extended LMR condition tuning: use A[3][1][2]. Bench: 3668002

--- src/search.cpp
+++ src/search.cpp
@@ -1151,7 +1151,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2;
+          r += 1 + improving;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
@@ -1170,9 +1170,6 @@ moves_loop: // When in check, search starts here
           r++;
 
       else if (move == ttMove)
-          r--;
-
-      if (priorCapture != givesCheck)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
