428267279437bcee810f30960493aba9d1feb754
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-04-26 20:59:04 +0200

    Further new extended LMR condition tuning: use A[7][5][0]. Bench: 3673392

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
-      if (move == ttMove && capture)
+      if (move == ttMove && givesCheck)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
