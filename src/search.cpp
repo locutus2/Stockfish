b3b3421696884c6abbe258d9c2f4506ea7de50f8
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-01 06:50:40 +0200

    More reductiona after null move for quiet moves (!= ttMove). Bench: 3500018

--- src/search.cpp
+++ src/search.cpp
@@ -1172,6 +1172,13 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (   (ss-1)->currentMove == MOVE_NULL
+          && !capture
+          && !givesCheck
+          && move != ttMove
+          && type_of(move) == NORMAL)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
