7bc5a57575c17d6923b8a2995e311e058358b21f
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-02 11:05:28 +0200

    LMR condition tuning for pawn moves. Use A[7][9][2]=-97.50. Bench: 3726791

--- src/search.cpp
+++ src/search.cpp
@@ -1173,8 +1173,8 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (   type_of(movedPiece) == PAWN
-          && !capture
-          && (ss-1)->currentMove != MOVE_NULL)
+          && (ss-1)->currentMove == MOVE_NULL
+          && (ss-1)->moveCount <= 7)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
