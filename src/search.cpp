502e1e52ee33d81b7c377b7cc09ce92a4bb8cf54
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-03 12:34:09 +0200

    LMR condition tuning for tt moves. Use A[4][11][0]=49.40. Bench: 3492794

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,12 @@ moves_loop: // When in check, search starts here
           r++;
 
       else if (move == ttMove)
-          r -= 1 + cutNode;
+          r--;
+
+      if (   move == ttMove
+          && ss->inCheck
+          && ttCapture)
+          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
