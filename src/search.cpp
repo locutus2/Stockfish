b7968d1e9c4d0ec2a1a73d2d5cf578ee5c48efbb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-03 22:03:43 +0200

    Count node as likelyFailLow also if non-PV. Bench: 3650744

--- src/search.cpp
+++ src/search.cpp
@@ -926,11 +926,9 @@ moves_loop: // When in check, search starts here
 
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal or greater than the current depth, and the result of this search was a fail low.
-    bool likelyFailLow =    PvNode
-                         && ttMove
+    bool likelyFailLow =    ttMove
                          && (tte->bound() & BOUND_UPPER)
-                         && tte->depth() >= depth
-                         && ttValue <= alpha;
+                         && tte->depth() >= depth;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
