def05881a5f4d2ff13312c1e86241464591be1a7
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-03 21:03:02 +0200

    First test after 66K of extension condition tuning. Use A[10][12][0]=-100.00. Bench: 2176637

--- src/search.cpp
+++ src/search.cpp
@@ -1120,6 +1120,9 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
               extension = 1;
+
+          if (likelyFailLow && move == ttMove)
+              extension--;
       }
 
       // Add extension to new depth
