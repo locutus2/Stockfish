b0cbf53699775f60d85937bed3c564e7b724ec85
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-05 19:16:12 +0200

    Double effect. LMR condition tuning for pawn moves. Use A[1][9][0]=-91.30. Bench: 3429084

--- src/search.cpp
+++ src/search.cpp
@@ -1174,9 +1174,9 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (   type_of(movedPiece) == PAWN
-          && likelyFailLow
-          && type_of(move) != PROMOTION)
-          r++;
+          && (ss-1)->currentMove == MOVE_NULL
+          && cutNode)
+          r -= 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
