cb945cd6ff2e866663e560746576e212b503b201
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-05 14:07:39 +0200

    LMR condition tuning for king moves. Use A[0][5][3]=83.50. Bench: 3188494

--- src/search.cpp
+++ src/search.cpp
@@ -1174,8 +1174,8 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (   type_of(movedPiece) == KING
-          && move == ttMove
-          && !cutNode)
+          && !PvNode
+          && !givesCheck)
           r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
