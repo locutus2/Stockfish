1684bcaa6a3ef74443eeabf59fd5f503540bd496
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-05 19:13:25 +0200

    Double effect. LMR condition tuning for king moves. Use A[4][12][0]=96.70. Bench: 3439495

--- src/search.cpp
+++ src/search.cpp
@@ -1174,9 +1174,9 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (   type_of(movedPiece) == KING
-          && !cutNode
-          && !ttCapture)
-          r--;
+          && ss->inCheck
+          && move == ttMove)
+          r += 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
