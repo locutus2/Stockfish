b86394b42f9da3538d6f952398b9f941f3716e15
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-05 04:47:26 +0200

    Extension condition tuning. Use A[7][8][1]=98.60. Bench: 7430353

--- src/search.cpp
+++ src/search.cpp
@@ -1121,9 +1121,9 @@ moves_loop: // When in check, search starts here
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
               extension = 1;
 
-          else if (   !givesCheck
-                   && !priorCapture)
-              extension = -1;
+          else if (   (ss-1)->moveCount > 7
+                   && (ss+1)->cutoffCnt <= 3)
+              extension = 1;
       }
 
       // Add extension to new depth
