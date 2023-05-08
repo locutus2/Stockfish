26c89099b1ab5461ab9d3f46ee12c3784ac16da6
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-08 08:57:48 +0200

    LMR tuning after null moves. Use A[1][4][1]=-91.70 (obmit !ss->incheck condition because its always true after null move). Bench: 3694540

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2;
+          r += 2 - ((ss-1)->currentMove == MOVE_NULL);
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
@@ -1169,11 +1169,6 @@ moves_loop: // When in check, search starts here
 
       else if (move == ttMove)
           r--;
-
-      if (   (ss-1)->currentMove == MOVE_NULL
-          && cutNode
-          && !priorCapture)
-          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
