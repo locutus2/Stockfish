dd4e694b8e514c51de665a45a2a1a2c803bc1f23
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-09 16:03:13 +0200

    LMR tuning after null moves. Use A[4][12][3]=-76.90. Bench: 3801317

--- src/search.cpp
+++ src/search.cpp
@@ -1171,9 +1171,9 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (   (ss-1)->currentMove == MOVE_NULL
-          && type_of(move) == PROMOTION
-          && !more_than_one(pos.checkers()))
-          r++;
+          && !ss->inCheck
+          && move != ttMove)
+          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
