a0d21d33837888731d580d059e2fbd8f0212f5cb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-10 23:36:32 +0200

    Use previous own tt move as further refutation in quiet move ordering if ply < depth at PV node. Bench: 3884945

--- src/search.cpp
+++ src/search.cpp
@@ -917,7 +917,7 @@ moves_loop: // When in check, search starts here
                                           contHist,
                                           countermove,
                                           ss->killers,
-                                          ss->ttPv ? (ss-2)->ttMove : MOVE_NONE);
+                                          PvNode && ss->ply < depth ? (ss-2)->ttMove : MOVE_NONE);
 
     value = bestValue;
     moveCountPruning = singularQuietLMR = false;
