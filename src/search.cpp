ba02eaae8efeb1540c52c55f2cb10cbf05b261f1
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-12 23:41:44 +0200

    Inspired by my pruning condition tuning do more pruning if move is not a counter or killer. Bench: 3430701

--- src/search.cpp
+++ src/search.cpp
@@ -979,7 +979,9 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - r, 0);
+          int lmrDepth = std::max(newDepth - r - (   move != countermove
+                                                  && move != ss->killers[0]
+                                                  && move != ss->killers[1]), 0);
 
           if (   capture
               || givesCheck)
