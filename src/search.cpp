e991774e85a5d5a6cb62318dceec4712260808d0
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-13 12:42:53 +0200

    This is a variant of my first try for handling search explosion. Here i merge the two conditions together by simply adding up the parts. Bench: 3936482

--- src/search.cpp
+++ src/search.cpp
@@ -1046,8 +1046,7 @@ moves_loop: // When in check, search starts here
 
       // Step 15. Extensions (~100 Elo)
       // We take care to not overdo to avoid search getting stuck.
-      if (       ss->ply              < 2 * thisThread->rootDepth
-          && 2 * ss->doubleExtensions <     thisThread->rootDepth)
+      if (ss->ply + 2 * ss->doubleExtensions < 3 * thisThread->rootDepth)
       {
           // Singular extension search (~94 Elo). If all moves but one fail low on a
           // search of (alpha-s, beta-s), and just one fails high on (alpha, beta),
