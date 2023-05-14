b419a92c52f9289d03ed4364bfbf10e5b17ea32a
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-15 01:20:11 +0200

    Move order tuning. Use A[1][3][0]=-66.00. Bench: 4004795

--- src/search.cpp
+++ src/search.cpp
@@ -917,7 +917,7 @@ moves_loop: // When in check, search starts here
                                       contHist,
                                       countermove,
                                       ss->killers,
-                                      (ss-1)->moveCount > 7 && pos.count<PAWN>() < 10 ? 16384 : 0);
+                                      cutNode && priorCapture ? 16384 : 0);
 
     value = bestValue;
     moveCountPruning = singularQuietLMR = false;
