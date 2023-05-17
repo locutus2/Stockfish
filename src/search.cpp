462ecf907d522323c9f42fa3c6ed17cd625d76ff
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-17 06:53:42 +0200

    Decrease history pruning depth limit if distance to PV < 4. Test on top of PR 4566 + 4571. Bench: 3552632

--- src/search.cpp
+++ src/search.cpp
@@ -1024,7 +1024,7 @@ moves_loop: // When in check, search starts here
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
               // Continuation history based pruning (~2 Elo)
-              if (   lmrDepth < 5 + (ss->pvDistance >= 3)
+              if (   lmrDepth < 5 + (ss->pvDistance >= 4)
                   && history < -4405 * depth)
                   continue;
 
