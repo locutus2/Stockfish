77024d8d691a1164fad5c970055857283aaef6f4
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-17 22:03:01 +0200

    Do less move sorting if distance to PV increases (factor 2000). But compensate by using depth factor -6218. Do not increase PV distance if first move played. Bench: 3249108

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -229,7 +229,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -4609 * depth + 1000 * pvDistance);
+          partial_insertion_sort(cur, endMoves, -6218 * depth + 2000 * pvDistance);
       }
 
       ++stage;
