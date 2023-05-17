663641109a1b7e487af09c8f7b69a6a5b8b497e0
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-17 13:27:30 +0200

    Do less move sorting if distance to PV increases (factor 100). Bench: 3619030

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -229,7 +229,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * (depth - pvDistance));
+          partial_insertion_sort(cur, endMoves, -3000 * depth + 100 * pvDistance);
       }
 
       ++stage;
