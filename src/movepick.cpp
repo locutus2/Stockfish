b40253eac12a65b78e0406e55c12da79b5b2ac89
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-19 23:31:18 +0200

    First test after 93K of my second PV distance quiet order tuning. Bench: 3585152

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -229,7 +229,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -2904 * depth - 238 * pvDistance - 390);
+          partial_insertion_sort(cur, endMoves, -3077 * depth - 48 * pvDistance - 404);
       }
 
       ++stage;
