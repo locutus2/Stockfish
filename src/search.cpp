d6f9c6fd481d30b6f2ff0ae85ba1947e5561c862
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-21 11:01:13 +0200

    Beta update after fail low: (bestValue + alpha + beta) / 3. Bench: 2924395

--- src/search.cpp
+++ src/search.cpp
@@ -394,7 +394,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (bestValue + beta) / 2;
+                  beta = (bestValue + alpha + beta) / 3;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
