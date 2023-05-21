3878410a8f0dae259ea9fd510ee1773f831f98bb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-21 10:19:47 +0200

    After fail low decrease beta more by using average of best value and beta. Bench: 3103048

--- src/search.cpp
+++ src/search.cpp
@@ -394,7 +394,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  beta = (bestValue + beta) / 2;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
