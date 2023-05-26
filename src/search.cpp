4356ca177a99d0fed8447ee289643d335a8df260
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-26 08:20:38 +0200

    Do more reduction if (ss+1)->cutoffCnt > 250. Bench: 2403828

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
-          r += 1 + ((ss+1)->cutoffCnt > 350);
+          r += 1 + ((ss+1)->cutoffCnt > 250);
 
       else if (move == ttMove)
           r--;
