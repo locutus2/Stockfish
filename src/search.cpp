4773a99a94dbc2e391ef980faaa495df24e44937
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-05-26 09:20:00 +0200

    More reduction if (ss+2)->cutoffCnt > 4. Bench: 2131153

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
-      if ((ss+2)->cutoffCnt > 2)
+      if ((ss+2)->cutoffCnt > 4)
           r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
