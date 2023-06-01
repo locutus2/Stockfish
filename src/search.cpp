ea25a5336286052dc622cf27781793a6a78d2bc6
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-01 21:05:26 +0200

    At root use bigger depth for PV search if not ttMove. Bench: 2670807

--- src/search.cpp
+++ src/search.cpp
@@ -1233,6 +1233,9 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
+          if (rootNode && move != ttMove)
+              newDepth++;
+
           value = -search<PV>(pos, ss+1, -beta, -alpha, newDepth, false);
       }
 
