e784a98806fbc8bfa9e94a96894826cf4857e17e
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-02 22:17:08 +0200

    Use in refutations counter move before second killer. Bench: 2570358

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -63,7 +63,7 @@ MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHist
                                                              Move cm,
                                                              const Move* killers)
            : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch),
-             ttMove(ttm), refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}}, depth(d)
+             ttMove(ttm), refutations{{killers[0], 0}, {cm, 0}, {killers[1], 0}}, depth(d)
 {
   assert(d > 0);
 
@@ -206,9 +206,11 @@ top:
       endMoves = std::end(refutations);
 
       // If the countermove is the same as a killer, skip it
-      if (   refutations[0].move == refutations[2].move
-          || refutations[1].move == refutations[2].move)
+      if (refutations[1].move == refutations[2].move)
           --endMoves;
+
+      else if (refutations[0].move == refutations[1].move)
+          refutations[1].move = MOVE_NONE;
 
       ++stage;
       [[fallthrough]];
