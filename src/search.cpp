86118d852cbc9b1378b09de2cdf124fea5e238be
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-03 14:24:54 +0200

    Less reduction at root if best value < previous best score. Bench: 2561192

--- src/search.cpp
+++ src/search.cpp
@@ -1154,6 +1154,9 @@ moves_loop: // When in check, search starts here
       if (PvNode)
           r -= 1 + 11 / (3 + depth);
 
+      if (rootNode && bestValue < Threads.main()->bestPreviousScore)
+          r--;
+
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
           r--;
