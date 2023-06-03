c7f8691771fcae95d065c15bf4007f8331e22fce
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-03 08:17:01 +0200

    Use bigger depth for PV search if negative extension. Bench: 2339289

--- src/search.cpp
+++ src/search.cpp
@@ -1233,10 +1233,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          if (depth < ss->ply && move != ttMove)
-              newDepth++;
-
-          value = -search<PV>(pos, ss+1, -beta, -alpha, newDepth, false);
+          value = -search<PV>(pos, ss+1, -beta, -alpha, newDepth + (extension < 0), false);
       }
 
       // Step 19. Undo move
