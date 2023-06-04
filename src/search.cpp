fba87220adeef43d742839c1e16425c60838478a
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-04 13:05:01 +0200

    Less reduction at non-cut nodes if tt move. Bench: 2530710

--- src/search.cpp
+++ src/search.cpp
@@ -1144,7 +1144,10 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 1 + (moveCount > 1);
+          r += 2;
+
+      else if (move == ttMove)
+          r--;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
