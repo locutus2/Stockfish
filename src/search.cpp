fc0200955fc9d26c62d2d2f6d08c8ccb4bb42702
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-04 15:59:03 +0200

    Tweak stat score. Bench: 2450352

--- src/search.cpp
+++ src/search.cpp
@@ -1165,10 +1165,11 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
-      ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
-                     + (*contHist[0])[movedPiece][to_sq(move)]
-                     + (*contHist[1])[movedPiece][to_sq(move)]
-                     + (*contHist[3])[movedPiece][to_sq(move)]
+      ss->statScore =  3 * thisThread->mainHistory[us][from_to(move)]
+                     + 2 * (*contHist[0])[movedPiece][to_sq(move)]
+                     +     (*contHist[1])[movedPiece][to_sq(move)]
+                     +     (*contHist[3])[movedPiece][to_sq(move)]
+                     +     (*contHist[5])[movedPiece][to_sq(move)]
                      - 3755;
 
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
