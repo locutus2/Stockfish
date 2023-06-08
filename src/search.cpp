43b54cde52c47c540944eeedd30aeb883bba3367
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-09 00:31:37 +0200

    Collect at root how often a move was the best move (weight with depth and age, init with main and capture history) and use only this for root move ordering (except tt move will be searched always first). Use factor 1024. Bench: 2582027

--- src/search.cpp
+++ src/search.cpp
@@ -313,7 +313,8 @@ void Thread::search() {
   int searchAgainCounter = 0;
 
   for(RootMove& rm : rootMoves)
-      rm.countBestMove = mainHistory[us][from_to(rm.pv[0])];
+      rm.countBestMove = rootPos.capture(rm.pv[0]) ? captureHistory[rootPos.moved_piece(rm.pv[0])][to_sq(rm.pv[0])][type_of(rootPos.piece_on(to_sq(rm.pv[0])))]
+                                                   : mainHistory[us][from_to(rm.pv[0])];
 
   // Iterative deepening loop until requested to stop or the target depth is reached
   while (   ++rootDepth < MAX_PLY
