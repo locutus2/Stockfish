6de1ad7bd9170bccfcc748e2ba81b135b0f5387d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-11 22:13:34 +0200

    Test second new LMR tuning after 30K. More reduction if 7 best conditions hold. Bench: 2803266

--- src/search.cpp
+++ src/search.cpp
@@ -1174,8 +1174,7 @@ moves_loop: // When in check, search starts here
           && ttCapture
           && move == ss->killers[1]
           && (ss-1)->ttPv
-          && likelyFailLow
-          && priorCapture)
+          && likelyFailLow)
           r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
