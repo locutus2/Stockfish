3b8c8db9d5c277f39d63681f88f774cf0bbc74d8
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-12 20:09:34 +0200

    Test second new LMR tuning after 120K. More reduction if 6 best conditions hold (skipping conditions which do not change my big bench). Bench: 2716791

--- src/search.cpp
+++ src/search.cpp
@@ -1173,8 +1173,7 @@ moves_loop: // When in check, search starts here
           && move == countermove
           && likelyFailLow
           && move == ss->killers[0]
-          && (ss+1)->cutoffCnt > 3
-          && priorCapture)
+          && (ss+1)->cutoffCnt > 3)
           r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
