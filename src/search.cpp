b69762ff771a0eaeda1abfb8c5a0a57f8749f403
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-13 16:59:14 +0200

    Test first new LMR tuning final values. More reduction if 3 best conditions hold (skipping conditions which do not change my big bench). Bench: 2440247

--- src/search.cpp
+++ src/search.cpp
@@ -1171,6 +1171,11 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (   !improving
+          && type_of(move) == PROMOTION
+          && givesCheck)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
