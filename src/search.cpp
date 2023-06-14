28464026b87d7d1ecceb80aa3412c684251206f0
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-14 17:20:15 +0200

    Test third new LMR tuning after 61K. Less reduction if 3 best conditions hold. Bench: 2370027

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,8 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (   move == ttMove
-          && move == ss->killers[1])
+          && move == ss->killers[1]
+          && type_of(move) == PROMOTION)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
