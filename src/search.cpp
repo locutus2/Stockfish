81e8be1be4f3a4f2e562287527eb379de96783e2
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-17 22:56:34 +0200

    Test third new LMR tuning final values. Less reduction if 3 best conditions hold. Bench: 2370027

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
-          r -= 1 + 12 / (3 + depth) + (type_of(move) == PROMOTION);
+          r -= 1 + 12 / (3 + depth) + (type_of(move) == PROMOTION && capture);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
