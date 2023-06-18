2021749f2999295e793253e4f71347000ab6ab59
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-18 13:00:41 +0200

    Test second new prune tuning after 28K. No history pruning if best conditions hold. Bench: 2448123

--- src/search.cpp
+++ src/search.cpp
@@ -1021,7 +1021,8 @@ moves_loop: // When in check, search starts here
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 6
-                  && history < -3832 * depth)
+                  && history < -3832 * depth
+                  && move != (ss-2)->killers[1])
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
