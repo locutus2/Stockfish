1c7d12d133ade52e21bc4317bdd99dff73f73a82
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-20 20:15:37 +0200

    Test second new prune tuning final values. No history pruning if 3 best conditions hold. Bench: 2696297

--- src/search.cpp
+++ src/search.cpp
@@ -1023,8 +1023,8 @@ moves_loop: // When in check, search starts here
               if (   lmrDepth < 6
                   && history < -3832 * depth
                   && (   (*contHist[1])[movedPiece][to_sq(move)] <= 0
-                      || (ss-1)->ttPv
-                      || ss->staticEval < beta))
+                      || ss->staticEval < beta
+                      || move != (ss-2)->killers[1]))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
