ff21405ab879cb8bf8430400d8ce0329902d5a73
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-20 20:01:34 +0200

    Test first new prune tuning final values. More history pruning for lmrDepth < 1 if 3 best conditions hold. Bench: 2943292

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,9 @@ moves_loop: // When in check, search starts here
                   continue;
 
               if (   lmrDepth < 1
-                  && (ss-1)->moveCount != 1)
+                  && !(ss->ply & 1)
+                  && (ss-1)->moveCount != 1
+                  && to_sq(move) == to_sq(ss->killers[1]))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
