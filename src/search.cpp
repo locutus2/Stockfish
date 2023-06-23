a2bd7f2eaa2ecb33e73654f914c2be4546f23a62
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-23 09:32:56 +0200

    Double effect of yellow LTC. Test second new LMR tuning after 120K. More reduction if 5 best conditions hold (skipping conditions which do not change my big bench). bench: 2696865

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
           && move == ss->killers[0]
           && likelyFailLow
           && improving)
-          r++;
+          r += 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
