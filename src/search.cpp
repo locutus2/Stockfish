1e654a15519ab5db0e763fd928c0a008d5f4036e
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-28 12:30:35 +0200

    Double effect: Test fourth LMR condition tuning after 290K. Two plies more reduction if 5 best conditions hold. Bench: 2593605

--- src/search.cpp
+++ src/search.cpp
@@ -1177,7 +1177,7 @@ moves_loop: // When in check, search starts here
          && priorCapture
          && ss->cutoffCnt > 3
          && type_of(movedPiece) != KING)
-          r++;
+          r += 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
