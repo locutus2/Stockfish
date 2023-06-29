e801003790febcbd29bb39db7faa09b5410a40f6
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-06-29 17:17:51 +0200

    Test fourth LMR condition tuning after 400+70K. More reduction if 5 best conditions hold. Bench: 2649083

--- src/search.cpp
+++ src/search.cpp
@@ -1170,6 +1170,13 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (   move == countermove
+          && move != ttMove
+          && !excludedMove
+          && !(ss-1)->excludedMove
+          && type_of(move) != PROMOTION)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
