08d2b319f0921714c9a78e8174b3cb787d303d36
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-07-02 14:55:55 +0200

    Less reduction for early checks (move count <= 9). Bench: 2614551

--- src/search.cpp
+++ src/search.cpp
@@ -1174,6 +1174,9 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (givesCheck && moveCount <= 9)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
