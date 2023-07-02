9393982874343237cda0124325198d594e53cf7f
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-07-02 23:08:18 +0200

    Two plies less reduction for captures if move count = 9. Bench: 2664966

--- src/search.cpp
+++ src/search.cpp
@@ -1175,7 +1175,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (moveCount == 9 && capture)
-          r--;
+          r -= 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
