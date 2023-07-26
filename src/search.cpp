219165a753f83887814af647a784cc4ff3cff332
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-07-26 09:44:34 +0200

    In NMP search use for reduction depth/4 if prior capture and ttPv node. Bench: 1543786

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 173, 6) + depth / 3 + 4 - (priorCapture && ss->ttPv && depth < 12);
+        Depth R = std::min(int(eval - beta) / 173, 6) + depth / (3 + (priorCapture && ss->ttPv)) + 4;
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
