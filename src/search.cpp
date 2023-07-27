6fca57fe63a631155ce0d566959a38c8d733a8b1
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-07-27 21:38:52 +0200

    If (ss-2)->currentMove is also a null move then ignore all stat score and eval conditions in NMP. Bench: 1570948

--- src/search.cpp
+++ src/search.cpp
@@ -775,11 +775,11 @@ namespace {
     // Step 9. Null move search with verification search (~35 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-2)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 17329
-        &&  eval >= beta
-        &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 21 * depth + 258
+        && (   (ss-2)->currentMove == MOVE_NULL
+            || (   (ss-1)->statScore < 17329
+                &&  eval >= beta
+                &&  eval >= ss->staticEval
+                &&  ss->staticEval >= beta - 21 * depth + 258))
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
