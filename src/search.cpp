c6e87c7851c877311fbe004ab657554584c598d8
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-07-31 18:43:45 +0200

    In NMP search use for reduction always depth*5/16. Bench: 1757711

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 173, 6) + depth * 2 / 7 + 4;
+        Depth R = std::min(int(eval - beta) / 173, 6) + depth * 5 / 16 + 4;
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
