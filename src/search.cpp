19a717fe50565cffe59e4283c59ee816984dbfc9
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-05 20:26:51 +0200

    Allow futility pruning als for depth = 9 if eval is far enoughe above beta (threshold 940) Bench: 1653994

--- src/search.cpp
+++ src/search.cpp
@@ -766,10 +766,9 @@ namespace {
     // Step 8. Futility pruning: child node (~40 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 9
+        &&  depth < 9 + (eval - beta > 940)
         &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
-        && (!excludedMove || depth > 3)
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
 
