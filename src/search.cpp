5ced111eda95ebc947cb5f99698494483178c50f
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-05 06:13:04 +0200

    At non-cut nodes do no futility pruning if depth >= 8. Bench: 1640268

--- src/search.cpp
+++ src/search.cpp
@@ -766,8 +766,7 @@ namespace {
     // Step 8. Futility pruning: child node (~40 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 9
-        && (cutNode || depth > 1)
+        &&  depth < 8 + cutNode
         &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
