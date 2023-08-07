076e71681c07042f9f9fc465e25e2140ce7f0a07
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-07 08:23:25 +0200

    Use singularQuietLMR only if singular move gives not check. Bench: 1545679

--- src/search.cpp
+++ src/search.cpp
@@ -1073,7 +1073,7 @@ moves_loop: // When in check, search starts here
               if (value < singularBeta)
               {
                   extension = 1;
-                  singularQuietLMR = !ttCapture;
+                  singularQuietLMR = !ttCapture && !givesCheck;
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
