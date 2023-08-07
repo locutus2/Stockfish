e4a77a7eb77a4eb97e86416f757c1c8bee392243
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-07 10:59:51 +0200

    In singular search do randomly no futility pruning if depth = 1. Bench: 1473891

--- src/search.cpp
+++ src/search.cpp
@@ -766,9 +766,10 @@ namespace {
     // Step 8. Futility pruning: child node (~40 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 8 + (cutNode || thisThread->nodes & 1)
+        &&  depth < 9
         &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
+        && (!excludedMove || depth > 1 || thisThread->nodes & 1)
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
 
