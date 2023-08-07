8d5da00f94c55d0f79c0722fe3310cb619154dfe
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-07 08:05:05 +0200

    Combine two if my futility depth tweaks which failed yellow: 1. At non-cut nodes do no futility pruning if depth >= 8 (https://tests.stockfishchess.org/tests/view/64cdcc5c5b17f7c21c0d764c) 2. In singular search do no futility pruning if depth = 1 (https://tests.stockfishchess.org/tests/view/64cdfe7b5b17f7c21c0d7b39) Bench: 1494347

--- src/search.cpp
+++ src/search.cpp
@@ -766,10 +766,10 @@ namespace {
     // Step 8. Futility pruning: child node (~40 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 9
+        &&  depth < 8 + cutNode
         &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
-        && (ss->cutoffCnt >= 1 || depth > 1)
+        && (!excludedMove || depth > 1)
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
 
