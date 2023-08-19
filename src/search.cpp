deb436255ed35b31bee06adfe6292dcbc739cd38
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-19 23:17:31 +0200

    Allow check extension only for tt move if ttCapture. Bench: 1488853

--- src/search.cpp
+++ src/search.cpp
@@ -1089,7 +1089,8 @@ moves_loop: // When in check, search starts here
 
           // Check extensions (~1 Elo)
           else if (   givesCheck
-                   && depth > 9)
+                   && depth > 9
+                   && (!ttCapture || move == ttMove))
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
