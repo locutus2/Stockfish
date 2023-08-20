e40ea103ad8c1823bc5f2fdbe7dea1daa30bd82d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-08-20 13:08:18 +0200

    Allow check extension only if not likelyFailLow. Bench: 1504688

--- src/search.cpp
+++ src/search.cpp
@@ -1090,7 +1090,7 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9
-                   && (!ttCapture || move == ttMove))
+                   && !likelyFailLow)
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
