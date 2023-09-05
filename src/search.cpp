b50631b23b73e9cd65df9cf6afab43a183b374dd
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-09-05 19:25:05 +0200

    Less reduction in non-LMR case if at PV node with no best move. Bench: 1596442

--- src/search.cpp
+++ src/search.cpp
@@ -1222,7 +1222,10 @@ moves_loop: // When in check, search starts here
           if (!ttMove && cutNode)
               r += 2;
 
-          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3 && (!PvNode || bestMove)), !cutNode);
+          if (PvNode && !bestMove)
+              r--;
+
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail high,
