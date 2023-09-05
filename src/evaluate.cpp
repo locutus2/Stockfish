0e94bc74c17604a64c3abafd976bf0f7faffe78f
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-09-05 06:37:53 +0200

    Use average of root simple eval and best value for lazy eval. Bench: 1612790

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -161,8 +161,8 @@ Value Eval::evaluate(const Position& pos) {
 
   bool lazy = abs(simpleEval) >=   RookValue + KnightValue
                                  + 16 * shuffling * shuffling
-                                 +  2 * std::max(abs(pos.this_thread()->bestValue),
-                                                 abs(pos.this_thread()->rootSimpleEval));
+                                 + (  abs(pos.this_thread()->bestValue)
+                                    + abs(pos.this_thread()->rootSimpleEval)) / 2;
 
   if (lazy)
       v = Value(simpleEval);
