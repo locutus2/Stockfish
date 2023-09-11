f2d077778e68cb442a7c28c632d75c7bbb5e164d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-09-11 22:44:33 +0200

    No quiet history pruning if move count <= 2 and improving. Bench: 1561459

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,7 @@ moves_loop: // When in check, search starts here
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 6
                   && history < -3832 * depth
-                  && (moveCount > 2 || priorCapture))
+                  && (moveCount > 2 || !improving))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
