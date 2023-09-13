0aead5060e0f0c781c6b990034d892ed5604589b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-09-13 09:27:29 +0200

    No quiet history pruning if move count <= 2 and move is first killer. Bench: 1601255

--- src/search.cpp
+++ src/search.cpp
@@ -1012,7 +1012,7 @@ moves_loop: // When in check, search starts here
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 6
                   && history < -3832 * depth
-                  && (lmrDepth >= 0 || ss->inCheck))
+                  && (moveCount > 2 || move != ss->killers[0]))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
