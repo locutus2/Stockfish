9b11afc7afe3969704488c70fc1c45f5acf1686b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-09-21 22:10:21 +0200

    Variant: Check of my lmr move count tuning (start=50,C=0.5) after 78K. Use only A[0][1] but decrease threshold by 20. Bench: 1608465

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,7 @@ moves_loop: // When in check, search starts here
       if (cutNode)
           r += 2;
 
-      else if (!PvNode && moveCount > 37 && !capture)
+      else if (!PvNode && moveCount > 27)
           r--;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
