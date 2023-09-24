fddc7f25a96115dcf7c6dc687daf6f098f2230da
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-09-24 12:01:44 +0200

    Check only A[2][0][0] and A[2][0][1] of final values of my lmr move count tuning dependent on node type and prior capture. Bench: 1345427

--- src/search.cpp
+++ src/search.cpp
@@ -1139,7 +1139,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2 + (moveCount > 34 && !priorCapture);
+          r += 2 + (moveCount > 34 && moveCount < 43 && !priorCapture);
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
