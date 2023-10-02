308064d3a0a02ccbf707e60181c290084ce6c81d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-02 15:04:53 +0200

    Test final values of my cmh bonus update weight tuning. But ignore added cmh3 and cmh5 like in master. Bench: 1280301

--- src/search.cpp
+++ src/search.cpp
@@ -1766,7 +1766,7 @@ moves_loop: // When in check, search starts here
 
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
-    for (int i : {1, 2, 3, 4, 5, 6})
+    for (int i : {1, 2, 4, 6})
     {
         // Only update the first 2 continuation histories if we are in check
         if (ss->inCheck && i > 2)
