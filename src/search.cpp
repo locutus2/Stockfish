8f07dbbb695c81058c30c5083a21ebf5c8f99073
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-06 19:23:31 +0200

    At expected ALL nodes with depth <= 1 and 2*(ss-1)->PVdistance > ss->ply go directly into quience search. Bench: 1238717

--- src/search.cpp
+++ src/search.cpp
@@ -527,7 +527,7 @@ namespace {
     constexpr bool rootNode = nodeType == Root;
 
     // Dive into quiescence search when the depth reaches zero
-    if (depth <= (!PvNode && !cutNode && (ss-1)->PVdistance >= 5))
+    if (depth <= (!PvNode && !cutNode && 2 * (ss-1)->PVdistance > ss->ply))
         return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
 
     // Check if we have an upcoming move that draws by repetition, or
