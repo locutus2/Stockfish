41a080bbcd5ad248d5e670d4eed5945c4c63985c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-15 08:59:04 +0200

    Simplify Viz recent cmh patch by removing usage of cmh3 in movepicker. Bench: 1173320

--- src/search.cpp
+++ src/search.cpp
@@ -1764,17 +1764,17 @@ moves_loop: // When in check, search starts here
 
 
   // update_continuation_histories() updates histories of the move pairs formed
-  // by moves at ply -1, -2, -4, and -6 with current move.
+  // by moves at ply -1, -2, -3, -4, and -6 with current move.
 
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
-    for (int i : {1, 2, 4, 6})
+    for (int i : {1, 2, 3, 4, 6})
     {
         // Only update the first 2 continuation histories if we are in check
         if (ss->inCheck && i > 2)
             break;
         if (is_ok((ss-i)->currentMove))
-            (*(ss-i)->continuationHistory)[pc][to] << bonus;
+            (*(ss-i)->continuationHistory)[pc][to] << bonus / (1 + 3 * (i == 3));
     }
   }
 
