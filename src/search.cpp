e11524277b5fc8d1d019b274fd55b11be19e92f7
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-19 22:14:58 +0200

    Use new returned probcut value also for tt save. Test against PR 4835. Bench: 1080978

--- src/search.cpp
+++ src/search.cpp
@@ -894,9 +894,11 @@ namespace {
 
                 if (value >= probCutBeta)
                 {
+                    value -= probCutBeta - beta;
+
                     // Save ProbCut data into transposition table
                     tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3, move, ss->staticEval);
-                    return value - (probCutBeta - beta);
+                    return value;
                 }
             }
 
