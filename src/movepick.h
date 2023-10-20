f376cffec027ab187ee175e2914a211ffb15de1b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-20 15:31:37 +0200

    Extend stat data types so different scale factors can be set for each history. Try now scale 3/4 for capture history. Test against PR 4836. Bench: 1113031

--- src/movepick.h
+++ src/movepick.h
@@ -90,14 +90,14 @@ enum StatsType { NoCaptures, Captures };
 /// ordering decisions. It uses 2 tables (one for each color) indexed by
 /// the move's from and to squares, see www.chessprogramming.org/Butterfly_Boards
 /// (~11 elo)
-using ButterflyHistory = Stats<int16_t, 7183, 5, 6, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)>;
+using ButterflyHistory = Stats<int16_t, 7183, 4, 5, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)>;
 
 /// CounterMoveHistory stores counter moves indexed by [piece][to] of the previous
 /// move, see www.chessprogramming.org/Countermove_Heuristic
 using CounterMoveHistory = Stats<Move, NOT_USED, NOT_USED, NOT_USED, PIECE_NB, SQUARE_NB>;
 
 /// CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-using CapturePieceToHistory = Stats<int16_t, 10692, 4, 5, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using CapturePieceToHistory = Stats<int16_t, 10692, 3, 4, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 /// PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 using PieceToHistory = Stats<int16_t, 29952, 4, 5, PIECE_NB, SQUARE_NB>;
