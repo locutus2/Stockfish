e3cc19a8f002920e6caaa6f34bdb116564809eae
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-20 15:33:04 +0200

    Extend stat data types so different scale factors can be set for each history. Try now scale 5/6 for capture history. Test against PR 4836. Bench: 1336022

--- src/movepick.h
+++ src/movepick.h
@@ -97,7 +97,7 @@ using ButterflyHistory = Stats<int16_t, 7183, 4, 5, COLOR_NB, int(SQUARE_NB) * i
 using CounterMoveHistory = Stats<Move, NOT_USED, NOT_USED, NOT_USED, PIECE_NB, SQUARE_NB>;
 
 /// CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-using CapturePieceToHistory = Stats<int16_t, 10692, 3, 4, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using CapturePieceToHistory = Stats<int16_t, 10692, 5, 6, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 /// PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 using PieceToHistory = Stats<int16_t, 29952, 4, 5, PIECE_NB, SQUARE_NB>;
