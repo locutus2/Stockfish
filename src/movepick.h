170fcef85fb49557a7e9b3699923968f331dd12a
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-27 19:09:17 +0200

    Retest: Use 2 bits of the hash key for grouping pawn structures by pawn count oddness separated by color. Bench: 1411390

--- src/movepick.h
+++ src/movepick.h
@@ -35,8 +35,11 @@ namespace Stockfish {
 constexpr int PAWN_HISTORY_SIZE = 512;
 
 inline int pawn_structure(const Position& pos) {
+    // Use two lowest key bits for encoding separate pawn count oddness for white and black pawns
+    return (pos.pawn_key() & (PAWN_HISTORY_SIZE - 4)) ^ (pos.count<PAWN>(WHITE) & 1)
+         ^ ((pos.count<PAWN>(BLACK) & 1) << 1);
     // Use lowest key bit for encoding pawn count oddness
-    return (pos.pawn_key() & (PAWN_HISTORY_SIZE - 2)) ^ (pos.count<PAWN>() & 1);
+    //return (pos.pawn_key() & (PAWN_HISTORY_SIZE - 2)) ^ (pos.count<PAWN>() & 1);
     // Use 4 lowest key bits for encoding pawn count modulus 4 separate for white and black pawns
     //return (pos.pawn_key() & (PAWN_HISTORY_SIZE - 16)) ^ (pos.count<PAWN>(WHITE) & 3)
     //     ^ ((pos.count<PAWN>(BLACK) & 3) << 2);
