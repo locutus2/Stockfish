656d96b0981393e7f3970d654f0162a93683d7cf
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-27 17:34:59 +0200

    Update pawn history also in fail low case like main history if prior moved piece was not a pawn. Test against PR 4849. Bench: 1233709

--- src/search.cpp
+++ src/search.cpp
@@ -1324,8 +1324,10 @@ moves_loop:  // When in check, search starts here
                                       stat_bonus(depth) * bonus);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)]
           << stat_bonus(depth) * bonus / 2;
-        thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq]
-          << stat_bonus(depth) * bonus / 2;
+
+        if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION)
+            thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq]
+              << stat_bonus(depth) * bonus / 2;
     }
 
     if (PvNode)
