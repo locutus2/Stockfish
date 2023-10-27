87ffc57403fd5759cf623201e9388ee3808b259c
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-10-27 09:32:06 +0200

    During by pawn history test i found that static eval update of this history seems not good. Now i measured stats also for the main history and it seems it has the same bad properties especially for high history values. So scale down main history update bonus for positive values. Bench: 1414698

--- src/search.cpp
+++ src/search.cpp
@@ -744,7 +744,10 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
     if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812);
+        int bonus =
+          std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812)
+          * (7183 - std::max(int(thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)]), 0))
+          / 7183;
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
     }
 
