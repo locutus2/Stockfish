30fffee234c782d81b39746251327dd12c353197
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-04 02:39:12 +0100

    More reduction if ttDepth < depth-1 and abs(ttValue - staticEval) < 20. Bench: 1353933

--- src/search.cpp
+++ src/search.cpp
@@ -1143,7 +1143,7 @@ moves_loop:  // When in check, search starts here
             r--;
 
         if (abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && ss->staticEval != VALUE_NONE
-            && ttDepth >= depth - 1 && abs(ttValue - ss->staticEval) < 20)
+            && ttDepth < depth - 1 && abs(ttValue - ss->staticEval) < 20)
             r++;
 
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
