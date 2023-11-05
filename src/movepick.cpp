8c3a0f0078c85954c94d0448bc03107811f5c74e
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-05 20:03:40 +0100

    Try now only double weight of pawn history in quiet move ordering. Use 1/3 of the sort limit compensation. Bench: 1379422

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -304,7 +304,7 @@ top:
             endMoves = generate<QUIETS>(pos, cur);
 
             score<QUIETS>();
-            partial_insertion_sort(cur, endMoves, -3000 * depth);
+            partial_insertion_sort(cur, endMoves, -1960 - 3130 * depth);
         }
 
         ++stage;
