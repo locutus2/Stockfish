34cc4968373354dd16a60214bbda6dddd1d6dba2
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-05 08:58:06 +0100

    Try now only the quiet sort limit adjustment of my tweak_quiet_order2 test. Bench: 1302028

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -305,7 +305,7 @@ top:
             endMoves = generate<QUIETS>(pos, cur);
 
             score<QUIETS>();
-            partial_insertion_sort(cur, endMoves, -3000 * depth);
+            partial_insertion_sort(cur, endMoves, -5881 - 3390 * depth);
         }
 
         ++stage;
