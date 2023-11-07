dc2231b823563aca39ff3a17d9c574a80b9a715d
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-07 04:40:03 +0100

    Increase update weight for main history in quiet move ordering from 2 to 3. Take my last depth dependent compensation but remove the cap (the difference should be small because 95% of the nodes have depth <= 8) and add to sort limit. Test against my passed PR 4866. Bench: 1230188

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -304,7 +304,7 @@ top:
             endMoves = generate<QUIETS>(pos, cur);
 
             score<QUIETS>();
-            partial_insertion_sort(cur, endMoves, -7238 - 3130 * depth - 54 * std::min(depth, 8));
+            partial_insertion_sort(cur, endMoves, -7238 - 3184 * depth);
         }
 
         ++stage;
