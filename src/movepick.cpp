a19a3b1b61a6a4654fca96fcd7b22a4db6f7e4eb
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-09 08:16:06 +0100

    Tripple up weight of main history for quiet evasion ordering. Bench: 1264080

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -216,8 +216,8 @@ void MovePicker::score() {
                 m.value = PieceValue[pos.piece_on(to_sq(m))] - Value(type_of(pos.moved_piece(m)))
                         + (1 << 28);
             else
-                m.value = (*mainHistory)[pos.side_to_move()][from_to(m)]
-                        + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)] / 2
+                m.value = 3 * (*mainHistory)[pos.side_to_move()][from_to(m)]
+                        + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                         + (*pawnHistory)[pawn_structure(pos)][pos.moved_piece(m)][to_sq(m)];
         }
 }
