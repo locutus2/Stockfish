edc99262d77c6999b95f880ade66a8410526edb9
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-09 07:39:38 +0100

    Apply static eval update also on capture history (adjust bonus by captured piece value). Bench: 1345172

--- src/search.cpp
+++ src/search.cpp
@@ -746,12 +746,17 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
     if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck)
     {
-        int bonus = std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812);
         if (priorCapture)
+        {
+            int bonus = std::clamp(
+              -18 * int((ss - 1)->staticEval + ss->staticEval + PieceValue[pos.captured_piece()]),
+              -1812, 1812);
             thisThread->captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())]
               << bonus;
+        }
         else
         {
+            int bonus = std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812);
             thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
             if (type_of(pos.piece_on(prevSq)) != PAWN
                 && type_of((ss - 1)->currentMove) != PROMOTION)
