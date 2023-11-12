66fc2e6487a447faf183fb3cd5b0bfc8233e5b4b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-12 17:10:51 +0100

    Add half cmh[1] for quiet evasion. Bench: 1389879

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -216,8 +216,9 @@ void MovePicker::score() {
                 m.value = PieceValue[pos.piece_on(to_sq(m))] - Value(type_of(pos.moved_piece(m)))
                         + (1 << 28);
             else
-                m.value = (*mainHistory)[pos.side_to_move()][from_to(m)] * 3 / 2
+                m.value = (*mainHistory)[pos.side_to_move()][from_to(m)]
                         + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                        + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)] / 2
                         + (*pawnHistory)[pawn_structure(pos)][pos.moved_piece(m)][to_sq(m)];
         }
 }
