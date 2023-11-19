f939bb61eaf7368f5466000ec8405bd84fc7e67e
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-19 21:44:59 +0100

    Fixed: Use 7/4*cmh[0] + 3/4*cmh[3] for quiet move ordering. Bench: 1451015

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -186,7 +186,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to] / 4;
             m.value += (*continuationHistory[3])[pc][to] * 3 / 4;
-            m.value += (*continuationHistory[3])[pc][to];
+            m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
             m.value += bool(pos.check_squares(pt) & to) * 16384;
