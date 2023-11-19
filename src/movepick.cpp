b1894342ab75c0de0e02aab03a12c55376b2dbec
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-19 06:27:38 +0100

    Use 5/4*cmh[0] for quiet move ordering. Bench: 1502117

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -182,10 +182,10 @@ void MovePicker::score() {
             // histories
             m.value = 2 * (*mainHistory)[pos.side_to_move()][from_to(m)];
             m.value += 2 * (*pawnHistory)[pawn_structure(pos)][pc][to];
-            m.value += (*continuationHistory[0])[pc][to];
+            m.value += (*continuationHistory[0])[pc][to] * 5 / 4;
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to] / 4;
-            m.value += (*continuationHistory[3])[pc][to] * 3 / 4;
+            m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
@@ -302,7 +302,7 @@ top:
             endMoves = generate<QUIETS>(pos, cur);
 
             score<QUIETS>();
-            partial_insertion_sort(cur, endMoves, -3130 * depth);
+            partial_insertion_sort(cur, endMoves, -1960 - 3130 * depth);
         }
 
         ++stage;
