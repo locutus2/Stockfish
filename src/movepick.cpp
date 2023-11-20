2e7990051950c10ff50087ee4fc032318a0b2332
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-20 19:43:56 +0100

    More history tweaks for quiet move ordering. Add flat compensation to sort limit. Bench: 1364098

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -302,7 +302,7 @@ top:
             endMoves = generate<QUIETS>(pos, cur);
 
             score<QUIETS>();
-            partial_insertion_sort(cur, endMoves, -1960 - 3130 * depth);
+            partial_insertion_sort(cur, endMoves, 5879 - 3130 * depth);
         }
 
         ++stage;
