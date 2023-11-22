db579630c939c9daba254c5536e272e5bd5aa1e6
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-22 13:11:27 +0100

    refactor to allow independent setting for histories

--- src/movepick.h
+++ src/movepick.h
@@ -43,7 +43,7 @@ inline int pawn_structure(const Position& pos) { return pos.pawn_key() & (PAWN_H
 // be a move or even a nested history. We use a class instead of a naked value
 // to directly call history update operator<<() on the entry so to use stats
 // tables at caller sites as simple multi-dim arrays.
-template<typename T, int D>
+template<typename T, int D, bool SKEW_POS>
 class StatsEntry {
 
     T entry;
@@ -58,7 +58,7 @@ class StatsEntry {
         assert(abs(bonus) <= D);  // Ensure range is [-D, D]
         static_assert(D <= std::numeric_limits<T>::max(), "D overflows T");
 
-        if (bonus > 0)
+        if (SKEW_POS && bonus > 0)
             entry = std::max(entry, short(0));
 
         entry += (bonus * D - entry * abs(bonus)) / (D * 5 / 4);
@@ -72,23 +72,23 @@ class StatsEntry {
 // template parameter D limits the range of updates in [-D, D] when we update
 // values with the << operator, while the last parameters (Size and Sizes)
 // encode the dimensions of the array.
-template<typename T, int D, int Size, int... Sizes>
-struct Stats: public std::array<Stats<T, D, Sizes...>, Size> {
-    using stats = Stats<T, D, Size, Sizes...>;
+template<typename T, int D, int SKEW_POS, int Size, int... Sizes>
+struct Stats: public std::array<Stats<T, D, SKEW_POS, Sizes...>, Size> {
+    using stats = Stats<T, D, SKEW_POS, Size, Sizes...>;
 
     void fill(const T& v) {
 
         // For standard-layout 'this' points to the first struct member
         assert(std::is_standard_layout_v<stats>);
 
-        using entry = StatsEntry<T, D>;
+        using entry = StatsEntry<T, D, SKEW_POS>;
         entry* p    = reinterpret_cast<entry*>(this);
         std::fill(p, p + sizeof(*this) / sizeof(entry), v);
     }
 };
 
-template<typename T, int D, int Size>
-struct Stats<T, D, Size>: public std::array<StatsEntry<T, D>, Size> {};
+template<typename T, int D, int SKEW_POS, int Size>
+struct Stats<T, D, SKEW_POS, Size>: public std::array<StatsEntry<T, D, SKEW_POS>, Size> {};
 
 // In stats table, D=0 means that the template parameter is not used
 enum StatsParams {
@@ -103,26 +103,26 @@ enum StatsType {
 // during the current search, and is used for reduction and move ordering decisions.
 // It uses 2 tables (one for each color) indexed by the move's from and to squares,
 // see www.chessprogramming.org/Butterfly_Boards (~11 elo)
-using ButterflyHistory = Stats<int16_t, 7183, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)>;
+using ButterflyHistory = Stats<int16_t, 7183, true, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)>;
 
 // CounterMoveHistory stores counter moves indexed by [piece][to] of the previous
 // move, see www.chessprogramming.org/Countermove_Heuristic
-using CounterMoveHistory = Stats<Move, NOT_USED, PIECE_NB, SQUARE_NB>;
+using CounterMoveHistory = Stats<Move, NOT_USED, false, PIECE_NB, SQUARE_NB>;
 
 // CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-using CapturePieceToHistory = Stats<int16_t, 10692, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using CapturePieceToHistory = Stats<int16_t, 10692, true, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 // PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
-using PieceToHistory = Stats<int16_t, 29952, PIECE_NB, SQUARE_NB>;
+using PieceToHistory = Stats<int16_t, 29952, true, PIECE_NB, SQUARE_NB>;
 
 // ContinuationHistory is the combined history of a given pair of moves, usually
 // the current one given a previous one. The nested history table is based on
 // PieceToHistory instead of ButterflyBoards.
 // (~63 elo)
-using ContinuationHistory = Stats<PieceToHistory, NOT_USED, PIECE_NB, SQUARE_NB>;
+using ContinuationHistory = Stats<PieceToHistory, NOT_USED, false, PIECE_NB, SQUARE_NB>;
 
 // PawnHistory is addressed by the pawn structure and a move's [piece][to]
-using PawnHistory = Stats<int16_t, 8192, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB>;
+using PawnHistory = Stats<int16_t, 8192, true, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB>;
 
 // MovePicker class is used to pick one pseudo-legal move at a time from the
 // current position. The most important method is next_move(), which returns a
