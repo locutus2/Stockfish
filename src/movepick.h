b6a592b96fd14bc6384149f9886e922469d70419
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-22 13:25:11 +0100

    Experiment: if bonus > 0 increase current capture history value at least to 0. Bench: 1425016

--- src/movepick.h
+++ src/movepick.h
@@ -110,7 +110,7 @@ using ButterflyHistory = Stats<int16_t, 7183, false, COLOR_NB, int(SQUARE_NB) * 
 using CounterMoveHistory = Stats<Move, NOT_USED, false, PIECE_NB, SQUARE_NB>;
 
 // CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-using CapturePieceToHistory = Stats<int16_t, 10692, false, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using CapturePieceToHistory = Stats<int16_t, 10692, true, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 // PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 using PieceToHistory = Stats<int16_t, 29952, false, PIECE_NB, SQUARE_NB>;
@@ -122,7 +122,7 @@ using PieceToHistory = Stats<int16_t, 29952, false, PIECE_NB, SQUARE_NB>;
 using ContinuationHistory = Stats<PieceToHistory, NOT_USED, false, PIECE_NB, SQUARE_NB>;
 
 // PawnHistory is addressed by the pawn structure and a move's [piece][to]
-using PawnHistory = Stats<int16_t, 8192, true, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB>;
+using PawnHistory = Stats<int16_t, 8192, false, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB>;
 
 // MovePicker class is used to pick one pseudo-legal move at a time from the
 // current position. The most important method is next_move(), which returns a
