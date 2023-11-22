780789374036f249f3a859071071e5deddfa37e4
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-22 07:59:47 +0100

    Collect bad capture history (use quadruple weight for positive updates). No futility pruning if badCaptureHist > 0. Bench: 1302419

--- src/search.cpp
+++ src/search.cpp
@@ -1291,7 +1291,7 @@ moves_loop:  // When in check, search starts here
 
         if (!ss->inCheck && mp.isBadCapture())
         {
-            int bonus = value > alpha ? stat_bonus(depth) : -stat_malus(depth);
+            int bonus = value > alpha ? 4 * stat_bonus(depth) : -stat_malus(depth);
             thisThread->badCaptureHistory[movedPiece][to_sq(move)][pos.piece_on(to_sq(move))]
               << bonus;
         }
