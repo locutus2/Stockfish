217f2296a485377b05d2e54bb15059690e66e650
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-11-23 19:07:15 +0100

    More reduction if (ss+2)->cutoffCnt >= 34. Inspired by Vizs tests i have done some measurements for the opposite direction and come up with this. Bench: 1284733

--- src/search.cpp
+++ src/search.cpp
@@ -1150,6 +1150,9 @@ moves_loop:  // When in check, search starts here
         if (move == (ss - 4)->currentMove && pos.has_repeated())
             r += 2;
 
+        if ((ss + 2)->cutoffCnt >= 34)
+            r++;
+
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
