d5c542f33465bae64072992711a3856fa96b243b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-13 13:52:16 +0100

    Combination: Half/double checking bonus for pawns/queens. Bench: 1214410

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -187,7 +187,8 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += (pt != PAWN && pos.check_squares(pt) & to) * 16384;
+            m.value +=
+              bool(pos.check_squares(pt) & to) * (pt == QUEEN ? 32768 : pt == PAWN ? 8192 : 16384);
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 50000
