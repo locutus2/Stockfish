6ad5b37b16fa184c178690cc66728a83038f3e71
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-13 17:56:34 +0100

    50% more checking bonus for queens. Bench: 1116344

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -187,8 +187,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value +=
-              bool(pos.check_squares(pt) & to) * (pt == QUEEN ? 32768 : pt == PAWN ? 8192 : 16384);
+            m.value += bool(pos.check_squares(pt) & to) * (pt == QUEEN ? 24576 : 16384);
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 50000
