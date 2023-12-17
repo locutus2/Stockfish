b56aed6eb8347da8bfcee2d44e1ba60c1eae4615
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-17 12:29:38 +0100

    Less reduction for captures at PV nodes if capture best move found. Bench: 1216695

--- src/search.cpp
+++ src/search.cpp
@@ -1146,7 +1146,7 @@ moves_loop:  // When in check, search starts here
             r--;
 
         if (PvNode && capture && captureBestMove)
-            r++;
+            r--;
 
         // Decrease reduction if a quiet ttMove has been singularly extended (~1 Elo)
         if (singularQuietLMR)
