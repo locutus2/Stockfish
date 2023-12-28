0684da498e68777d72f540732ef6d86b64c8cf33
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-28 18:44:10 +0100

    Test first at STC the LTC tuned values of my weights for only main history (pawn history changes are very small so ignore it here) static eval update separated for positive and negative value. Probably fails. Bench: 1160756

--- src/search.cpp
+++ src/search.cpp
@@ -749,10 +749,10 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture)
     {
         int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1652, 1546);
-        thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
-        if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION
-            && bonus > 0)
-            thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq] << bonus / 2;
+        thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)]
+          << bonus * (bonus > 0 ? 83 : 67) / 64;
+        if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION)
+            thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq] << bonus / 4;
     }
 
     // Set up the improving flag, which is true if current static evaluation is
