ff7fe7882b7597c927b61dd8433d9dcf96d7ffa9
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-30 22:16:55 +0100

    Experiment: add move order history (as copy of main history) only for move ordering. Update main order history only if positive static eval but use 75% more bonus in this case. Tests against Vizs PR 4947. Bench: 1353408

--- src/search.cpp
+++ src/search.cpp
@@ -761,7 +761,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
         int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1652, 1546);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
         if (bonus > 0)
-            thisThread->mainOrderHistory[~us][from_to((ss - 1)->currentMove)] << 2 * bonus;
+            thisThread->mainOrderHistory[~us][from_to((ss - 1)->currentMove)] << bonus * 7 / 4;
         if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION)
             thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq] << bonus / 4;
     }
