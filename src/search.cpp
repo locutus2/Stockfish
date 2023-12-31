97360045d64d0cc0fc73c1dbacd1d2da4386f303
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-31 22:54:51 +0100

    bench correction history

--- src/search.cpp
+++ src/search.cpp
@@ -1482,7 +1482,7 @@ moves_loop:  // When in check, search starts here
     {
         auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8,
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
-        thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] << bonus;
+        thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] << bonus * (16 + (bonus > 0 ? 0 : 0)) / 16;
     }
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
