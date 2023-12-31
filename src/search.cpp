3b6e0207d90004a136c5d90d5447e4603c4dcd3b
Stefan Geschwentner <stgeschwentner@gmail.com>
2023-12-31 10:15:17 +0100

    Try my STC passed move order history update now instead directly on main history. Tests against the newest Vizs PR 4950 (not 4947). Bench: 1643741

--- src/search.cpp
+++ src/search.cpp
@@ -757,7 +757,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     if (is_ok((ss - 1)->currentMove) && !(ss - 1)->inCheck && !priorCapture)
     {
         int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1652, 1546);
-        thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
+        thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << (bonus > 0 ? 2 * bonus : bonus / 2);
         if (type_of(pos.piece_on(prevSq)) != PAWN && type_of((ss - 1)->currentMove) != PROMOTION)
             thisThread->pawnHistory[pawn_structure(pos)][pos.piece_on(prevSq)][prevSq] << bonus / 4;
     }
