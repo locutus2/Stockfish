7ea269a3ba9eacbc2a2f83c78f3f4a1f61104797
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-03 04:01:59 +0100

    test cut node + mcp

--- src/search.cpp
+++ src/search.cpp
@@ -1151,7 +1151,6 @@ moves_loop:  // When in check, search starts here
                 bool C1 = moveCountPruning;
                 constexpr double P1 = 0.0927054;
                 constexpr int V1[9] = { 22,  19,  15,  8,   0,   -16, -30, -37, -41 };
-                */
 
                 bool C0 = cutNode;
                 constexpr double P0 = 0.321481;
@@ -1159,6 +1158,14 @@ moves_loop:  // When in check, search starts here
                 bool C1 = singularQuietLMR;
                 constexpr double P1 = 0.0476765;
                 constexpr int V1[9] = { 12,   9,   6,   2,   0,   -5,  -7,  -8,  -9 };
+                */
+
+                bool C0 = cutNode;
+                constexpr double P0 = 0.321481;
+                constexpr int V0[9] = { 127, 94, 65, 30, 0, -47, -68, -79, -86 };
+                bool C1 = moveCountPruning;
+                constexpr double P1 = 0.0927054;
+                constexpr int V1[9] = { 22,  19,  15,  8,   0,   -16, -30, -37, -41 };
 
                 if (MEASURE && !ss->inCheck && lmrDepth < 14)
                 {
