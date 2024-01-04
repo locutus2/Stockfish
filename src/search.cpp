db278289da5a880e7a6d8716e1346307128052d5
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-04 09:45:24 +0100

    Use combination of improving and move count pruning for quiet futility pruning. Bench: 1543285

--- src/search.cpp
+++ src/search.cpp
@@ -1040,8 +1040,8 @@ moves_loop:  // When in check, search starts here
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 14
                     && ss->staticEval + (bestValue < ss->staticEval - 57 ? 124 : 71)
-                           + 118 * lmrDepth + 128 * cutNode - 256 * improving
-                           + 332 * cutNode * improving
+                           + 118 * lmrDepth + 256 * improving + 256 * moveCountPruning
+                           - 7591 * improving * moveCountPruning
                          <= alpha)
                     continue;
 
