f47d784c4690b39d8ded3d281c28b5661e2b4e37
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-04 18:34:35 +0100

    Tune quiet futility pruning margin by different conditions. But this time use only one parameter per condition and add a compensation term instead based on the measured frequencies of the conditions. This tries to keep the average margin change to zero. Bench: 1176235

--- src/search.cpp
+++ src/search.cpp
@@ -47,7 +47,7 @@
 
 namespace Stockfish {
 
-int A[7][2];
+int A[7];
 TUNE(SetRange(-512, 512), A);
 
 namespace Search {
@@ -1045,13 +1045,13 @@ moves_loop:  // When in check, search starts here
                 if (!ss->inCheck && lmrDepth < 14
                     && ss->staticEval + (bestValue < ss->staticEval - 57 ? 124 : 71)
                            + 118 * lmrDepth
-                           + !cutNode * A[0][0] + cutNode * A[0][1]
-                           + !improving * A[1][0] + improving * A[1][1]
-                           + !priorCapture * A[2][0] + priorCapture * A[2][1]
-                           + !PvNode * A[3][0] + PvNode * A[3][1]
-                           + !singularQuietLMR * A[4][0] + singularQuietLMR * A[4][1]
-                           + !moveCountPruning * A[5][0] + moveCountPruning * A[5][1]
-                           + !ttCapture * A[6][0] + ttCapture * A[6][1]
+                           + cutNode * A[0]          - int(A[0] * 0.321481)
+                           + improving * A[1]        - int(A[1] * 0.378073)
+                           + priorCapture * A[2]     - int(A[2] * 0.28863)
+                           + PvNode * A[3]           - int(A[3] * 0.0781727)
+                           + singularQuietLMR * A[4] - int(A[4] * 0.0476765)
+                           + moveCountPruning * A[5] - int(A[5] * 0.0927054)
+                           + ttCapture * A[6]        - int(A[6] * 0.091047)
                          <= alpha)
                     continue;
 
