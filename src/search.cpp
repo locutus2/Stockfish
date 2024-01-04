b0d54dc00fde19bb3fb65006bbe296bd65a28e08
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-04 12:33:03 +0100

    pv + ttcapture

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,14 @@ moves_loop:  // When in check, search starts here
                 constexpr double P1 = 0.0927054;
                 constexpr int V1[9] = { 22,  19,  15,  8,   0,   -16, -30, -37, -41 };
                 constexpr double P01 = 0.0398305;
-                */
+
+                bool C0 = cutNode;
+                constexpr double P0 = 0.321481;
+                constexpr int V0[9] = { 127, 94, 65, 30, 0, -47, -68, -79, -86 };
+                bool C1 = singularQuietLMR;
+                constexpr double P1 = 0.0476765;
+                constexpr int V1[9] = { 12,   9,   6,   2,   0,   -5,  -7,  -8,  -9 };
+                constexpr double P01 = 0.0366509;
 
                 bool C0 = PvNode;
                 constexpr double P0 = 0.0781727;
@@ -1199,6 +1206,15 @@ moves_loop:  // When in check, search starts here
                 constexpr double P1 = 0.0476765;
                 constexpr int V1[9] = { 12,   9,   6,   2,   0,   -5,  -7,  -8,  -9 };
                 constexpr double P01 = 0.0105854;
+                */
+
+                bool C0 = PvNode;
+                constexpr double P0 = 0.0781727;
+                constexpr int V0[9] = { 18,   12,  8,   3,   0,   -5,  -7,  -9,  -9, };
+                bool C1 = ttCapture;
+                constexpr double P1 = 0.091047;
+                constexpr int V1[9] = { 18,  14,  10,  5,   0,   -7,  -13, -19, -23 };
+                constexpr double P01 = 0.0231626;
 
                 if (MEASURE && !ss->inCheck && lmrDepth < 14)
                 {
