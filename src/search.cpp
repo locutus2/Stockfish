55908bbb7468fe93bc91f56d214ef28c781b20e5
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-12 14:28:25 +0100

    Tweak futility move count formula. For not improving +depth-ply/2 Bench: 1288318

--- src/search.cpp
+++ src/search.cpp
@@ -90,7 +90,7 @@ Depth reduction(bool i, Depth d, int mn, int delta, int rootDelta) {
 }
 
 constexpr int futility_move_count(bool improving, Depth depth, int ply) {
-    return improving ? (3 + depth * depth) : (3 + 3 * depth - ply + depth * depth) / 2;
+    return improving ? (3 + depth * depth) : (3 + depth - ply / 2 + depth * depth) / 2;
 }
 
 // Guarantee evaluation does not hit the tablebase range
