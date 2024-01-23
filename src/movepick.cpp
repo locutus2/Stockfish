2ef51e5e94ec1ed4d9d009e3674f8b070b45341a
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-23 21:07:47 +0100

    Slightly increase good quiet threshold for low depth. Take 5. Bench: 1207775

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -310,7 +310,7 @@ top:
                 return *cur != refutations[0] && *cur != refutations[1] && *cur != refutations[2];
             }))
         {
-            if ((cur - 1)->value > 39840 / (10 * depth - 3) - 9000
+            if ((cur - 1)->value > 17010 / (2 * depth + 1) - 9000
                 || (cur - 1)->value <= quiet_threshold(depth))
                 return *(cur - 1);
 
