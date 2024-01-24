acc029a93ade2b68494120a6dc4f6bb53f8b4922
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-01-24 21:08:35 +0100

    Tweak weights for positive/negative eval bonus update dependent on PV/non-PV node. Take 2. Bench: 1307749

--- src/search.cpp
+++ src/search.cpp
@@ -770,7 +770,7 @@ Value Search::Worker::search(
     if (((ss - 1)->currentMove).is_ok() && !(ss - 1)->inCheck && !priorCapture)
     {
         int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1680, 1406);
-        bonus     = bonus > 0 ? 2 * bonus : bonus / 2;
+        bonus     = bonus * (bonus > 0 ? 168 - 31 * PvNode : 26 + 46 * PvNode) / 64;
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
