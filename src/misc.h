38c205c29b24b83b8eb1064e54ceee5b8a31bea8
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-02-01 06:43:53 +0100

    set default

--- src/misc.h
+++ src/misc.h
@@ -36,7 +36,7 @@ namespace Stockfish {
 //------------------------------------
 // AUC stuff
 constexpr int  HISTORY_BUCKETS  = 10000;
-constexpr bool USE_DEPTH_WEIGHT = false;
+constexpr bool USE_DEPTH_WEIGHT = true;
 
 inline int getWeight(int depth) { return USE_DEPTH_WEIGHT && depth > 0 ? depth : 1; }
 
