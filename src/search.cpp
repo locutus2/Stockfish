2c1bac022f57fdb1563117c889915ee0e0ee7882
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-02-08 20:21:59 +0100

    Use for history eval update difference of static eval from two moves before (instead of one move). Because this bonus if way more balanced then master remove also the rescaling dependent on sign. Bench: 1153582

--- src/search.cpp
+++ src/search.cpp
@@ -728,7 +728,6 @@ Value Search::Worker::search(
         && !(ss - 2)->inCheck && !priorCapture)
     {
         int bonus = std::clamp(14 * int((ss - 2)->staticEval - ss->staticEval), -1661, 1495);
-        bonus     = bonus > 0 ? 2 * bonus : bonus / 2;
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
