40789ad2cd737e5376191e34c76653aeec758904
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-02-14 18:43:55 +0100

    Less reduction if prior capture and move gives check. Bench: 1111981

--- src/search.cpp
+++ src/search.cpp
@@ -1120,6 +1120,9 @@ moves_loop:  // When in check, search starts here
         else if (move == ttMove)
             r = 0;
 
+        if (givesCheck && priorCapture)
+            r--;
+
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
                       + (*contHist[1])[movedPiece][move.to_sq()]
@@ -1129,7 +1132,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore / 14894;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
-        if (depth >= 2 && moveCount > 1 + rootNode && (!givesCheck || !priorCapture))
+        if (depth >= 2 && moveCount > 1 + rootNode)
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
