7aa51aa06ec7afaac27525cb1ee1afda517e987e
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-02-19 19:02:32 +0100

    output

--- src/uci.cpp
+++ src/uci.cpp
@@ -465,7 +465,7 @@ void UCI::stats(Position& pos, std::istream& args, StateListPtr& states, std::os
     //constexpr int     PARAMS_MIN[]  = {-K * D0 + OFF0, -K * D1 + OFF1};
     //constexpr int     PARAMS_MAX[]  = {K * D0 + OFF0, K * D1 + OFF1};
     //constexpr int     PARAMS_STEP[] = {D0, D1};
-    const std::string PARAMS_NAME[N_PARAMS] = {"a0", "a5"};
+    const std::string PARAMS_NAME[N_PARAMS] = {"a0", "a1", "a2", "a3", "a4", "a5"};
     int     PARAMS_MIN[N_PARAMS];
     int     PARAMS_MAX[N_PARAMS];
     int     PARAMS_STEP[N_PARAMS];
