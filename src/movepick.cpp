6176db8afc4795162c79969d6215669d8e45582e
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-02-19 19:42:02 +0100

    Tweak quiet order factors. Bench: 1340663

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -179,10 +179,10 @@ void MovePicker::score() {
 
             // histories
             m.value = 2 * (*mainHistory)[pos.side_to_move()][m.from_to()];
-            m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
+            m.value += 65 * (*pawnHistory)[pawn_structure_index(pos)][pc][to] / 32;
             m.value += 2 * (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
-            m.value += (*continuationHistory[2])[pc][to] / 4;
+            m.value += 7 * (*continuationHistory[2])[pc][to] / 32;
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
 
