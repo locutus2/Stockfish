c8d5b9d961e845b9217c5c2cde947ef92fdb6eb0
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-03-09 06:59:34 +0100

    Tuned less reduction rule after 60K: only most significant term P[5][1]. Bench: 1960549

--- src/search.cpp
+++ src/search.cpp
@@ -1144,8 +1144,8 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1 + rootNode)
         {
-            if (!cutNode)
-                r++;
+            if (!ss->ttHit)
+                r--;
 
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
