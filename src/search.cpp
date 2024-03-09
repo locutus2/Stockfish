a48ce636d188f9b24ccb4adcf00a83f65057ed79
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-03-09 17:21:29 +0100

    Faster tuning by generating only random thresholds <= max value. Tune conditions for less reduction rule (C=15). Bench: 1714391

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop:  // When in check, search starts here
             }
 
             if (CC && conditions > 0)
-                r++;
+                r--;
 
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
