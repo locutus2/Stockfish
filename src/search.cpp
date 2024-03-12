7b21e419861396ea4bd0e9016f72113950ce5e23
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-03-12 15:14:52 +0100

    Final tuned less reduction rule: only five terms P[2][0],P[8][0],P[1][0],P[4][1],P[9][1]. But use reduction rule also if LMR is not tried. Bench: 1969180

--- src/search.cpp
+++ src/search.cpp
@@ -1132,6 +1132,9 @@ moves_loop:  // When in check, search starts here
         else if (move == ttMove)
             r = 0;
 
+        if (cutNode && ss->ttPv && !ss->inCheck && capture && !givesCheck)
+            r--;
+
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
                       + (*contHist[1])[movedPiece][move.to_sq()]
@@ -1143,9 +1146,6 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1 + rootNode)
         {
-            if (cutNode && ss->ttPv && !ss->inCheck && capture && !givesCheck && !improving)
-                r--;
-
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth. This may lead to hidden multiple extensions.
