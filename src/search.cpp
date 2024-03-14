a141b9d632d8b291c5757c573e44f95f01cc602f
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-03-14 12:17:41 +0100

    Tuned conditions for less reduction rule (C=15, one param per conditionwith range [-100..100]). Use only two terms P[0],P[8] Bench: 1688109

--- src/search.cpp
+++ src/search.cpp
@@ -1144,6 +1144,9 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1 + rootNode)
         {
+            if (PvNode && capture)
+                r--;
+
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth. This may lead to hidden multiple extensions.
