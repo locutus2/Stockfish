f806518cf24f81210ad4b9e3d62aef341a8cc010
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-03-16 11:59:49 +0100

    Double effect: Tuned conditions for less reduction rule (C=15, one param per conditionwith range [-100..100]). Use only two terms P[0],P[8]. Bench: 1925115

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,7 @@ moves_loop:  // When in check, search starts here
         if (depth >= 2 && moveCount > 1 + rootNode)
         {
             if (PvNode && capture)
-                r--;
+                r -= 2;
 
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
