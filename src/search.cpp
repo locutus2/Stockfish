f3a9b4648b9937e7e696d671b900b96751122c32
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-04-04 23:17:00 +0200

    First test my LTC LMR condition tunings at STC: more reduction if 4 largest conditions holds (ss->ttPv can be obmitted because its superseeded by PvNode). Bench: 1874547

--- src/search.cpp
+++ src/search.cpp
@@ -1128,8 +1128,7 @@ moves_loop:  // When in check, search starts here
 
         if (  !givesCheck
             && PvNode
-            && priorCapture
-            && ttCapture)
+            && priorCapture)
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
