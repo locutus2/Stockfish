c7adcff8221a325b48957c178df63c4854e43a22
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-04-10 13:39:06 +0200

    New tuned conditions for more reduction rule (C=15, 13 conditions, also tune condition threshold). Use four largest terms P[2],P[8],P[4],P[10]. Bench: 2007271

--- src/search.cpp
+++ src/search.cpp
@@ -1126,6 +1126,12 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r--;
 
+        if (   ss->ttPv
+            && !capture
+            && !ss->inCheck
+            && move == ttMove)
+            r++;
+
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
