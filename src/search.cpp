90c2fcdf2d0b4514b304d2373b62176b1091cccb
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-04-16 00:01:01 +0200

    Update also cmh at root. Bench: 1333672

--- src/search.cpp
+++ src/search.cpp
@@ -1767,6 +1767,8 @@ void update_all_stats(const Position& pos,
 // by moves at ply -1, -2, -3, -4, and -6 with current move.
 void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
+    (*(ss - ss->ply)->continuationHistory)[pc][to] << bonus;
+
     for (int i : {1, 2, 3, 4, 6})
     {
         // Only update the first 2 continuation histories if we are in check
