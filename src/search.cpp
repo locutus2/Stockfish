7180f7b9e1bde9a6d4b8aabd9321b0d7bbe35f0d
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-04-16 12:16:33 +0200

    Update also cmh at index before the root for even plies. Bench: 1178208

--- src/search.cpp
+++ src/search.cpp
@@ -1767,7 +1767,8 @@ void update_all_stats(const Position& pos,
 // by moves at ply -1, -2, -3, -4, and -6 with current move.
 void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
-    (*(ss - ss->ply - 1)->continuationHistory)[pc][to] << bonus;
+    if (!(ss->ply & 1))
+        (*(ss - ss->ply - 1)->continuationHistory)[pc][to] << bonus;
 
     for (int i : {1, 2, 3, 4, 6})
     {
