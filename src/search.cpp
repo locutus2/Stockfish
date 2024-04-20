44cfa281274baf5858d7071794295964252ba488
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-04-20 22:43:03 +0200

    Update also cmh with bonus/32 at root. Bench: 1720515

--- src/search.cpp
+++ src/search.cpp
@@ -1767,8 +1767,7 @@ void update_all_stats(const Position& pos,
 // by moves at ply -1, -2, -3, -4, and -6 with current move.
 void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
-    if (!(ss->ply & 1))
-        (*(ss - ss->ply)->continuationHistory)[pc][to] << bonus / 128;
+    (*(ss - ss->ply)->continuationHistory)[pc][to] << bonus / 32;
 
     for (int i : {1, 2, 3, 4, 6})
     {
