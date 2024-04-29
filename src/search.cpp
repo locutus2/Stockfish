2e4abd0659539183709ae8a8f1bdd61974da130d
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-04-29 02:59:04 +0200

    Test tuned conditions for less reduction if no tt hit (C=15, 17 conditions, tuned condition threshold). Use 2 of 4 largest term P[0],P[11]. Bench: 1333220

--- src/search.cpp
+++ src/search.cpp
@@ -1140,7 +1140,7 @@ moves_loop:  // When in check, search starts here
 
         if (   !ss->ttHit
             && move == ss->killers[0]
-            && capture)
+            && type_of(movedPiece) == PAWN)
             r--;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
