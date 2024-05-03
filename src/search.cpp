b90e637b6a2480a462de572ae84c89540266e2ab
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-04 01:29:00 +0200

    Tune conditions for more extension rule (C=15, 17 conditions + threshold). Bench: 1616086

--- src/search.cpp
+++ src/search.cpp
@@ -48,7 +48,7 @@
 namespace Stockfish {
 
 int X = 0;
-int P[18];
+int P[17];
 
 TUNE(SetRange(0, 100), X);
 TUNE(SetRange(-100, 100), P);
@@ -1106,6 +1106,55 @@ moves_loop:  // When in check, search starts here
                                                   [type_of(pos.piece_on(move.to_sq()))]
                           > 3807)
                 extension = 1;
+            else
+            {
+                bool CC = true;
+                if (CC)
+                {
+                    std::vector<bool> C = {
+                      move == ss->killers[0], //PvNode,
+                      cutNode,
+                      move == ss->killers[1], //ss->ttPv,
+                      improving,
+                      ss->ttPv,
+                      PvNode,
+                      priorCapture,
+                      ttCapture,
+                      capture,
+                      givesCheck,
+                      ((ss + 1)->cutoffCnt > 3), //move == ttMove,
+                      type_of(movedPiece) == PAWN,
+                      ss->inCheck,
+                      type_of(movedPiece) == KING,
+                      move == countermove,
+                      move == ttMove,
+                      (ss-1)->currentMove == Move::none(),
+                    };
+
+                    /*
+                    int M = 0;
+                    for (int i = 0; i < int(C.size()); ++i)
+                        M = std::max(M, std::abs(P[i]));
+                    M++;
+                    int Mi = uuu;
+
+                    int X          = nodes % (M-Mi) + Mi;
+                    */
+                    int conditions = 0;
+                    for (int i = 0; i < int(C.size()) && CC; ++i)
+                    {
+                        if (X <= std::abs(P[i]))
+                        {
+                            conditions++;
+                            CC = CC && (P[i] > 0 ? C[i] : !C[i]);
+                        }
+                    }
+
+                    if (CC && conditions > 0)
+                        extension = 1;
+                }
+            }
+
         }
 
         // Add extension to new depth
@@ -1144,53 +1193,6 @@ moves_loop:  // When in check, search starts here
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
             r--;
-
-        bool CC = (ss - 1)->currentMove == Move::null();
-        if (CC)
-        {
-            std::vector<bool> C = {
-              move == ss->killers[0], //PvNode,
-              cutNode,
-              move == ss->killers[1], //ss->ttPv,
-              improving,
-              ss->ttPv,
-              PvNode,
-              priorCapture,
-              ttCapture,
-              capture,
-              givesCheck,
-              ((ss + 1)->cutoffCnt > 3), //move == ttMove,
-              type_of(movedPiece) == PAWN,
-              ss->inCheck,
-              type_of(movedPiece) == KING,
-              move == countermove,
-              move == ttMove,
-              extension > 0,
-              extension < 0,
-            };
-
-            /*
-            int M = 0;
-            for (int i = 0; i < int(C.size()); ++i)
-                M = std::max(M, std::abs(P[i]));
-            M++;
-            int Mi = uuu;
-
-            int X          = nodes % (M-Mi) + Mi;
-            */
-            int conditions = 0;
-            for (int i = 0; i < int(C.size()) && CC; ++i)
-            {
-                if (X <= std::abs(P[i]))
-                {
-                    conditions++;
-                    CC = CC && (P[i] > 0 ? C[i] : !C[i]);
-                }
-            }
-
-            if (CC && conditions > 0)
-                r--;
-        }
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
