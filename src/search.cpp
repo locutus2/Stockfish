1c1bc06f949243b0e581820f5747b94ae29d9378
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-03 22:07:38 +0200

    Test tuned conditions for less reduction if after null move (C=15, 18 conditions, tuned condition threshold). Use only precondition. Bench: 1335817

--- src/search.cpp
+++ src/search.cpp
@@ -1139,6 +1139,9 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r--;
 
+        if ((ss - 1)->currentMove == Move::null())
+            r--;
+
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
