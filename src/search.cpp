e52cf652719ecc3f272b167435ab3fc67a6a47c0
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-04 06:46:44 +0200

    Test tuned conditions for more extensions (C=15, 17 conditions, tuned condition threshold). Use two largest terms P[6],P[14]. Bench: 1561762

--- src/search.cpp
+++ src/search.cpp
@@ -1100,6 +1100,10 @@ moves_loop:  // When in check, search starts here
                                                   [type_of(pos.piece_on(move.to_sq()))]
                           > 3807)
                 extension = 1;
+
+            else if (   priorCapture
+                     && move == countermove)
+                extension = 1;
         }
 
         // Add extension to new depth
