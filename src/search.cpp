d91e199ed4d045320ab104559e5d8811da7dbcea
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-04 10:45:13 +0200

    Test tuned conditions for less futility pruning (C=15, 15 conditions + threshold). Use 3 of 4 largest terms P[13],P[5],P[10] (ignore redudant/contradictive conditions). Bench: 1512888

--- src/search.cpp
+++ src/search.cpp
@@ -1004,7 +1004,8 @@ moves_loop:  // When in check, search starts here
                   ss->staticEval + (bestValue < ss->staticEval - 54 ? 128 : 57) + 131 * lmrDepth;
 
                 // Futility pruning: parent node (~13 Elo)
-                if (!ss->inCheck && lmrDepth < 14 && futilityValue <= alpha && (!PvNode || move != countermove))
+                if (!ss->inCheck && lmrDepth < 14 && futilityValue <= alpha
+                    && (!PvNode || move != countermove || type_of(movedPiece) != PAWN))
                 {
                     if (bestValue <= futilityValue && std::abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY
                         && futilityValue < VALUE_TB_WIN_IN_MAX_PLY)
