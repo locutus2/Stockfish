85468f14e6e8d1dc48a3392c2ee20f2df7caa731
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-05 07:56:18 +0200

    14% more/less eval history update factor if improving/not-improving. Bench: 1434442

--- src/search.cpp
+++ src/search.cpp
@@ -745,7 +745,7 @@ Value Search::Worker::search(
     // Use static evaluation difference to improve quiet move ordering (~9 Elo)
     if (((ss - 1)->currentMove).is_ok() && !(ss - 1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp((-13 - 2 * improving) * int((ss - 1)->staticEval + ss->staticEval),
+        int bonus = std::clamp((-12 - 4 * improving) * int((ss - 1)->staticEval + ss->staticEval),
                                -1644, 1384);
         bonus     = bonus > 0 ? 2 * bonus : bonus / 2;
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
