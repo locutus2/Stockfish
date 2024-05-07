4e46036422b904ddc166942b17cd9061f44de833
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-07 06:06:06 +0200

    Test tuned conditions for more reduction (C=15, 23 conditions, randomly selection proportional to parameter value, max 3 conditions). Use 3 largest terms P[9],P[19],P[1] (simplyfied and combined with PVNode rule, take 2, bench changes on my 1000 position file, functional equivalent to using directly the raw conditions). Bench: 2180675

--- src/search.cpp
+++ src/search.cpp
@@ -1136,11 +1136,8 @@ moves_loop:  // When in check, search starts here
             r++;
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
-        if (PvNode)
+        if (PvNode && (ss->ttHit || type_of(movedPiece) == PAWN))
             r--;
-
-        if (!ss->ttHit && ss->ttPv && type_of(movedPiece) != PAWN)
-            r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
