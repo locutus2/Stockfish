517bb721b64b7a13b153515f699a0035c3d77477
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-07 21:03:45 +0200

    Test after 72K tuned combinations of up to 5 conditions for more singular extension (C=15, 23 conditions, randomly selection proportional to parameter value). Use largest term P[12]. Bench: 2564507

--- src/search.cpp
+++ src/search.cpp
@@ -1058,7 +1058,8 @@ moves_loop:  // When in check, search starts here
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin)
-                              + (value < singularBeta - quadMargin);
+                              + (value < singularBeta - quadMargin)
+                              + (move == ss->killers[0]);
 
                     depth += ((!PvNode) && (depth < 14));
                 }
