7396efbca2a1b05fc7d9621a0b2d0e9415827c60
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-08 21:15:47 +0200

    Variant which allows the additional extension only if current extension < 4. Test after 72K tuned combinations of up to 5 conditions for more singular extension (C=15, 23 conditions, randomly selection proportional to parameter value). Use 5 of 7 largest terms P[12],P[18],P[4],P[17],P[8] (ignore contradictive conditions). Bench: 2367963

--- src/search.cpp
+++ src/search.cpp
@@ -1060,9 +1060,8 @@ moves_loop:  // When in check, search starts here
                               + (value < singularBeta - tripleMargin)
                               + (value < singularBeta - quadMargin);
 
-                    if (extension < 4 && move == ss->killers[0] && !(ss - 1)->ttPv
-                        && (ss + 1)->cutoffCnt <= 3 && !(ss - 1)->ttHit
-                        && type_of(movedPiece) != PAWN && alpha >= ss->staticEval)
+                    if (extension < 4 && move == ss->killers[0] && !(ss - 1)->ttHit && priorCapture
+                        && !(ss - 1)->ttPv && (ss + 1)->cutoffCnt <= 3)
                         extension++;
 
                     depth += ((!PvNode) && (depth < 14));
