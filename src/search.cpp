ad8e0165a5ea23760ba651ae4a64f94faa4e1ab3
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-08 23:51:06 +0200

    No eval history update if previous move is a promotion. This is a refactored version which removes the now redundant promotion check for pawn history update. Bench: 3548926

--- src/search.cpp
+++ src/search.cpp
@@ -736,12 +736,12 @@ Value Search::Worker::search(
     }
 
     // Use static evaluation difference to improve quiet move ordering (~9 Elo)
-    if ((ss - 1)->currentMove.is_ok() && !(ss - 1)->inCheck && !priorCapture && (!ttMove || !pos.gives_check(ttMove)))
+    if ((ss - 1)->currentMove.is_ok() && !(ss - 1)->inCheck && !priorCapture && (ss - 1)->currentMove.type_of() != PROMOTION)
     {
         int bonus = std::clamp(-13 * int((ss - 1)->staticEval + ss->staticEval), -1796, 1526);
         bonus     = bonus > 0 ? 2 * bonus : bonus / 2;
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
-        if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
+        if (type_of(pos.piece_on(prevSq)) != PAWN)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
               << bonus / 2;
     }
