48bec64541a4a3bbc2c8591ca66a3ce3628697b6
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-12 05:33:22 +0200

    In singular search trigger move count pruning 8 moves later. Bench: 2827769

--- src/search.cpp
+++ src/search.cpp
@@ -960,7 +960,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-            moveCountPruning = !excludedMove && moveCount >= futility_move_count(improving, depth);
+            moveCountPruning = moveCount >= futility_move_count(improving, depth) + 8 * bool(excludedMove);
 
             // Reduced depth of the next LMR search
             int lmrDepth = newDepth - r;
