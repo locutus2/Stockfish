45039595f1268176e1fe3e6d8cef0bc4b84fad69
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-14 23:42:12 +0200

    Test tuned combinations of up to 3 conditions for more reduction (C=30, 28 conditions, random proportional selection). Use 3 of 4 largest terms P[3],P[18],P[12]. Bench:

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r--;
 
-        if (improving && type_of(movedPiece) != KING && move != ttMove)
+        if (improving && ttValue <= alpha && move != ttMove)
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
