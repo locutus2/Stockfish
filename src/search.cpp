fa934c1753bfb0a0a28aedc74ac194177bfe3a70
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-20 23:03:31 +0200

    Try first at VVLTC (with LTC bounds) because of vondeles fleet, later i try smaller TC: Test VVLTC tuned combinations of up to 3 conditions for more reduction (C=30, 28 conditions, random proportional selection). Use largest term P[11]. Bench: 1267681

--- src/search.cpp
+++ src/search.cpp
@@ -1134,7 +1134,7 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r--;
 
-        if ((ss - 1)->currentMove == Move::null() && move == ttMove && improving)
+        if ((ss - 1)->currentMove == Move::null())
             r--;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
