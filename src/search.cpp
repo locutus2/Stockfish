1f99810f8eaf5c9ed126a2d431a8fb711528824e
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-24 03:40:51 +0200

    Test tune combinations of up to 2 conditions for more reduction if not at ttPv node (C=50, 30 conditions, random proportional selection). Use 2 largest terms P[3],P[19]. Bench: 1222319

--- src/search.cpp
+++ src/search.cpp
@@ -1146,6 +1146,9 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r++;
 
+        else if (!ss->ttPv && extension >= 0)
+            r++;
+
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
