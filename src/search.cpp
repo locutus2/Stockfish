6647067d4770567caa73f9c1fd6da757b5be8c47
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-05-30 21:53:02 +0200

    Test tuned combinations of up to 3 conditions for more cutoff count reduction if not at ttPv node (C=100, 48 conditions). Use 3 of 4 largest terms P[12],P[2],P[4]. Bench: 1373172

--- src/search.cpp
+++ src/search.cpp
@@ -1146,7 +1146,7 @@ moves_loop:  // When in check, search starts here
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
-        if ((ss + 1)->cutoffCnt > 2 + ((ss - 1)->currentMove == Move::null()))
+        if ((ss + 1)->cutoffCnt > 3 - (type_of(movedPiece) == QUEEN && !ss->ttPv && !cutNode && priorCapture))
             r++;
 
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
