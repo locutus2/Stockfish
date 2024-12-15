3bf636d5b8eddac0cbd0c599e05d2bf781e6f972
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-15 12:01:09 +0100

    Use factor 9/8 for pawn history in quiet move order. Bench: 1130519

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -157,7 +157,7 @@ void MovePicker::score() {
 
             // histories
             m.value = 2 * (*mainHistory)[pos.side_to_move()][m.from_to()];
-            m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
+            m.value += 9 * (*pawnHistory)[pawn_structure_index(pos)][pc][to] / 8;
             m.value += (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
