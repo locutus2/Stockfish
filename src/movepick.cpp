1d80e73556a8f473eee45eea465dc4a6f07f3554
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-21 12:59:04 +0100

    Use factor 2 for cmh0 in evasion move order. Bench: 1074448

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -190,7 +190,7 @@ void MovePicker::score() {
                   PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m)) + (1 << 28);
             else
                 m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
-                        + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
+                        + 2 * (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
                         + (*pawnHistory)[pawn_structure_index(pos)][pos.moved_piece(m)][m.to_sq()];
         }
 }
