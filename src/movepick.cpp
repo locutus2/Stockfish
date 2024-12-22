2dd7c2237946a268237a49a3bad1bfe6c29583bc
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-22 15:17:06 +0100

    Use divisor 20 for good capture checking. Bench: 1111611

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -239,7 +239,7 @@ top:
     case GOOD_CAPTURE :
         if (select([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / 19) ? true
+                return pos.see_ge(*cur, -cur->value / 20) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
