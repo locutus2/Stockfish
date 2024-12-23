815e5dc4bf45d03c667b5efa6a65ff3704f1e7cb
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-23 23:12:28 +0100

    Use divisor 28 (value > 0) and divisor 14 (value <=0) for good capture checking. Bench: 1392879

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -239,7 +239,7 @@ top:
     case GOOD_CAPTURE :
         if (select([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / 28) ? true
+                return pos.see_ge(*cur, -cur->value / (cur->value > 0 ? 28 : 14)) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
