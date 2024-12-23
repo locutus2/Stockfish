ce38d796801d9982e6baed5922c96058c547e41a
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-23 23:20:58 +0100

    Using higher divisior if value > 0 seems good so try the extreme of threshold zero. Bench: 972424

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -239,7 +239,7 @@ top:
     case GOOD_CAPTURE :
         if (select([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / 18) ? true
+                return pos.see_ge(*cur, std::max(-cur->value / 18, 0)) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
