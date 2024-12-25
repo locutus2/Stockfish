85d2d087f8bc06b6cbdbe529148df507bf3cd300
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-25 12:30:20 +0100

    Use factor 7, 4 and 3 for main, cmh and pawn history for evasion move order. Bench: 1197232

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -189,9 +189,10 @@ void MovePicker::score() {
                 m.value =
                   PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m)) + (1 << 28);
             else
-                m.value = 8 * (*mainHistory)[pos.side_to_move()][m.from_to()]
-                        + 4 * (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
-                        + 3 * (*pawnHistory)[pawn_structure_index(pos)][pos.moved_piece(m)][m.to_sq()];
+                m.value =
+                  7 * (*mainHistory)[pos.side_to_move()][m.from_to()]
+                  + 4 * (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
+                  + 3 * (*pawnHistory)[pawn_structure_index(pos)][pos.moved_piece(m)][m.to_sq()];
         }
 }
 
