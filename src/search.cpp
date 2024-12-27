eafc2457c0368c92af3e11e4da6606e41b451bbb
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-27 20:41:05 +0100

    No null move pruning for first two plies. Bench: 1269995

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ Value Search::Worker::search(
     // Step 9. Null move search with verification search (~35 Elo)
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
         && ss->staticEval >= beta - 21 * depth + 421 && !excludedMove && pos.non_pawn_material(us)
-        && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
+        && ss->ply > 1 && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
     {
         assert(eval - beta >= 0);
 
