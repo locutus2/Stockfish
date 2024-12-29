e40e468682c4106d024d778dbba2223d25d806f5
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-29 19:02:31 +0100

    Test ALLNODE[0][1] from my reduction term tuning. Bench: 1042512

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if position is or has been on the PV (~7 Elo)
         if (ss->ttPv)
-            r -= 1024 + (ttData.value > alpha) * 1024 + (ttData.depth >= depth) * 1024;
+            r -= !allNode * 1024 + (ttData.value > alpha) * 1024 + (ttData.depth >= depth) * 1024;
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
