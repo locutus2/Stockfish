f4a792118292d20907faa68261312ebe67073454
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-30 02:03:37 +0100

    Test PVNODE[1][0] from my reduction term tuning. Bench: 1198471

--- src/search.cpp
+++ src/search.cpp
@@ -1161,16 +1161,13 @@ moves_loop:  // When in check, search starts here
 
         r -= std::min(std::abs(correctionValue) / 32768, 2048);
 
-        if (allNode)
-            r += 1024;
-
         // Increase reduction for cut nodes (~4 Elo)
         if (cutNode)
             r += 2518 - (ttData.depth >= depth && ss->ttPv) * 991;
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)
-            r += 1043 + (depth < 8) * 999;
+            r += 1043 - PvNode * 1024 + (depth < 8) * 999;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
