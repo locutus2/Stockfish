069d1b892b4fc0a0b6b4edded2a330370659a18d
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-12-30 16:23:40 +0100

    Tune for the different reduction terms additional reductions dependent on improving/not-improving (40 params, C=100). A parameter > 0 gives one ply more/less reduction. Bench: 1294909

--- src/search.cpp
+++ src/search.cpp
@@ -52,19 +52,18 @@
 
 namespace Stockfish {
 
-int ALLNODE[7][2];
-int CUTNODE[7][2];
-int PVNODE[7][2];
+int IMP[10][2];
+int NOTIMP[10][2];
 
-TUNE(SetRange(-100,100), ALLNODE, CUTNODE, PVNODE);
+TUNE(SetRange(-100,100), IMP, NOTIMP);
 
 #define RED(n) (1024 * (\
                            ( \
-                              allNode * (ALLNODE[(n)][1] > 0) + PvNode * (PVNODE[(n)][1] > 0) + cutNode * (CUTNODE[(n)][1] > 0) \
+                              improving * (IMP[(n)][1] > 0) + !improving * (NOTIMP[(n)][1] > 0) \
                            ) \
                            - \
                            ( \
-                              allNode * (ALLNODE[(n)][0] > 0) + PvNode * (PVNODE[(n)][0] > 0) + cutNode * (CUTNODE[(n)][0] > 0) \
+                              improving * (IMP[(n)][0] > 0) + !improving * (NOTIMP[(n)][0] > 0) \
                            ) \
                 ))
 
@@ -1169,7 +1168,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r -= 1024;
+            r -= -RED(1) + 1024;
 
         // These reduction adjustments have no proven non-linear scaling
 
@@ -1179,19 +1178,19 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~4 Elo)
         if (cutNode)
-            r += 2518 - (ttData.depth >= depth && ss->ttPv) * 991;
+            r += RED(2) + 2518 - (ttData.depth >= depth && ss->ttPv) * 991;
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)
-            r += RED(1) + 1043 + (depth < 8) * 999;
+            r += RED(3) + 1043 + (depth < 8) * 999;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
-            r += RED(2) + 938 + allNode * 960;
+            r += RED(4) + 938 + allNode * 960;
 
         // For first picked move (ttMove) reduce reduction (~3 Elo)
         else if (move == ttData.move)
-            r -= -RED(3) + 1879;
+            r -= -RED(5) + 1879;
 
         if (capture)
             ss->statScore =
@@ -1206,11 +1205,13 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history (~8 Elo)
         r -= ss->statScore * 1287 / 16384;
 
-        r += RED(4);
+        r += RED(6);
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1)
         {
+            r += RED(7);
+
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth.
@@ -1244,9 +1245,9 @@ moves_loop:  // When in check, search starts here
         {
             // Increase reduction if ttMove is not present (~6 Elo)
             if (!ttData.move)
-                r += RED(5) + 2037;
+                r += RED(8) + 2037;
 
-            r += RED(6);
+            r += RED(9);
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
             value =
