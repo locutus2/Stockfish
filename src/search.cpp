0525f836a09382429e949dceade8d054f6b3fd14
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-02 06:03:02 +0100

    Test the most significant parameter ALLNODE[4] of my reduction term plies tuning. Bench: 979018

--- src/search.cpp
+++ src/search.cpp
@@ -1193,6 +1193,9 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1)
         {
+            if (allNode)
+                r += 93;
+
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth.
