23ea65556ee566f6d89a65c08f3282bed77d46e8
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-10 18:46:43 +0100

    Test the second most significant parameter CUTNODE[0] of my reduction term plies tuning together with the corresponding PVNODE[0] and ALLNODE[0] and finally simplify. Bench: 942269

--- src/search.cpp
+++ src/search.cpp
@@ -1168,7 +1168,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)
-            r += 1043 + (depth < 8) * 999 + cutNode * (2 * 87);
+            r += 1060 + (depth < 8) * 999 + cutNode * 73;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
