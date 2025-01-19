18b465513f3b784d6607856b1e3fd37c77fc9e5c
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-19 19:30:50 +0100

    Replace in my PR 5807 the term moveCount * 64 with an array (cap at move count 63) and tune it (64 parameters, C=102.4).
    Bench: 1803474

--- src/search.cpp
+++ src/search.cpp
@@ -52,6 +52,24 @@
 
 namespace Stockfish {
 
+constexpr int S = 64;
+
+int mcReduction[64] = {
+    S*0, S*1, S*2, S*3, S*4, S*5, S*6, S*7, S*8, S*9, 
+    S*10, S*11, S*12, S*13, S*14, S*15, S*16, S*17, S*18, S*19, 
+    S*20, S*21, S*22, S*23, S*24, S*25, S*26, S*27, S*28, S*29, 
+    S*30, S*31, S*32, S*33, S*34, S*35, S*36, S*37, S*38, S*39, 
+    S*40, S*41, S*42, S*43, S*44, S*45, S*46, S*47, S*48, S*49, 
+    S*50, S*51, S*52, S*53, S*54, S*55, S*56, S*57, S*58, S*59, 
+    S*60, S*61, S*62, S*63
+};
+
+constexpr int DELTA = 1024;
+
+Range centeredRange(int v) { return Range(v - DELTA, v + DELTA); }
+
+TUNE(SetRange(centeredRange), mcReduction);
+
 namespace TB = Tablebases;
 
 void syzygy_extend_pv(const OptionsMap&            options,
@@ -1155,7 +1173,9 @@ moves_loop:  // When in check, search starts here
 
         // These reduction adjustments have no proven non-linear scaling
 
-        r += 307 - moveCount * 64;
+        r += 307;
+       
+        r -= mcReduction[std::min(moveCount, 63)];
 
         r -= std::abs(correctionValue) / 34112;
 
