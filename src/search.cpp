2073c643edc3f8b5a6e0aef340855f2e362a4832
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-19 23:53:09 +0100

    More reduction in LMR if depth >= 12 and ttCapture exists or no tt hit. Bench: 1360101

--- src/search.cpp
+++ src/search.cpp
@@ -1191,6 +1191,9 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1)
         {
+            if (depth >= 12 && (ttCapture || !ss->ttHit))
+                r += 1024;
+
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth.
