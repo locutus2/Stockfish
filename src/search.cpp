5aad5cab6da004afa8735faf9dafc6654c7bf466
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-19 01:28:20 +0100

    More reduction in LMR at non-PV nodes if inCheck + capture + ttPv >= 2. Bench: 1436282

--- src/search.cpp
+++ src/search.cpp
@@ -1191,6 +1191,9 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1)
         {
+            if (!PvNode && ss->inCheck + capture + ss->ttPv >= 2)
+                r += 1024;
+
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth.
