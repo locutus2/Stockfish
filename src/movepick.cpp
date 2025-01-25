58670a8c6cd6a857b946a228737df184e5c2d8e8
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-25 16:30:23 +0100

    Add king history (indexed by both king files) for quiet move ordering (for compensation half weight of main history there). Use history divisor D=7183 like main history. Bench: 1364003

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -158,8 +158,8 @@ void MovePicker::score() {
             Square    to   = m.to_sq();
 
             // histories
-            m.value = 2 * (*mainHistory)[pos.side_to_move()][m.from_to()];
-            m.value += (*pawnHistory)[pawn_structure_index(pos)][pc][to];
+            m.value = (*mainHistory)[pos.side_to_move()][m.from_to()];
+            m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
             m.value += (*kingHistory)[pc][to];
             m.value += (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
