eb187c333a9b4443169aeec15de6caca8a65dec8
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-26 21:41:57 +0100

    Double correction update limit at PV nodes. Bench: 1882972

--- src/search.cpp
+++ src/search.cpp
@@ -1434,9 +1434,8 @@ moves_loop:  // When in check, search starts here
     {
         const auto    m             = (ss - 1)->currentMove;
         constexpr int nonPawnWeight = 165;
-
-        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8,
-                                -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+        constexpr int limit         = CORRECTION_HISTORY_LIMIT / (PvNode ? 2 : 4);
+        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8, -limit, limit);
         thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)]
           << bonus * 114 / 128;
         thisThread->majorPieceCorrectionHistory[us][major_piece_index(pos)] << bonus * 163 / 128;
