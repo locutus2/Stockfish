077de28dd018694b9c6edf569123a5d45cc10986
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-27 22:43:41 +0100

    Use higher divisor for correction update if not PV node. Bench: 1722893

--- src/search.cpp
+++ src/search.cpp
@@ -1432,7 +1432,7 @@ moves_loop:  // When in check, search starts here
         const auto    m             = (ss - 1)->currentMove;
         constexpr int nonPawnWeight = 165;
 
-        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / (8 - PvNode),
+        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / (9 - PvNode),
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)]
           << bonus * 114 / 128;
