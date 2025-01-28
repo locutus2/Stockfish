a7f5a9860ad112392bee33860e447b2a1c9fa491
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-28 22:59:01 +0100

    The correction value seems to have a bias so subtract it. Bench: 2199983

--- src/search.cpp
+++ src/search.cpp
@@ -94,7 +94,7 @@ int correction_value(const Worker& w, const Position& pos, const Stack* ss) {
       m.is_ok() ? (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                  : 0;
 
-    return (7000 * pcv + 6300 * micv + 7550 * (wnpcv + bnpcv) + 6320 * cntcv);
+    return 7000 * pcv + 6300 * micv + 7550 * (wnpcv + bnpcv) + 6320 * cntcv - 1372331;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
