3b0677504a9ae0fb7c7a0bd393806880ed996d5b
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-01 00:59:48 +0100

    Triple effect: Give order bonus for capture checks proportional to captured piece value. Bench: 1980836

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -145,7 +145,8 @@ void MovePicker::score() {
     for (auto& m : *this)
         if constexpr (Type == CAPTURES)
             m.value =
-              (2 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & m.to_sq()) + 7) * int(PieceValue[pos.piece_on(m.to_sq())])
+              (7 + 3 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & m.to_sq()))
+                * int(PieceValue[pos.piece_on(m.to_sq())])
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
 
         else if constexpr (Type == QUIETS)
