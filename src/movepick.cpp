7f3280f72d7a21193f7ccc6a0c085987f6530a0e
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-01-31 22:20:31 +0100

    12.5% less quiet move order bonus if not double check. Bench: 2732643

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -167,7 +167,7 @@ void MovePicker::score() {
 
             // bonus for checks
             m.value += bool(pos.check_squares(pt) & to)
-                     * (8 + bool(pos.blockers_for_king(~pos.side_to_move()) & from)) * 2048;
+                     * (7 + bool(pos.blockers_for_king(~pos.side_to_move()) & from)) * 2048;
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
