b176fa5a3938e14b08c4db102782a5a23fa921cc
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-03 10:00:05 +0100

    Test tuned values. Get reduction correction now for each searched move freshly (fixed version). Bench: 2758683

--- src/search.cpp
+++ src/search.cpp
@@ -139,7 +139,7 @@ int reduction_correction_value(const Worker& w, const Position& pos, const Stack
         ? (*(ss - 2)->continuationReductionCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
         : 0;
 
-    return (7037 * pcv + 6671 * micv + 7631 * (wnpcv + bnpcv) + 6362 * cntcv) / 131072;
+    return (7221 * pcv + 6270 * micv + 7280 * (wnpcv + bnpcv) + 6523 * cntcv) / 131072;
 }
 
 void update_reduction_correction_history(const Position& pos,
@@ -149,12 +149,12 @@ void update_reduction_correction_history(const Position& pos,
     const Move  m  = (ss - 1)->currentMove;
     const Color us = pos.side_to_move();
 
-    static constexpr int nonPawnWeight = 159;
+    static constexpr int nonPawnWeight = 163;
 
     workerThread.pawnReductionCorrectionHistory[pawn_structure_index<Correction>(pos)][us]
-      << bonus * 104 / 128;
+      << bonus * 106 / 128;
     workerThread.minorPieceReductionCorrectionHistory[minor_piece_index<Reduction>(pos)][us]
-      << bonus * 145 / 128;
+      << bonus * 155 / 128;
     workerThread.nonPawnReductionCorrectionHistory[WHITE][non_pawn_index<WHITE, Reduction>(pos)][us]
       << bonus * nonPawnWeight / 128;
     workerThread.nonPawnReductionCorrectionHistory[BLACK][non_pawn_index<BLACK, Reduction>(pos)][us]
@@ -162,7 +162,7 @@ void update_reduction_correction_history(const Position& pos,
 
     if (m.is_ok())
         (*(ss - 2)->continuationReductionCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
-          << bonus * 146 / 128;
+          << bonus * 159 / 128;
 }
 
 // History and stats update bonus, based on depth
@@ -1269,6 +1269,8 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 1451 / 16384;
 
+        int reductionBonus = 0;
+
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
@@ -1306,10 +1308,9 @@ moves_loop:  // When in check, search starts here
 
                     if (failLowLMR || value <= alpha)
                     {
-                        int bonus = std::clamp((value > alpha ? -1024 : 1024) * d / 8,
-                                               -REDUCTION_CORRECTION_HISTORY_LIMIT / 4,
-                                               REDUCTION_CORRECTION_HISTORY_LIMIT / 4);
-                        update_reduction_correction_history(pos, ss, *thisThread, bonus);
+                        reductionBonus = std::clamp((value > alpha ? -1039 : 1106) * d / 8,
+                                                    -REDUCTION_CORRECTION_HISTORY_LIMIT / 4,
+                                                    REDUCTION_CORRECTION_HISTORY_LIMIT / 4);
                     }
                 }
 
@@ -1349,6 +1350,9 @@ moves_loop:  // When in check, search starts here
         pos.undo_move(move);
 
         assert(value > -VALUE_INFINITE && value < VALUE_INFINITE);
+
+        if (reductionBonus)
+            update_reduction_correction_history(pos, ss, *thisThread, reductionBonus);
 
         // Step 20. Check for a new best move
         // Finished searching the move. If a stop occurred, the return value of
