c04dc85d132beda817e5be818b04f874a02160ae
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-03 22:09:28 +0100

    Test tuned fixed reverse login reduction history. Bench: 2454848

--- src/search.cpp
+++ src/search.cpp
@@ -139,7 +139,7 @@ int reduction_correction_value(const Worker& w, const Position& pos, const Stack
         ? (*(ss - 2)->continuationReductionCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
         : 0;
 
-    return (7037 * pcv + 6671 * micv + 7631 * (wnpcv + bnpcv) + 6362 * cntcv) / 131072;
+    return (7223 * pcv + 6289 * micv + 7216 * (wnpcv + bnpcv) + 6794 * cntcv) / 131072;
 }
 
 void update_reduction_correction_history(const Position& pos,
@@ -149,12 +149,12 @@ void update_reduction_correction_history(const Position& pos,
     const Move  m  = (ss - 1)->currentMove;
     const Color us = pos.side_to_move();
 
-    static constexpr int nonPawnWeight = 159;
+    static constexpr int nonPawnWeight = 162;
 
     workerThread.pawnReductionCorrectionHistory[pawn_structure_index<Correction>(pos)][us]
-      << bonus * 104 / 128;
+      << bonus * 116 / 128;
     workerThread.minorPieceReductionCorrectionHistory[minor_piece_index<Reduction>(pos)][us]
-      << bonus * 145 / 128;
+      << bonus * 150 / 128;
     workerThread.nonPawnReductionCorrectionHistory[WHITE][non_pawn_index<WHITE, Reduction>(pos)][us]
       << bonus * nonPawnWeight / 128;
     workerThread.nonPawnReductionCorrectionHistory[BLACK][non_pawn_index<BLACK, Reduction>(pos)][us]
@@ -162,7 +162,7 @@ void update_reduction_correction_history(const Position& pos,
 
     if (m.is_ok())
         (*(ss - 2)->continuationReductionCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
-          << bonus * 146 / 128;
+          << bonus * 147 / 128;
 }
 
 // History and stats update bonus, based on depth
@@ -1301,7 +1301,7 @@ moves_loop:  // When in check, search starts here
                 {
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
-                    reductionBonus = std::clamp((value > alpha ? 1024 : -1024) * d / 8,
+                    reductionBonus = std::clamp((value > alpha ? 961 : -1005) * d / 8,
                                                 -REDUCTION_CORRECTION_HISTORY_LIMIT / 4,
                                                 REDUCTION_CORRECTION_HISTORY_LIMIT / 4);
                 }
