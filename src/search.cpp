47e4c2bc71cdaa103ff8a82b0de824f71f3b030a
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-05 22:56:29 +0100

    More reduction if at least two LMR researches failed. Bench: 2663479

--- src/search.cpp
+++ src/search.cpp
@@ -1208,7 +1208,7 @@ moves_loop:  // When in check, search starts here
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
 
-            if (failedLMRResearch > 0)
+            if (failedLMRResearch > 1)
                 r += 1024;
 
             Depth d = std::max(
