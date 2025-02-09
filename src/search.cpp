e3cb164d3b712735a3063de7d2ff8af1eac2dc3a
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-09 13:15:41 +0100

    Add momentum to correction value only if last move was the first move. Bench: 2722810

--- src/search.cpp
+++ src/search.cpp
@@ -750,7 +750,8 @@ Value Search::Worker::search(
     Value unadjustedStaticEval = VALUE_NONE;
     ss->correctionValue        = correction_value(*thisThread, pos, ss);
     const auto correctionValue =
-      ss->correctionValue + (ss->correctionValue - (ss - 2)->correctionValue) / 8;
+      ss->correctionValue
+      + ((ss - 1)->moveCount == 1 ? (ss->correctionValue - (ss - 2)->correctionValue) / 8 : 0);
     if (ss->inCheck)
     {
         // Skip early pruning when in check
@@ -1535,7 +1536,8 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
     Value unadjustedStaticEval = VALUE_NONE;
     ss->correctionValue        = correction_value(*thisThread, pos, ss);
     const auto correctionValue =
-      ss->correctionValue + (ss->correctionValue - (ss - 2)->correctionValue) / 8;
+      ss->correctionValue
+      + ((ss - 1)->moveCount == 1 ? (ss->correctionValue - (ss - 2)->correctionValue) / 8 : 0);
     if (ss->inCheck)
         bestValue = futilityBase = -VALUE_INFINITE;
     else
