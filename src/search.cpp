ccf13824a4fedbb704d8927bf3114c3504822196
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-10 15:01:20 +0100

    Double correction limit. For compensation half correction value factors. Bench: 3052556

--- src/search.cpp
+++ src/search.cpp
@@ -94,7 +94,7 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
       m.is_ok() ? (*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                  : 0;
 
-    return 6995 * pcv + 6593 * micv + 7753 * (wnpcv + bnpcv) + 6049 * cntcv;
+    return 3498 * pcv + 3297 * micv + 3877 * (wnpcv + bnpcv) + 3025 * cntcv;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
