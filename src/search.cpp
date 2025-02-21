1010bc281b0e07bb0d38e8e5ac97e1434a71791e
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-21 16:43:28 +0100

    No quiet history pruning at ttPv nodes if not in check. Bench: 2351895

--- src/search.cpp
+++ src/search.cpp
@@ -1048,11 +1048,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4107 * depth
-                    && (!ss->ttPv || (ss - 1)->statScore > 0 || moveCount > 6
-                        || ttData.value <= alpha || depth > 5 || !improving
-                        || (ss - 1)->currentMove == Move::null() || (ss - 1)->excludedMove
-                        || ttCapture))
+                if (history < -4107 * depth && (!ss->ttPv || ss->inCheck))
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
