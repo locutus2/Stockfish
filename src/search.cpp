92a1542873bc5f2618b98bf73ec469c1ee69b735
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-24 01:19:32 +0100

    Apply move count reduction rule only if not ttpv. Bench: 2659932

--- src/search.cpp
+++ src/search.cpp
@@ -1168,7 +1168,10 @@ moves_loop:  // When in check, search starts here
 
         // These reduction adjustments have no proven non-linear scaling
 
-        r += 316 - moveCount * 32;
+        r += 316;
+
+        if (!ss->ttPv)
+            r -= moveCount * 32;
 
         r -= std::abs(correctionValue) / 31568;
 
