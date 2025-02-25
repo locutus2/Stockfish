188dc83ffc862780a79d08361652926c52e317d5
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-25 23:03:26 +0100

    No quiet history pruning under a bunch of conditions. Take 3. Bench: 2386583

--- src/search.cpp
+++ src/search.cpp
@@ -1066,7 +1066,10 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4107 * depth)
+                if (history < -4107 * depth
+                    && (moveCount > 2 || !cutNode || ttCapture || depth > 8 || !(ss - 1)->ttHit
+                        || (ss - 1)->statScore > -5902 || (ss - 1)->moveCount <= 5
+                        || (*contHist[3])[movedPiece][move.to_sq()] <= 0))
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
