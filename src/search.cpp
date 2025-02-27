3ea54c04f070de40c308514782a909dd15c732d9
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-02-27 10:28:59 +0100

    Decrease even further cutoffCnt reduction rule threshold at ALL nodes. Bench: 1865434

--- src/search.cpp
+++ src/search.cpp
@@ -1204,7 +1204,7 @@ moves_loop:  // When in check, search starts here
             r += 1123 + (depth < 8) * 982;
 
         // Increase reduction if next ply has a lot of fail high
-        if ((ss + 1)->cutoffCnt > 3 - 2 * allNode)
+        if ((ss + 1)->cutoffCnt > 3 - 3 * allNode)
             r += 981 + allNode * 833;
 
         // For first picked move (ttMove) reduce reduction
