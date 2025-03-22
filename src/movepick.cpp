8d7a269aec67962e7d8724417f87740cbded1635
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-03-22 18:46:19 +0100

    8x bonus: Order bonus for pawn pushes which attacks an opponent piece (excluding pawns). But ignore squares which are defended by an opponent pawn. Bench: 1859292

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -173,7 +173,7 @@ void MovePicker::score() {
             m.value += bool(pos.check_squares(pt) & to) * 16384;
 
             // bonus for pawn moves which attack an opponent piece (excluding pawns)
-            m.value += (pt == PAWN && pawnAttackSquaresOnOpponent & to) * 16384;
+            m.value += (pt == PAWN && to & pawnAttackSquaresOnOpponent) * 32768;
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
