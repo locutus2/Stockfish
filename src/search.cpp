c1a2300a05ea603cfe41523ba183b56f941bbfce
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-04-04 00:57:29 +0200

    Change index scheme of tt move history to piece/to indices (apply also the bestmove fix). Bench: 1961737

--- src/search.cpp
+++ src/search.cpp
@@ -1142,7 +1142,7 @@ moves_loop:  // When in check, search starts here
                     int corrValAdj1  = std::abs(correctionValue) / 248873;
                     int corrValAdj2  = std::abs(correctionValue) / 255331;
                     int doubleMargin = 262 * PvNode - 188 * !ttCapture - corrValAdj1
-                                     - ttMoveHistory[pawn_structure_index(pos)][us] / 128;
+                                     - ttMoveHistory[movedPiece][move.to_sq()] / 128;
                     int tripleMargin =
                       88 + 265 * PvNode - 256 * !ttCapture + 93 * ss->ttPv - corrValAdj2;
 
@@ -1439,7 +1439,7 @@ moves_loop:  // When in check, search starts here
         if (!PvNode)
         {
             int bonus = (ttData.move == bestMove) ? 800 : -600 * moveCount;
-            ttMoveHistory[pawn_structure_index(pos)][us] << bonus;
+            ttMoveHistory[pos.moved_piece(ttData.move)][ttData.move.to_sq()] << bonus;
         }
     }
 
