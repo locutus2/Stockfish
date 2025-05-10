33d80c7985cb885c9d3df426808897c818453f8f
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-11 00:54:34 +0200

    Try opposite direction: Modify good capture SEE threshold by eval margin (static eval - alpha) but scale down with depth. Take 3 (faster down scaling). Test against PR 6047. Bench: 1926188

--- src/search.cpp
+++ src/search.cpp
@@ -995,7 +995,7 @@ moves_loop:  // When in check, search starts here
 
     MovePicker mp(pos, ttData.move, depth, &thisThread->mainHistory, &thisThread->lowPlyHistory,
                   &thisThread->captureHistory, contHist, &thisThread->pawnHistory, ss->ply,
-                  (ss->staticEval - alpha) * 16 / (depth + 15));
+                  (ss->staticEval - alpha) * 8 / (depth + 7));
 
     value = bestValue;
 
