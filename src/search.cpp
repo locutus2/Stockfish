33d3275f47e9eb6d6a1c2f747400fdaf3e55cc7b
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-22 14:14:50 +0200

    Add new continuation like history for skipping LMR (threshold = 25000 + max(reduction, 0)).  Bench: 2486498

--- src/search.cpp
+++ src/search.cpp
@@ -1255,7 +1255,8 @@ moves_loop:  // When in check, search starts here
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1
-            && (capture || ss->inCheck || (*ss->skipLmrHistory)[movedPiece][move.to_sq()] < 25000))
+            && (capture || ss->inCheck
+                || (*ss->skipLmrHistory)[movedPiece][move.to_sq()] < 25000 + std::max(r, 0)))
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
