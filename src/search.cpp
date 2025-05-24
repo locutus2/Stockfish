6eeaacdf33dd28904f04ae9bd3e7168a085956f2
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-24 20:59:20 +0200

    Less quiet history pruning at cut nodes. Bench: 2100947

--- src/search.cpp
+++ src/search.cpp
@@ -1094,7 +1094,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4229 * depth - 29000 * cutNode + 6500)
+                if (history < -4229 * depth - 1024 * cutNode)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
