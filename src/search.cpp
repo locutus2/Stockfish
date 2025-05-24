e9802c5c9b3cd359daa074f7e50fcc347d2c1335
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-24 11:30:50 +0200

    More/less quiet history pruning if priorCapture/not-priorCapture. Bench: 2219209

--- src/search.cpp
+++ src/search.cpp
@@ -1094,7 +1094,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4229 * depth)
+                if (history < -4229 * depth + 33000 * priorCapture - 16500)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
