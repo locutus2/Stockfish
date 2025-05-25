1d97c944aff7c0ad76bd47b37c254a544611d2aa
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-25 16:57:58 +0200

    Half effect: Less quiet history pruning at cut nodes (depth dependent). Bench: 2337471

--- src/search.cpp
+++ src/search.cpp
@@ -1094,7 +1094,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < (-4229 - 256 * cutNode) * depth)
+                if (history < (-4229 - 128 * cutNode) * depth)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
