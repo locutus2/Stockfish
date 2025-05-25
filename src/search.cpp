5e9179a6d29b89d6c0c0fc80f2b1bcf42fc3b66e
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-25 22:43:51 +0200

    Eigth effect: More history pruning if in check (depth dependent). Bench: 2251970

--- src/search.cpp
+++ src/search.cpp
@@ -1088,7 +1088,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < (-4229 + 2048 * ss->inCheck) * depth)
+                if (history < (-4229 + 256 * ss->inCheck) * depth)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
