aef3988bb29e799548ec8807d09465f8127b7fd9
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-26 20:52:32 +0200

    Extreme version with factor 4096: More history pruning if in check (depth dependent) + compensation. Bench: 2257684

--- src/search.cpp
+++ src/search.cpp
@@ -1088,7 +1088,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < (-4301 + 128 * ss->inCheck) * depth)
+                if (history < (-7437 + 4096 * ss->inCheck) * depth)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
