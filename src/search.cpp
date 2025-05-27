afba2520a8e7ef5c3e35bb31183c1314eda7f23b
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-27 12:28:29 +0200

    Factor=4096, only depth <= 1: More history pruning if at ALL node or prior capture + compensation. Bench: 2337648

--- src/search.cpp
+++ src/search.cpp
@@ -1088,7 +1088,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4229 * depth + (depth <= 1) * (2336 - 4096 * !(allNode || priorCapture)))
+                if (history < -4229 * depth + (depth <= 1) * (4096 * (allNode || priorCapture) - 1760))
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
