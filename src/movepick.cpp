3efce8391fd1d209e4532747f070ba68d8a92c2f
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-28 18:13:13 +0200

    Give order bonus (1024) for capturing a pinner. Bench: 2318322

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -152,7 +152,8 @@ void MovePicker::score() {
 
         if constexpr (Type == CAPTURES)
             m.value = (*captureHistory)[pc][to][type_of(capturedPiece)]
-                    + 7 * int(PieceValue[capturedPiece]) + 1024 * bool(pos.check_squares(pt) & to);
+                    + 7 * int(PieceValue[capturedPiece]) + 1024 * bool(pos.check_squares(pt) & to)
+		    + 1024 * (to & pos.pinners(~us));
 
         else if constexpr (Type == QUIETS)
         {
