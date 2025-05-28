0f92b2dd8ee0f1c800990e2bf2503b5518f85e04
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-05-28 08:56:32 +0200

    Replace quiet history pruning threshold by a term dependent on lmrDepth. But add a cap to avoid pruning of moves with positive history. Bench: 2126471

--- src/search.cpp
+++ src/search.cpp
@@ -1088,7 +1088,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -5172 * lmrDepth - 9823)
+                if (history < -5172 * std::max(lmrDepth, -1) - 9823)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
