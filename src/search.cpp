f71aa36db95be8fd7ecd708e1e52aaf8fa8abf6b
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-06-01 19:01:05 +0200

    In singular search isPvNode is set to false but never set back to the old value. Fix this. Bench: 2300655

--- src/search.cpp
+++ src/search.cpp
@@ -1140,6 +1140,7 @@ moves_loop:  // When in check, search starts here
             ss->excludedMove = move;
             value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
             ss->excludedMove = Move::none();
+            ss->isPvNode     = PvNode;
 
             if (value < singularBeta)
             {
