c43ff792daf1444734c98d410690075e7b3b3daf
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-06-03 15:51:33 +0200

    Only for LMR: More reduction at non-PV nodes if depth >= 14 and moveCount >= 22. Bench: 2326640

--- src/search.cpp
+++ src/search.cpp
@@ -1230,6 +1230,9 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
+            if (!PvNode && depth >= 14 && moveCount >= 22)
+                r += 1024;
+
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth.
