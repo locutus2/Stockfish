7a76bd46076807c3d331ae125907515168343a17
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-06-08 08:38:13 +0200

    Only for LMR: Less reduction proportional to priorReduction (factor 256). Bench: 2103132

--- src/search.cpp
+++ src/search.cpp
@@ -1230,8 +1230,7 @@ moves_loop:  // When in check, search starts here
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
-            if (priorReduction >= 6)
-                r -= 1024;
+            r -= 256 * priorReduction;
 
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
