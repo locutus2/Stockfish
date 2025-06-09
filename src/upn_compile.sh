fa59b238abb41682970eb97fbdb16354e8d9f905
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-06-09 22:50:14 +0200

    add 1000 to compile

--- src/upn_compile.sh
+++ src/upn_compile.sh
@@ -4,6 +4,8 @@ touch search.cpp && make build fail_low=no research=no n_conditions=100 && mv st
 touch search.cpp && make build fail_low=yes research=no n_conditions=100 && mv stockfish stockfish_upn_fl_100
 touch search.cpp && make build fail_low=no research=no n_conditions=2500 && mv stockfish stockfish_upn_fh_2500
 touch search.cpp && make build fail_low=yes research=no n_conditions=2500 && mv stockfish stockfish_upn_fl_2500
+touch search.cpp && make build fail_low=no research=no n_conditions=1000 && mv stockfish stockfish_upn_fh_1000
+touch search.cpp && make build fail_low=yes research=no n_conditions=1000 && mv stockfish stockfish_upn_fl_1000
 touch search.cpp && make build fail_low=no research=no n_conditions=200 && mv stockfish stockfish_upn_fh_200
 touch search.cpp && make build fail_low=yes research=no n_conditions=200 && mv stockfish stockfish_upn_fl_200
 touch search.cpp && make build fail_low=no research=no n_conditions=400 && mv stockfish stockfish_upn_fh_400
