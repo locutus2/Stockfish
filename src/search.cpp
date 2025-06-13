d57ea3bb7a53f18cd2772874cc070b1355963e36
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-06-13 09:28:10 +0200

    Even more reduction if ttCapture exists and move count >= 11. Bench: 2560169

--- src/search.cpp
+++ src/search.cpp
@@ -1201,7 +1201,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
-            r += 1210 + (depth < 8) * 963;
+            r += 1210 + (depth < 8) * 963 + (moveCount >= 11) * 1024;
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 2)
