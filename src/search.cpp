35c6aecc7cfc983a5cf3328dc25c136c783c3e3b
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-06-20 19:00:51 +0200

    Apply move count reduction only for non-captures. Bench: 2179052

--- src/search.cpp
+++ src/search.cpp
@@ -1194,9 +1194,7 @@ moves_loop:  // When in check, search starts here
         r += 316;  // Base reduction offset to compensate for other tweaks
         r -= std::abs(correctionValue) / 28047;
 
-        if (capture)
-            r -= 495;
-        else
+        if (!capture)
             r -= moveCount * 66;
 
         // Increase reduction for cut nodes
