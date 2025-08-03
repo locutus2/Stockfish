11d5913be98d1c9fe1807daea33e7d240efadbf9
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-03 22:08:39 +0200

    More/less reduction (up to a full ply) near leafs/root. But use ply+depth instead of root depth as divisor. Bench: 3513911

--- src/search.cpp
+++ src/search.cpp
@@ -1212,7 +1212,7 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 789 / 8192;
 
-        r += (ss->ply - depth) * 256 / (ss->ply + depth);
+        r += (ss->ply - depth) * 1024 / (ss->ply + depth);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
