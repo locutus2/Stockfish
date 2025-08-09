aee8cc22c97e21f2a575e863b96467ad9af739ef
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-09 10:33:56 +0200

    Don't call qsearch if at PV node and in check. Bench: 2895363

--- src/search.cpp
+++ src/search.cpp
@@ -582,7 +582,7 @@ Value Search::Worker::search(
     // Dive into quiescence search when the depth reaches zero
     if (depth <= 0)
     {
-        if ((ss - 1)->moveCount != 1 || !pos.captured_piece())
+        if (!PvNode || !pos.checkers())
         {
             constexpr auto nt = PvNode ? PV : NonPV;
             return qsearch<nt>(pos, ss, alpha, beta);
