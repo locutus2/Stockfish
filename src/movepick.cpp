7514390e4979b06b0d2bd9ef1e38cc21a90c929a
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-13 11:16:46 +0200

    50% more effect: Add quiet order bonus for pawns dependent on rule 50 count. Bench: 2975758

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -177,7 +177,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
                 m.value += bonus[pt] * v;
             }
             else if (pt == PAWN)
-                m.value += pos.rule50_count() * 640;
+                m.value += pos.rule50_count() * 768;
 
             if (ply < LOW_PLY_HISTORY_SIZE)
                 m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
