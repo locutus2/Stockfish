65b0cc506d2b4ea59aef82e5d41c1e0a22b8a172
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-17 13:46:25 +0200

    If iteration fail high and at least one best move change occurs then increase also alpha. Use min(best value - delta, beta). Bench: 3029867

--- src/search.cpp
+++ src/search.cpp
@@ -380,7 +380,7 @@ void Search::Worker::iterative_deepening() {
                 else if (bestValue >= beta)
                 {
                     if (bestMoveChanges > previousBestMoveChanges)
-                        alpha = std::max(bestValue - delta, alpha);
+                        alpha = std::min(std::max(bestValue - delta, -VALUE_INFINITE), beta);
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
