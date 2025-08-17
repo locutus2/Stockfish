c441937759bd37aa69a562f50fbc3999452436ca
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-17 09:53:19 +0200

    More history pruning for later threads. Bench: 2996176

--- src/search.cpp
+++ src/search.cpp
@@ -1071,7 +1071,7 @@ moves_loop:  // When in check, search starts here
                             + pawnHistory[pawn_history_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4361 * depth)
+                if (history < -4361 * depth + int(threadIdx % 8) * 512)
                     continue;
 
                 history += 71 * mainHistory[us][move.from_to()] / 32;
