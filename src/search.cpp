9f00ef9f8f883cf0ff954eb81c4358ffbdb42266
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-28 16:57:38 +0200

    Inspired by my bmc window tuning try a simple version: after fail low use alpha = (7 * alpha + std::min(bestValue + delta, VALUE_INFINITE)) / 8. Bench: 2380775

--- src/search.cpp
+++ src/search.cpp
@@ -369,7 +369,7 @@ void Search::Worker::iterative_deepening() {
                 // otherwise exit the loop.
                 if (bestValue <= alpha)
                 {
-                    beta  = alpha;
+                    beta  = (7 * alpha + std::min(bestValue + delta, VALUE_INFINITE)) / 8;
                     alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                     failedHighCnt = 0;
