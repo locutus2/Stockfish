1ede4ff6444fec7fb9c35e1646117dc11019fefd
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-08-30 04:48:17 +0200

    Adter fail low use beta  = std::max(alpha, (3 * alpha + std::min(bestValue + delta, VALUE_INFINITE)) / 4). Bench: 2254897

--- src/search.cpp
+++ src/search.cpp
@@ -369,7 +369,7 @@ void Search::Worker::iterative_deepening() {
                 // otherwise exit the loop.
                 if (bestValue <= alpha)
                 {
-                    beta  = std::max(alpha, (7 * alpha + std::min(bestValue + delta, VALUE_INFINITE)) / 8);
+                    beta  = std::max(alpha, (3 * alpha + std::min(bestValue + delta, VALUE_INFINITE)) / 4);
                     alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                     failedHighCnt = 0;
