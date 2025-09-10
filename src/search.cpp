68fbe008fb7b664acf2abc21658c6cc3393ec7aa
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-09-10 23:22:01 +0200

    Quadruple effect: Less capture SEE pruning for better side (at least 100 units better) dependent on rule 50 count and advantage. Bench: 2709511

--- src/search.cpp
+++ src/search.cpp
@@ -1066,7 +1066,7 @@ moves_loop:  // When in check, search starts here
                 // SEE based pruning for captures and checks
                 // Avoid pruning sacrifices of our last piece for stalemate
                 int margin = std::max(157 * depth + captHist / 29, 0)
-                           + capture * std::max(bestValue - 300, 0) * pos.rule50_count() / 32;
+                           + capture * std::max(bestValue - 100, 0) * pos.rule50_count() / 32;
                 if ((alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
                     && !pos.see_ge(move, -margin))
                     continue;
