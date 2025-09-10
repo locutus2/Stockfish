c45b8fa9e5af43a94049854a870e4a02dc196115
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-09-11 01:49:19 +0200

    Less reduction proportional to product of current and previous own move count. Bench: 2791893

--- src/search.cpp
+++ src/search.cpp
@@ -1183,8 +1183,7 @@ moves_loop:  // When in check, search starts here
         // These reduction adjustments have no proven non-linear scaling
 
         r += 543;  // Base reduction offset to compensate for other tweaks
-        r -= moveCount * 66;
-        r -= (ss - 2)->moveCount * 24 * ss->ply / rootDepth;
+        r -= moveCount * (66 + (ss - 2)->moveCount);
         r -= std::abs(correctionValue) / 30450;
 
         // Increase reduction for cut nodes
