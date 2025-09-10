ae3fb608e6d5189de9015b1b60a23b328f059893
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-09-11 00:27:04 +0200

    Half effect: Less reduction proportional to previous own move count. Bench: 2911887

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop:  // When in check, search starts here
 
         r += 543;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 66;
-        r -= (ss - 2)->moveCount * 32;
+        r -= (ss - 2)->moveCount * 8;
         r -= std::abs(correctionValue) / 30450;
 
         // Increase reduction for cut nodes
