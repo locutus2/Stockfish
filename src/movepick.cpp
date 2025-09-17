dcefae20b4344d4c2ca2214b227fa571806f39ab
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-09-17 23:17:41 +0200

    Use for evasion captures the same move order score as for general captures. Bench: 2615257

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -183,7 +183,8 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
         else  // Type == EVASIONS
         {
             if (pos.capture_stage(m))
-                m.value = (*captureHistory)[pc][to][type_of(capturedPiece)] + (1 << 28);
+                m.value = (*captureHistory)[pc][to][type_of(capturedPiece)]
+                        + 7 * int(PieceValue[capturedPiece]) + (1 << 28);
             else
             {
                 m.value = (*mainHistory)[us][m.from_to()] + (*continuationHistory[0])[pc][to];
