ded0cf3ecd36ce43d98416f8ba7a072c09474ee5
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-09-18 13:01:42 +0200

    In evasion ordering use for captures the capture history + 3 * captured piece value. Bench: 2600942

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -184,7 +184,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
         {
             if (pos.capture_stage(m))
                 m.value = (*captureHistory)[pc][to][type_of(capturedPiece)]
-                        + 2 * PieceValue[capturedPiece] + (1 << 28);
+                        + 3 * PieceValue[capturedPiece] + (1 << 28);
             else
             {
                 m.value = (*mainHistory)[us][m.from_to()] + (*continuationHistory[0])[pc][to];
