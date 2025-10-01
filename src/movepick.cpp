b54cb901af2ec20f6cec20a95d56755adc152908
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-10-01 23:35:21 +0200

    Use weight 17/16 for cmh0 in quiet ordering. Bench: 1908438

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -160,7 +160,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             // histories
             m.value = 2 * (*mainHistory)[us][m.from_to()];
             m.value += 2 * (*pawnHistory)[pawn_history_index(pos)][pc][to];
-            m.value += (*continuationHistory[0])[pc][to] * 15 / 16;
+            m.value += (*continuationHistory[0])[pc][to] * 17 / 16;
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
             m.value += (*continuationHistory[3])[pc][to];
