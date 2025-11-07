c7250efe71b5ac83d637a2808d2a6eef76fd953f
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-11-07 20:18:01 +0100

    50% more effect: Try a simplified version of my bad quiet update: use move count instead (factor=9) for all best moves. Bench: 2626084

--- src/search.cpp
+++ src/search.cpp
@@ -1803,7 +1803,7 @@ void update_all_stats(const Position& pos,
     Piece                  movedPiece     = pos.moved_piece(bestMove);
     PieceType              capturedPiece;
 
-    int bonus = std::min(121 * depth - 77, 1633) + 375 * (bestMove == ttMove) + 12 * moveCount;
+    int bonus = std::min(121 * depth - 77, 1633) + 375 * (bestMove == ttMove) + 9 * moveCount;
     int malus = std::min(825 * depth - 196, 2159) - 16 * moveCount;
 
     if (!pos.capture_stage(bestMove))
