7826fff81c061ccd18dbaa066e4913174c3ab903
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-11-14 11:55:20 +0100

    After introduction of TI-net try to remove quiet order threat bonus. Bench: 2502122

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -145,7 +145,6 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
         ExtMove& m = *it++;
         m          = move;
 
-        const Square    from          = m.from_sq();
         const Square    to            = m.to_sq();
         const Piece     pc            = pos.moved_piece(m);
         const PieceType pt            = type_of(pc);
@@ -168,12 +167,6 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
 
             // bonus for checks
             m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
-
-            // penalty for moving to a square threatened by a lesser piece
-            // or bonus for escaping an attack by a lesser piece.
-            int v = threatByLesser[pt] & to ? -10 : 10 * bool(threatByLesser[pt] & from);
-            m.value += PieceValue[pt] * v;
-
 
             if (ply < LOW_PLY_HISTORY_SIZE)
                 m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
