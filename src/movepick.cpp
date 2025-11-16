9c5ebdbe7f17b5dc29da3b402a2673f87a6dc6bf
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-11-16 10:43:36 +0100

    Half effect: Give threat bonus also if piece is attacked but undefended. Bench: 2818613

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -175,9 +175,9 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
-            int v = threatByLesser[pt] & to
-                    ? -19
-                    : 20 * bool((threatByLesser[pt] | undefendedThreat) & from);
+            int v = threatByLesser[pt] & to   ? -19
+                  : threatByLesser[pt] & from ? 20
+                                              : 10 * bool(undefendedThreat & from);
             m.value += PieceValue[pt] * v;
 
 
