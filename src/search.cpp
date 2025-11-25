0e2972b2b92fd3ea3647ba4ad676b712776c2999
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-11-25 16:39:34 +0100

    At PV nodes: Scale up reduction with factor 33/32. Bench: 2849242

--- src/search.cpp
+++ src/search.cpp
@@ -1210,8 +1210,8 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
-        if (!PvNode)
-            r -= r / 32;
+        if (PvNode)
+            r += r / 32;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
