d9d50db0e556fbcecba85c589e666fcf52abde02
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-11-25 15:31:34 +0100

    At non-PV nodes: Scale down reduction with factor 31/32. Bench: 3010703

--- src/search.cpp
+++ src/search.cpp
@@ -1209,8 +1209,9 @@ moves_loop:  // When in check, search starts here
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
-        r -= r / 32;
 
+        if (!PvNode)
+            r -= r / 32;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
