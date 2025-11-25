c799532a961b316b40fdbb104ee4accf2f095290
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-11-25 17:11:11 +0100

    Double effect: Scale up reduction with factor 17/16. Bench: 2658732

--- src/search.cpp
+++ src/search.cpp
@@ -1209,9 +1209,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
-
-        if (PvNode)
-            r += r / 32;
+        r += r / 16;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
