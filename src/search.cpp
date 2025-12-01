cdcb7042be6295b5eab5567421bdc448a0382a0a
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-12-01 11:08:01 +0100

    At non-PV nodes: Scale up reduction with factor 5/4. Bench: 2353525

--- src/search.cpp
+++ src/search.cpp
@@ -1219,7 +1219,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         if (!PvNode)
-            r += r / 8;
+            r += r / 4;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
