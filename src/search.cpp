0d8a56f0982d92229fad86d39496228477696e4f
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-12-20 02:11:27 +0100

    At ALL nodes: Scale up reduction with factor 5/4. Bench: 2559852

--- src/search.cpp
+++ src/search.cpp
@@ -1221,7 +1221,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         if (allNode)
-            r += r / 8;
+            r += r / 4;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
