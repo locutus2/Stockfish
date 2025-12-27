ec4084b9d0faa42ee3d2738e6d02bab2763a883b
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-12-27 09:20:05 +0100

    Double effect of new depth dependent reduction scaling. Test against PR 6496. Bench: 2568564

--- src/search.cpp
+++ src/search.cpp
@@ -1222,8 +1222,8 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         // Scale up reductions for expected ALL nodes
-        if (!ss->ttPv)
-            r += r / (depth + 1);
+        if (allNode)
+            r += r * 2 / (depth + 1);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
