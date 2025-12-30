a31d4ebbb06f279abab3020737274d044354e370
Stefan Geschwentner <stgeschwentner@gmail.com>
2025-12-30 17:08:38 +0100

    Use offset=3 for new depth dependent reduction scaling. Bench: 2497574

--- src/search.cpp
+++ src/search.cpp
@@ -1220,7 +1220,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r / (depth + 2);
+            r += r / (depth + 3);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
