b7f934b414dd41f74f64e2f04875689eb42c3ac2
Stefan Geschwentner <git config --global user.email>
2026-01-04 22:04:04 +0100

    Use following depth dependent reduction factor: r * depth / (depth * depth + 5). Bench: 2251267

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * 2 * depth / (depth * (2 * depth + 1) + 1);
+            r += r * depth / (depth * depth + 5);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
