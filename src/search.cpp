40e04311b7d10587d2e7b7f946a28440a868714c
Stefan Geschwentner <git config --global user.email>
2026-01-06 23:08:35 +0100

    After 92k LTC lmr conditions tuning test most significant negative param A[0][1][2]. Bench: 2525017

--- src/search.cpp
+++ src/search.cpp
@@ -1192,13 +1192,16 @@ moves_loop:  // When in check, search starts here
             r -= 2719 + PvNode * 983 + (ttData.value > alpha) * 922
                + (ttData.depth >= depth) * (934 + cutNode * 1011);
 
+        else if (cutNode && !improving)
+            r -= 173;
+
         r += 714;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 73;
         r -= std::abs(correctionValue) / 30370;
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 3199 + 997 * !ttData.move + 173 * improving;
+            r += 3372 + 997 * !ttData.move;
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
