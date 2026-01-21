59d0ee93ae60dd87f75f0af70987662f723b594e
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-01-21 01:47:41 +0100

    Scale down reductions depenent on depth at CUT nodes. Bench: 2430159

--- src/search.cpp
+++ src/search.cpp
@@ -1227,6 +1227,9 @@ moves_loop:  // When in check, search starts here
         if (allNode)
             r += r / (depth + 1);
 
+        else if (cutNode)
+            r -= r / (depth + 1);
+
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
         {
