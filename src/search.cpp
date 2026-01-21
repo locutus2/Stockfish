c1dbe7c513401f29b06013730649b972e778b482
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-01-21 03:18:05 +0100

    Scale up reductions depenent on depth at CUT nodes. Bench: 2329234

--- src/search.cpp
+++ src/search.cpp
@@ -1224,11 +1224,8 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         // Scale up reductions for expected ALL nodes
-        if (allNode)
+        if (!PvNode)
             r += r / (depth + 1);
-
-        else if (cutNode)
-            r -= r / (depth + 1);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
