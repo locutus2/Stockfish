b07452cf3eb3195ff03e6cd7d6e2bc0d47d4ac83
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-01-25 03:06:23 +0100

    Asymptotic 25% more LMR scaling. Bench: 2625531

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * 3 / (2 * depth + 4);
+            r += r * 5 / (4 * depth + 6);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
