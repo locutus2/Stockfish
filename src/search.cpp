062489afa676afefb22c6b23a29a431b1908acb0
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-01-25 03:46:37 +0100

    More LMR scaling with factor (msb(depth)+1)/(4*depth-2). Bench: 2490994

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * 5 / (4 * depth + 6);
+            r += r * (msb(depth) + 1) / (4 * depth - 2);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
