9e4fa0e1f0af9cd3d1fc16a87074542ca4ea5289
Stefan Geschwentner <git config --global user.email>
2026-01-27 23:31:14 +0100

    Asymptotic 25% more LMR scaling if prior capture. Bench: 2302955

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,12 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r / (depth + 1);
+        {
+            if (priorCapture)
+                r += r * 5 / (4 * depth + 6);
+            else
+                r += r / (depth + 1);
+        }
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
