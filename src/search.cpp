9b0c38d76aaf81bd5cc49dd4534e226652b6504b
Stefan Geschwentner <git config --global user.email>
2026-02-02 12:00:52 +0100

    More effect: Less/more LMR scaling if low/high move count. Bench: 2419188

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (moveCount + 64) / (71 * depth + 71);
+            r += r * (moveCount + 32) / (36 * depth + 36);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
