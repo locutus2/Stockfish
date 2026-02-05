cd9b17f8fa84e3edacc3f916fd0f893524245f36
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-05 09:50:02 +0100

    1/8 effect: More LMR scaling dependent on (ss+2)->cutoffCnt. Bench: 2866278

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * ((ss + 2)->cutoffCnt + 4) / (4 * depth + 4);
+            r += r * ((ss + 2)->cutoffCnt + 8) / (8 * depth + 8);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
