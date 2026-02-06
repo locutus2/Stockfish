8d8f4a8041695021db6e09fe6d5e6756ef1b60bd
Stefan Geschwentner <git config --global user.email>
2026-02-06 11:13:01 +0100

    More LMR scaling dependent on (ss+2)->cutoffCnt. But scale down effect with depth (quarter this down scaling effect). Bench: 2377479

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (2 * (ss + 2)->cutoffCnt + depth) / (2 * depth * depth + 2);
+            r += r * (4 * (ss + 2)->cutoffCnt + depth) / (4 * depth * depth + 4);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
