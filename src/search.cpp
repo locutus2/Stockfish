9fa0f33b76a11e4686daaea6a4bf6d9734a1064a
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-10 18:24:00 +0100

    No LMR scaling if tt lower bound and tt valua > alpha. Bench: 2849483

--- src/search.cpp
+++ src/search.cpp
@@ -1223,7 +1223,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         // Scale up reductions for expected ALL nodes
-        if (allNode)
+        if (allNode && (!(ttData.bound & BOUND_LOWER) || ttData.value <= alpha))
             r += r / (depth + 1);
 
         // Step 17. Late moves reduction / extension (LMR)
