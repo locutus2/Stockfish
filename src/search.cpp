a18c2a1e01ffa6aac91c28700cbfc4ca4ca589f7
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-10 01:59:27 +0100

    No LMR scaling for captures. Bench: 2590248

--- src/search.cpp
+++ src/search.cpp
@@ -1223,8 +1223,8 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 850 / 8192;
 
         // Scale up reductions for expected ALL nodes
-        if (allNode)
-            r += r / (depth + 1 + 4 * capture);
+        if (allNode && !capture)
+            r += r / (depth + 1);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
