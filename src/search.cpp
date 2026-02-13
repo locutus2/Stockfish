0bf4716006987ad20c04d9b421d3213aff1afb25
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-13 23:23:08 +0100

    Half LMR scaling for captures at 2*depth < rootDepth. Bench: 2883625

--- src/search.cpp
+++ src/search.cpp
@@ -1223,7 +1223,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (2 - (capture && depth < 7)) / (2 * depth + 2);
+            r += r * (2 - (capture && 2 * depth < rootDepth)) / (2 * depth + 2);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
