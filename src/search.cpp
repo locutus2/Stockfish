5693fb4bb106ade2f30319a6e2ff1f65c96cd32f
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-17 13:31:58 +0100

    Skip razoring if tt capture exists and depth >= 3. Bench: 2499658

--- src/search.cpp
+++ src/search.cpp
@@ -868,7 +868,7 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && (!ttCapture || depth < 2) && eval < alpha - 485 - 281 * depth * depth)
+    if (!PvNode && eval < alpha - 485 - 281 * depth * depth && (!ttCapture || depth < 3))
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node
