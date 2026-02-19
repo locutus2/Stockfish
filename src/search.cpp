a38b6d28b3057fba4199113c5d3c9c4dfbd30506
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-19 22:35:32 +0100

    [Simplified] Skip multi-cut at PV nodes if tt value < beta and previous move have a stat score > 0. Instead decrease depth. Bench: 2683692

--- src/search.cpp
+++ src/search.cpp
@@ -1163,8 +1163,7 @@ moves_loop:  // When in check, search starts here
                     return value;
                 }
 
-                if (depth > 1)
-                    depth--;
+                depth--;
             }
 
             // Negative extensions
