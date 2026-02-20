77c7166a8e9e1f92bd6df36453587e9317d96a91
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-21 00:24:09 +0100

    Double effect: Add history table for alternative good moves depending on the original tt move (collect stats only for quiet moves). Use the absolute value of this history in quiet ordering (suprisingly i have measured for low and high history values bigger P(value>alpha), thats the reason for abs, see screenshot in repo comment). Bench: 2818132

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -167,7 +167,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             m.value += (*continuationHistory[2])[pc][to];
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
-            m.value += std::abs((*ttMoveAlternativeHistory)[pc][to]);
+            m.value += 2 * std::abs((*ttMoveAlternativeHistory)[pc][to]);
 
             // bonus for checks
             m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
