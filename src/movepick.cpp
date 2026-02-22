0553e0be501f17a2a6a9a5fcf4f359362ba0b244
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-02-22 11:47:24 +0100

    restore master

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -167,7 +167,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             // histories
             m.value = 2 * (*mainHistory)[us][m.raw()];
             m.value += 2 * sharedHistory->pawn_entry(pos)[pc][to];
-            m.value += (*continuationHistory[0])[pc][to] * (2 + bool(pos.captured_piece())) / 2;
+            m.value += (*continuationHistory[0])[pc][to];// * (2 + bool(pos.captured_piece())) / 2;
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
             m.value += (*continuationHistory[3])[pc][to];
