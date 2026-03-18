2e12ae0bb55751ec7a8fc2b14168000d31dcf131
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-03-18 14:52:21 +0100

    Try multi-cut only if (ss+1)->moveCount != 1. Test on top of passed VVLTC tuning PR 6669. Bench: 2450171

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,7 @@ moves_loop:  // When in check, search starts here
             // over the original beta, we assume this expected cut-node is not
             // singular (multiple moves fail high), and we can prune the whole
             // subtree by returning a softbound.
-            else if (cutNode && value >= beta && !is_decisive(value))
+            else if (value >= beta && !is_decisive(value) && (ss + 1)->moveCount != 1)
             {
                 ttMoveHistory << std::max(-424 - 107 * depth, -3375);
                 return value;
