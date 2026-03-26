bad392aaa851446831ec34da6048e98fb34b4a76
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-03-26 19:30:41 +0100

    Skip multi-cut if previous move count = 0. Bench: 2977124

--- src/search.cpp
+++ src/search.cpp
@@ -1177,7 +1177,7 @@ moves_loop:  // When in check, search starts here
             // over the original beta, we assume this expected cut-node is not
             // singular (multiple moves fail high), and we can prune the whole
             // subtree by returning a softbound.
-            else if (value >= beta && !is_decisive(value))
+            else if (value >= beta && !is_decisive(value) && (ss - 1)->moveCount != 0)
             {
                 ttMoveHistory << std::max(-424 - 107 * depth, -3375);
                 return value;
