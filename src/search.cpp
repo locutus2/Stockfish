bde0eafb2449e904712bd8af32be6a345dc85a7e
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-03-27 23:43:36 +0100

    20% less time usage if opponent played expected move. Bench: 3164843

--- src/search.cpp
+++ src/search.cpp
@@ -521,7 +521,7 @@ void Search::Worker::iterative_deepening() {
             double highBestMoveEffort = nodesEffort > 86000 ? 0.74 : 0.96;
 
             double opponentPlayedExpectedMove =
-              (rootPos.key() == mainThread->expectedPositionKey ? 0.9 : 1.1);
+              (rootPos.key() == mainThread->expectedPositionKey ? 0.8 : 1.0);
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
                              * bestMoveInstability * highBestMoveEffort
