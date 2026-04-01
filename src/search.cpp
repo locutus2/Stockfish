f1a47e53843c57acbda26ee64709fe4963514e97
Stefan Geschwentner <git config --global user.email>
2026-04-01 17:21:15 +0200

    Skip multi-cut at non-cut nodes if previous stat score > 0. Instead decrease depth. Bench: 2930209

--- src/search.cpp
+++ src/search.cpp
@@ -1177,10 +1177,17 @@ moves_loop:  // When in check, search starts here
             // over the original beta, we assume this expected cut-node is not
             // singular (multiple moves fail high), and we can prune the whole
             // subtree by returning a softbound.
-            else if (value >= beta && !is_decisive(value) && (cutNode || (ss - 1)->statScore <= 0))
+            else if (value >= beta && !is_decisive(value))
             {
-                ttMoveHistory << std::max(-424 - 107 * depth, -3375);
-                return value;
+                if (cutNode || (ss - 1)->statScore <= 0)
+                {
+                    ttMoveHistory << std::max(-424 - 107 * depth, -3375);
+                    return value;
+                }
+
+                depth--;
+
+                assert(depth > 0);
             }
 
             // Negative extensions
