742fc58708360cf97e19675fbd25aee13ed5075b
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-04-16 14:41:14 +0200

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1114,12 +1114,12 @@ moves_loop:  // When in check, search starts here
 		//C = depth;
 		C = 0;
                 V = history + 4097 * depth;
-                V += -CC * (*contHist[0])[movedPiece][move.to_sq()];
+                //V += -CC * (*contHist[0])[movedPiece][move.to_sq()];
                 //V += -CC * (*contHist[0])[movedPiece][move.to_sq()] / 2;
                 //V += CC * mainHistory[us][move.raw()];
 		//V += CC * 4096;
                 //V += CC * mainHistory[us][move.raw()] * 71 / 32;
-                //V += CC * sharedHistory.pawn_entry(pos)[movedPiece][move.to_sq()];
+                V += -CC * sharedHistory.pawn_entry(pos)[movedPiece][move.to_sq()];
                 //V = history + 1*CC * (mainHistory[us][move.raw()] + 3840) + 4097 * depth;
                 //V = history + 1*CC * (mainHistory[us][move.raw()] + 1920) + 4097 * depth;
                 //if (history < -4097 * depth)
