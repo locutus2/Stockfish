24f5927ecb06110f1eac219e07420aff4db46f7a
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-04-19 14:15:23 +0200

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1109,30 +1109,31 @@ moves_loop:  // When in check, search starts here
 
 		CC = true;
 		int hist0 = history;
-		int hist1 = history + CC * mainHistory[us][move.raw()] / 4;
+		int hist1 = history + CC * mainHistory[us][move.raw()];
 		//int hist1 = history + CC * (*contHist[3])[movedPiece][move.to_sq()];
 		//int hist2 = (hist1 + 1559.62) / 9812.07 * 9040.88 + 834.991; // factor 0,9214039443257131267917982647902
 		//int hist2 = (hist1 + 1385.49) / 9206.6 * 8929.47 - 70.262; // factor 0,96989876827493320009558360306736
 		//int hist2 = (hist1 + 1471.81) / 9329.07 * 9048.21 - 154.901; // factor 0,96989410520019680418305361627686
 		//int hist2 = (hist1 + 434.254) / 9140.06 * 9066.48 + 186.95; // factor 0,99194972461887558724997428900904
 		//int hist2 = (hist1 + 624.576) / 9382.6 * 9165.41 + 528.209; // factor 0,97685183211476562999594994990728
-		int hist2 = (hist1 + 48.0659) / 9224.74 * 9165.41 + 528.209; // factor 0,9935683824151141387182728185293
-                //hist1 < -2466 - 4446 * depth;
+		//int hist2 = (hist1 + 48.0659) / 9224.74 * 9165.41 + 528.209; // factor 0,9935683824151141387182728185293
+		int hist2 = (hist1 + 1777.6) / 9972.28 * 9165.41 + 528.209; // factor 0,91908871391497230322453842050163
 		P0 = hist0 < -4097*depth;
 		//P1 = P0;
-		//P1 = hist1 < -2466 - 4446 * depth; // main
+		//P1 = hist1 < -2466 - 4446 * depth; //old: main
 		//P1 = hist1 < -1312 - 4224 * depth; //old: main/2
 		//P1 = hist1 < -623 - 4130 * depth; //old: main/4
+		P1 = hist1 < -2352 - 4458 * depth; // main
 		//P1 = hist1 < -1165 - 4194 * depth; // main/2
-		P1 = hist1 < -580 - 4124 * depth; // main/4
+		//P1 = hist1 < -580 - 4124 * depth; // main/4
 
 		/*
 		 * Mean #0: Total 70535670 Mean 528.209
-		Mean #1: Total 70535670 Mean -48.0659
-		Mean #2: Total 70535670 Mean 1091.34
+		Mean #1: Total 70535670 Mean -1777.6
+		Mean #2: Total 70535670 Mean -1190.13
 		Stdev #0: Total 70535670 Stdev 9165.41
-		Stdev #1: Total 70535670 Stdev 9224.74
-		Stdev #2: Total 70535670 Stdev 9010.82
+		Stdev #1: Total 70535670 Stdev 9972.28
+		Stdev #2: Total 70535670 Stdev 9907.76
 		 * */
                 // Continuation history based pruning
                 //if (history < -4097 * depth)
