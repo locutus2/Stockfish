3ee0b63c5bde44da59834f03e04b8f46ad038ee8
Stefan Geschwentner <git config --global user.email>
2026-04-30 16:53:56 +0200

    Tweak history pruning threshold via ALL node. Take 3. Bench: 3045262

--- src/search.cpp
+++ src/search.cpp
@@ -1123,7 +1123,7 @@ moves_loop:  // When in check, search starts here
                             + sharedHistory.pawn_entry(pos)[movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4097 * depth + 2048 * allNode)
+                if (history < -4097 * depth + 2048 * allNode - 631)
                     continue;
 
                 history += 71 * mainHistory[us][move.raw()] / 32;
