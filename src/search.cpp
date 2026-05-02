091aa4cbf8f02b28d996e25c3a72486af072c6f4
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-05-02 17:42:46 +0200

    25% less LMR scaling after a null move. Bench: 2872967

--- src/search.cpp
+++ src/search.cpp
@@ -1262,7 +1262,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (273 - 136 * ((ss - 1)->currentMove == Move::null())) / (256 * depth + 260);
+            r += r * (273 - 68 * ((ss - 1)->currentMove == Move::null())) / (256 * depth + 260);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
