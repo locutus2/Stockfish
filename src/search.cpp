bd745205481b02305e536acad7e4b51f8da95cac
Stefan Geschwentner <git config --global user.email>
2026-05-04 12:37:28 +0200

    Quadruple effect: Tweak history pruning threshold via ALL node. Bench: 2041444

--- src/search.cpp
+++ src/search.cpp
@@ -1124,7 +1124,7 @@ moves_loop:  // When in check, search starts here
                             + sharedHistory.pawn_entry(pos)[movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4097 * depth + 4096 * allNode - 1786)
+                if (history < -4097 * depth + 8192 * allNode - 3572)
                     continue;
 
                 history += 71 * mainHistory[us][move.raw()] / 32;
