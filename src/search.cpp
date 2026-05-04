de0ac763cd081d1b6f981c22dd3de0943064cc11
Stefan Geschwentner <git config --global user.email>
2026-05-04 18:00:01 +0200

    Triple LMR scaling if at root of a probcut search. Bench: 3065279

--- src/search.cpp
+++ src/search.cpp
@@ -1263,7 +1263,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (273 + 68 * isProbcut) / (256 * depth + 260);
+            r += r * (273 + 546 * isProbcut) / (256 * depth + 260);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
