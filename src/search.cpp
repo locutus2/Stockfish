0f46d1191bac18721d48d9137f65747e14a547c2
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-05-24 16:55:31 +0200

    Increase best move history bonus by (number of searched moves)/256 if at ALL node. As compensation increase also malus by bonus/256. Bench: 2393995

--- src/search.cpp
+++ src/search.cpp
@@ -1884,7 +1884,10 @@ void update_all_stats(const Position& pos,
     int malus = std::min(1005 * depth - 205, 2218);
 
     if (allNode)
+    {
+        malus += bonus / 256;
         bonus += bonus * (quietsSearched.size() + capturesSearched.size()) / 256;
+    }
 
     if (!pos.capture_stage(bestMove))
     {
