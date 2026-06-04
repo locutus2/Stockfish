c9d7c4b0d345e0269a312745116a0e0f4181559e
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-04 13:55:44 +0200

    Introduce smoothed main history. This returns an exponetial average of the history values (period=2). Test against PR 6871. Bench: 2949694

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -228,8 +228,7 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
         else if constexpr (Type == QUIETS)
         {
             // histories
-            auto mainHist = (*mainHistory)[us][m.raw()].get();
-            m.value       = mainHist.first + mainHist.second;
+            m.value = 2 * (*mainHistory)[us][m.raw()];
             m.value += 2 * sharedHistory->pawn_entry(pos)[pc][to];
             m.value += (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
