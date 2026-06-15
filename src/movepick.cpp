cc1c31df225c4652f03a54ed98bd3634bb40edb4
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-15 12:25:10 +0200

    Increase low ply history in quiet move ordering by 100%. Bench: 2884775

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -246,7 +246,7 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
 
 
             if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 12 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
+                m.value += 16 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
         }
 
         else  // Type == EVASIONS
