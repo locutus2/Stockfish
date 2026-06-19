2b2b0a1d1808170920f2e7e2d8d9bb1954e5c575
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-20 01:29:24 +0200

    If singular extension increase depth only if no prior capture. Bench: 2891820

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth += !ss->inCheck;
+                depth += !priorCapture;
             }
 
             // Multi-cut pruning
