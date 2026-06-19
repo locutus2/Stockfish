06f038520615c2f3f3c7bfa7d5c9d958fc66e8e6
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-20 01:27:08 +0200

    If singular extension increase depth only if not in check. Bench: 2588516

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth++;
+                depth += !ss->inCheck;
             }
 
             // Multi-cut pruning
