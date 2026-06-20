4a084a73203a1abb23da19a16afa0d2f2b587d91
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-20 10:44:21 +0200

    If singular extension increase depth only if no tt capture. Bench: 2609621

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth += !priorCapture;
+                depth += !ttCapture;
             }
 
             // Multi-cut pruning
