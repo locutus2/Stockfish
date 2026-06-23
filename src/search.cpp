2f6ed3b7c406faabf5bdc961edd0a207724a2489
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-23 18:14:53 +0200

    Try opposite direction: If singular extension increase depth by 2 if not at ALL node (capped by extension). Bench: 3330552

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth += std::min(extension, 1 + improving);
+                depth += std::min(extension, 2 - allNode);
             }
 
             // Multi-cut pruning
