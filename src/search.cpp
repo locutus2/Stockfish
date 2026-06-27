de4dc39f715652f96f4059c691317f44859982cd
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-27 04:28:42 +0200

    Less reduction at non-PV nodes if sum of 7 conditions >= 6. Bench: 2246762

--- src/search.cpp
+++ src/search.cpp
@@ -1312,6 +1312,12 @@ moves_loop:  // When in check, search starts here
         else if (move == ttData.move)
             r = std::max(0, r - 2016);
 
+        if (!PvNode
+            && 2 * (move == ttData.move) + bool(excludedMove) + capture + cutNode + !ss->ttPv
+                   + opponentWorsening
+                 >= 6)
+            r -= 1024;
+
         if (capture)
             ss->statScore = 809 * int(PieceValue[pos.captured_piece()]) / 128
                           + captureHistory[movedPiece][move.to_sq()][type_of(pos.captured_piece())];
