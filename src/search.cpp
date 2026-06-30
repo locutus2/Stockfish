286daebd228c84f1ace079f0bb7f5d005408b75e
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-06-30 15:04:56 +0200

    Calculate index as sum of different conditions. Apply positive and negative reductions proportional to the index. Take 3 (same index as take 2 but mean=0 and sigma=512). Bench: 2597054

--- src/search.cpp
+++ src/search.cpp
@@ -1301,7 +1301,7 @@ moves_loop:  // When in check, search starts here
             int index = 31 * ((ss - 1)->moveCount > 0) + 15 * (move == ttData.move) + 5 * cutNode
                       + 4 * bool(excludedMove) + 3 * (capture + ttCapture) + givesCheck
                       + !priorCapture + !improving + opponentWorsening;
-            r -= 26 * index - 1024;
+            r -= 50 * index - 1791;
         }
 
         // Increase reduction for cut nodes
