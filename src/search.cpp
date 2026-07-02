81c6cbf89be2e7d9f0dbfc7cb1e8382c6cf25a90
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-07-03 00:24:34 +0200

    Double effect and half progress margin: More reduction (dependent on goodness of previous position) at non-ttPv nodes if we make from a good previous postion (static eval > 0, look 7 plies back) a significant progress (>= 75) in static eval. Bench: 2332770

--- src/search.cpp
+++ src/search.cpp
@@ -1324,8 +1324,8 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 445 / 4096;
 
         if (!ss->ttPv && ss->ply >= 7 && ss->staticEval != VALUE_NONE && (ss - 7)->staticEval < 0
-            && ss->staticEval >= -(ss - 7)->staticEval + 150)
-            r -= (ss - 7)->staticEval / 2;
+            && ss->staticEval >= -(ss - 7)->staticEval + 75)
+            r -= 2 * (ss - 7)->staticEval;
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
