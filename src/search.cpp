f2f233c0e06d881b81f6ac4d96b36eca166c2600
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-07-10 23:11:03 +0200

    Half effect: Sligthly more reduction at ALL nodes if not also ttPv node. Bench: 2247353

--- src/search.cpp
+++ src/search.cpp
@@ -1327,7 +1327,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += 256 * !ss->ttPv + r * 272 / (256 * depth + 285);
+            r += 64 * !ss->ttPv + r * 272 / (256 * depth + 285);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
