1c2624d56d9edf19c85a57555c37ca2a21179541
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-07-10 23:34:44 +0200

    Half effect: Increase ALL node scaling factor if not at ttPv node. Bench: 2857584

--- src/search.cpp
+++ src/search.cpp
@@ -1327,7 +1327,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (272 + 128 * !ss->ttPv) / (256 * depth + 285);
+            r += r * (272 + 64 * !ss->ttPv) / (256 * depth + 285);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
