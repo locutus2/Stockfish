5d7a5fe5e60229026d520e656f0ff536a862e348
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-07-23 17:33:48 +0200

    Slightly decrease ALL node reduction scaling weight by depth + more compensation. Bench: 2576270

--- src/search.cpp
+++ src/search.cpp
@@ -1330,7 +1330,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (283 - depth) / (256 * depth + 268);
+            r += r * (288 - depth) / (256 * depth + 268);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
