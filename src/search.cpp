6b43bdc2fa60388ba611cdace8fa6cef5aa17312
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-07-27 11:15:04 +0200

    Replace reduction scaling with a ply, depth and root depth based formula. Take 2. Bench: 2400577

--- src/search.cpp
+++ src/search.cpp
@@ -1331,7 +1331,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * 110 * ss->ply / (256 * (ss->ply + depth + rootDepth));
+            r += r * 307 * ss->ply / (rootDepth * (256 * depth + 268));
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
