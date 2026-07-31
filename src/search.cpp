74c53ba62b47bc215a51caccefed3a50386ede21
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-07-31 17:15:15 +0200

    Double effect: More/less reduction scaling if eval<=alpha / eval>alpha. Bench: 2563659

--- src/search.cpp
+++ src/search.cpp
@@ -1331,7 +1331,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (269 + 8 * (eval <= alpha)) / (256 * depth + 268);
+            r += r * (262 + 16 * (eval <= alpha)) / (256 * depth + 268);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
