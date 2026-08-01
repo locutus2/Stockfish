6dad96245fb89695e8b0f5b40be53e3466529b42
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-08-01 13:21:46 +0200

    Less reduction scaling if prior capture. Bench: 2281155

--- src/search.cpp
+++ src/search.cpp
@@ -1336,7 +1336,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * (276 + 8 * priorCapture) / (256 * depth + 268);
+            r += r * (276 - 8 * priorCapture) / (256 * depth + 268);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
