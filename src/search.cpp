b0d91bc1d8da095fa71d30d0711cb8a0e4dd6e3b
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-08-15 16:19:18 +0200

    Because we use fractional reductions but full depth not all of them normally can be used in LMR. Now collect this unused reductions and propagate it as additional base reduction to the search of the next move at the current node + more compensation. Bench: 2842733

--- src/search.cpp
+++ src/search.cpp
@@ -1330,7 +1330,7 @@ moves_loop:  // When in check, search starts here
             r -= 3023 + PvNode * 1004 + (ttData.value > alpha) * 885
                + (ttData.depth >= depth) * (816 + cutNode * 940);
 
-        r += 504;  // Base reduction offset to compensate for other tweaks
+        r += 143;  // Base reduction offset to compensate for other tweaks
         r -= moveCount * 65;
         r -= std::abs(correctionValue) / 26310;
 
