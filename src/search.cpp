2cfda614008668fb49970ac3cbb7fd8457e141d6
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-09-03 04:07:25 +0200

    Use a more classical zobrist key incremental update everytime a dirty threat is added. Add threats correction history (inspired by threat input net) + compensation. Take 2. Bench: 2178955

--- src/search.cpp
+++ src/search.cpp
@@ -94,12 +94,12 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
     const int   btcv   = shared.threats_correction_entry<BLACK>(pos)[us].threatsBlack;
     const int   cntcv =
       m.is_ok()
-          ? 8453
+          ? 8536
             * ((*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
                + (*(ss - 4)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()])
-          : 61800;
+          : 62401;
 
-    return 14802 * pcv + 10198 * micv + 12453 * (wnpcv + bnpcv) + 1782 * (wtcv + btcv) + cntcv;
+    return 14946 * pcv + 10297 * micv + 12574 * (wnpcv + bnpcv) + 1180 * (wtcv + btcv) + cntcv;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
