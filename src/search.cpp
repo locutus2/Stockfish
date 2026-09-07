caaa715c8751bc56c0c74b69d60760c31c6b177a
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-09-07 21:03:08 +0200

    Keep asymptotic effect the same but quarter effect for depth 1. Scale down reduction (increasing with depth) at cut nodes. Bench: 2673088

--- src/search.cpp
+++ src/search.cpp
@@ -1359,7 +1359,7 @@ moves_loop:  // When in check, search starts here
             r += r * 276 / (256 * depth + 268);
 
         else if (cutNode)
-            r -= r * depth / (16 * depth + 32);
+            r -= r * depth / (16 * depth + 80);
 
         // Apply the computed LMR
         if (depth >= 2 && moveCount > 1)
