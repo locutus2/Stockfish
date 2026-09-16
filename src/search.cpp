7c4bad0b421540fbff638789c5175e7a10b9dbc6
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-09-16 19:04:36 +0200

    test

--- src/search.cpp
+++ src/search.cpp
@@ -113,6 +113,7 @@ int correction_value2(const Worker& w, const Position& pos, const Stack* const s
         : 0;
         //: 64049/2;
 
+    return 9512 * shared.minor_piece_correction_entry(pos)[~us].minor;
     return 13806 * shared.pawn_correction_entry(pos)[~us].pawn;
     //return cntcv2;
     //return 0;
@@ -916,7 +917,8 @@ Value Search::Worker::search(
     //const auto correctionValue2 = 0.89995658429032340447266938511437 * (1 * correctionValue + 0.79990168176360644222406597842344 * correction_value2(*this, pos, ss));
     //const auto correctionValue2 = 0.97492530156036740565863735290863 * (1 * correctionValue + 0.74559074411642630709548748574273 * correction_value2(*this, pos, ss));
     //const auto correctionValue2 = 0.98698546367786688575403086517725 * (1 * correctionValue + 0.72491226017961918386608808808464 * correction_value2(*this, pos, ss));
-    const auto correctionValue2 = 1.0401282689451751047995951999934 * (1 * correctionValue + 0.40813730624925712488630998734112 * correction_value2(*this, pos, ss));
+    //const auto correctionValue2 = 1.0401282689451751047995951999934 * (1 * correctionValue + 0.40813730624925712488630998734112 * correction_value2(*this, pos, ss));
+    const auto correctionValue2 = 1.0409172546174074588691790303573 * (1 * correctionValue + 0.55484160871571058538882135534896 * correction_value2(*this, pos, ss));
 
     dbg_mean_of(std::abs(correctionValue), 12);
     dbg_mean_of(std::abs(correctionValue2), 13);
