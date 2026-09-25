710afad6fa820a4a028609f6a96d5395976c891e
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-09-25 23:22:52 +0200

    Apply for time usage factor based on score swing + compensation to maintain average time usage. But use score swing cap 400 instead of 200. Bench: 1495562

--- src/search.cpp
+++ src/search.cpp
@@ -600,7 +600,7 @@ bool Search::Worker::iterative_deepening() {
               interpolate(i64(nodesEffort), i64(75800), i64(104510), 0.969, 0.714), 0.693, 0.838);
 
             Value  scoreSwing       = std::abs(rootMoves[0].score - rootMoves[0].previousScore);
-            double uncertaintyBonus = 0.9538 + std::min(double(scoreSwing), 100.0) / 1048.0;
+            double uncertaintyBonus = 0.9688 + std::min(double(scoreSwing), 400.0) / 4129.0;
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
                              * bestMoveInstability * highBestMoveEffort * uncertaintyBonus;
